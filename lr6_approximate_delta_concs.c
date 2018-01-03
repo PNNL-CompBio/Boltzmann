28ec1d5b4936c4551492a90e1540f3438fe0f04e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:05 -0800

    r4194 | dbaxter | 2017-05-09 10:03:52 -0700 (Tue, 09 May 2017) | 7 lines
    Changed dbug printing, but also changed algorithm,
    removing flux scaling, and combining forward and backward terms from
    the same reaction and avoiding computing exponentials of large
    magnitude numbers, also adding limits on arguments to exponentials along with
    a dg0_scale factor to keep dg0 sums well scaled.
    M    lr6_approximate_delta_concs.c

--- lr6_approximate_delta_concs.c
+++ lr6_approximate_delta_concs.c
@@ -57,23 +57,35 @@ int lr6_approximate_delta_concs(struct state_struct *state,
   struct  molecule_struct *molecule;
   struct  molecules_matrix_struct *molecules_matrix;
   struct  rxn_matrix_struct *rxn_matrix;
+  /*
   double  *forward_rxn_likelihoods;
   double  *reverse_rxn_likelihoods; 
   double  *counts;
-  double  *conc_to_count;
-  double  *product_term;
   double  *reactant_term;
-  double  *kf_rel;
-  double  *kr_rel;
+  
+  double  *conc_to_count;
+  */
+  double  *c;
+  double  *log_kf_rel;
+  double  *log_kr_rel;
+  double  *ke;
+  double  *rke;
+  double  b;
+  double  base;
+  double  log_c;
+  double  log_m_c;
+  double  dg0_scale_factor;
+  /*
   double  flux_scaling;
   double  frb;
   double  lrb;
   double  recip_frb;
-  double  forward;
-  double  backward;
+  double  recip_base_rt;
+  */
   double  pt;
   double  rt;
-  double  recip_base_rt;
+  double  max_log_g0_sum;
+  double  sflux;
   int64_t *molecules_ptrs;
   int64_t *rxn_indices;
   int64_t *coefficients;
@@ -83,8 +95,10 @@ int lr6_approximate_delta_concs(struct state_struct *state,
   int num_species;
   int num_rxns;
   int rxn;
+  int padi;
+  /*
   int base_rxn;
-
+  */
   int i;
   int j;
 
@@ -111,21 +125,35 @@ int lr6_approximate_delta_concs(struct state_struct *state,
   rxn_ptrs         = rxn_matrix->rxn_ptrs;
   molecule_indices = rxn_matrix->molecules_indices;
   rcoefficients    = rxn_matrix->coefficients;
+  c                = state->product_term;
+  /*
+  dg0_scale_factor = .001;
+  */
+  dg0_scale_factor = state->dg0_scale_factor;
+  /*
   product_term     = state->product_term;
   reactant_term    = state->reactant_term;
   base_rxn         = (int)state->base_reaction;
   counts           = state->ode_counts;
   conc_to_count    = state->conc_to_count;
-  kf_rel           = state->kf_rel;
-  kr_rel           = state->kr_rel;
+  */
+  ke               = state->ke;
+  rke              = state->rke;
+  log_kf_rel       = state->log_kf_rel;
+  log_kr_rel       = state->log_kr_rel;
+  max_log_g0_sum   = state->max_log_g0_sum;
+  /*
   forward_rxn_likelihoods = state->ode_forward_lklhds;
   reverse_rxn_likelihoods = state->ode_reverse_lklhds;
+  */
   if (success) {
+    /*
     for (i=0;i<num_species;i++) {
       counts[i] = concs[i] * conc_to_count[i];
     }
     success = update_rxn_likelihoods(state,counts,forward_rxn_likelihoods,
 				     reverse_rxn_likelihoods);
+    */
   }
   /*
   state->flux_scaling = 0.0;
@@ -138,20 +166,60 @@ int lr6_approximate_delta_concs(struct state_struct *state,
   */
   lfp      = state->lfp;
   /*
-  if ((base_rxn < 0)  || (base_rxn >= num_rxns)) {
-    success = 0;
-    if (lfp) {
-      fprintf(lfp,"lr2_approximate_delta_concs: Error, base_rxn = %d is not in [0:%d)\n",
-	      base_rxn,num_rxns);
-      fflush(lfp);
-    }
-  }
-  */
-  /*
-    Fill the reactant_terms and product_terms vectors for each reaction.
-    We really just need to know whether or not there are nonzero concentrations
-    for species on each side of each reaction, so as not to use our
-    flux approximations that assume presence of species.
+    The flux for a species is given by the sum of contributions from
+    the reactions it is present in.
+
+    The contribution, c, from a reaction with forward rate constant kf;
+    reverse rate constant kr; reciprocal equilibrium constant, rke (kr/kf);
+    product of reactant concentrations, R; 
+    and product of product concentrations, P; for a product species is
+    
+    c = kf * R - kr * P
+    
+    but kf and kr may not be computable even though c might be quite reasonable
+    ad kf and kr are nearly the same size.
+    But we have rke = kr/kf, and we have log(kf) and log(kr) available.
+
+    then we write c = kf * (R - rke * P)
+
+    and then let b = R - rke * P
+
+    then if b > 0 then 
+    
+     we may write 
+
+           log(c) = log(kf) + log(b);
+
+	   and c = exp(log(c));
+
+     if b = 0, c = 0
+
+     if b < 0 then
+         log(-c) = log(kf) + log(-b)
+	  c     = -exp(log(-c))
+
+     so c is a product_per_reaction_flux_contribution,
+     and -c is a reactant_per_reaction_flux_contribution.
+
+     As we won't need to use the product_term vector used in other routines
+     here we use it to store the vector c.
+ 
+     For a reactant species the sign is merely flipped.    
+
+     NB one might want to factor out kr instead of kb above then we 
+     have 
+              c = kr * (ke * R - P)
+
+	      let b = ke * R - P then
+
+	      log_c = log(kr) + log(b) when b> 0
+
+	      log_m_c = log(kr) + log(-b) when b < 0
+    
+              so we might want to choose the kr or kf with the smaller 
+	      absolute value so that the b term is larger (and as its a
+	      subtraction would have less cancellation error).
+         
   */
   if (success) {
     for (i=0;i<num_rxns;i++) {
@@ -172,42 +240,82 @@ int lr6_approximate_delta_concs(struct state_struct *state,
 	    pt = pt * concs[mi];
 	  }
 	}
+      } /* end for j */
+      if (abs(log_kf_rel[i]) < abs(log_kr_rel[i])) {
+	b = rt - (rke[i] * pt);
+	base = log_kf_rel[i] * dg0_scale_factor;
+      } else {
+	b = (ke[i] * rt) - pt;
+	base = log_kr_rel[i] * dg0_scale_factor;
       }
-      product_term[i] = pt;
-      reactant_term[i] = rt;
+      if (b == 0.0) {
+	/*
+	  reaction is at equilbrium
+	*/
+	c[i] = 0.0;
+      } else {
+	if (b > 0) {
+	  log_c = base + log(b);
+	  /* 
+	    here we should check on suitability of  log_c for 
+	    an arguemt to exp (should be in [-704:704])
+	  */
+	  if (log_c > max_log_g0_sum) {
+	    if (lfp) {
+	      fprintf(lfp,
+		      "lr6_approximate_delta_concs: log_c > %le, set to %le\n",
+		      max_log_g0_sum,max_log_g0_sum);
+	      fflush(lfp);
+	    }
+	    /*
+	    success = 0;
+	    */
+	    log_c = max_log_g0_sum;
+	  }
+	  c[i] = exp(log_c);
+	} else {
+	  log_m_c = base + log(-b);
+	  /* 
+	    here we should check on suitability of  log_c for 
+	    an arguemt to exp (should be in [-704:704])
+	  */
+	  if (log_m_c > max_log_g0_sum) {
+	    if (lfp) {
+	      fprintf(lfp,
+	      "lr6_approximate_delta_concs: log_m_c > %le, set to %le\n",
+		      max_log_g0_sum,max_log_g0_sum);
+	      fflush(lfp);
+	    }
+	    /*
+	    success = 0;
+	    */
+	    log_m_c = max_log_g0_sum;
+	  }
+	  c[i] = -exp(log_m_c);
+	}
+      }
     } /* end for (i...) */
-  }
+  } /* end if (success) */
   //------------------------------------
   if (success) {
     molecule = molecules;
     for (i=0;i<num_species;i++) {
-      forward = 0.0;
-      backward = 0.0;
+      sflux = 0.0;
       if (molecule->variable == 1) {
 	for (j=molecules_ptrs[i];j<molecules_ptrs[i+1];j++) {
 	  rxn = rxn_indices[j];
 	  if (coefficients[j] < 0) {
-	    if (reactant_term[rxn] > 0.0) {
-	      forward -= (kf_rel[rxn] * reactant_term[rxn]);
-	    }
-	    if (product_term[rxn] > 0.0) {
-	      backward += (kr_rel[rxn] * product_term[rxn]);
-	    }
+	    sflux -= c[rxn];
 	  } else {
 	    if (coefficients[j] > 0) {
-	      if (reactant_term[rxn] > 0.0) {
-		forward += (kf_rel[rxn] * reactant_term[rxn]);
-	      }
-	      if (product_term[rxn] > 0.0) {
-		backward -= (kr_rel[rxn] * product_term[rxn]);
-	      }
+	      sflux += c[rxn];
 	    }
 	  }
 	} /* end for(j...) */
 	/*
 	flux[i] = flux_scaling * ((forward * recip_frb) + (backward * lrb));
 	*/
-	flux[i] = forward + backward;
+	flux[i] = sflux;
       } else {
 	flux[i] = 0.0;
       }
@@ -216,15 +324,10 @@ int lr6_approximate_delta_concs(struct state_struct *state,
   } /* end if success */
 #ifdef DBG
   if (lfp) {
-    fprintf(lfp,"Mol_index\t   count   \t    flux\n");
+    fprintf(lfp,"Mol_index\t   conc   \t    flux\n");
     for (i=0;i<num_species;i++) {
       fprintf(lfp,"%d\t%le\t%le\n",
-	      i,counts[i],flux[i]);
-    }
-    fprintf(lfp,"rxn_n\tfrwrd_lklhd\trvrs_lklhd\n");
-    for (i=0;i<num_rxns;i++) {
-      fprintf(lfp,"%d\t%le\t%le\n",i,forward_rxn_likelihoods[i],
-	      reverse_rxn_likelihoods[i]);
+	      i,concs[i],flux[i]);
     }
     fflush(lfp);
   }
