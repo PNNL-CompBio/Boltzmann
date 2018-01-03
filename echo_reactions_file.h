274e2b5e49764d9002b87e9092b8977880e15a35
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:21 -0800

    r2040 | dbaxter | 2013-02-06 10:20:50 -0800 (Wed, 06 Feb 2013) | 3 lines
    Added file pointer argument for call from boltzmann_boot_check routine.
    M    echo_reactions_file.h

--- echo_reactions_file.h
+++ echo_reactions_file.h
@@ -20,4 +20,5 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int echo_reactions_file(struct state_struct *state);
+extern int echo_reactions_file(struct state_struct *state,
+			       FILE* rxn_echo_fp);
