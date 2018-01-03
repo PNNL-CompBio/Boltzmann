9644e2e64095e3c1d132786d7cb554a565d0de3b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:30 -0800

    r4471 | dbaxter | 2017-08-25 16:14:50 -0700 (Fri, 25 Aug 2017) | 4 lines
    Removed molecules_matrix_p and transpose_wrok_p arguments as those are
    now fields in state. Added setup argument.
    M    alloc4.h

--- alloc4.h
+++ alloc4.h
@@ -20,6 +20,7 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int alloc4(struct state_struct *state,
-		  struct molecules_matrix_struct **molecules_matrix_p,
-		  int64_t **transpose_work_p);
+#ifndef _ALLOC4_H_
+#define _ALLOC4_H_ 1
+extern int alloc4(struct state_struct *state, int setup);
+#endif
