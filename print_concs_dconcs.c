cf2acde6b809db3e5f607e1c4eed5a40fae5e08f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:08 -0800

    r4235 | dbaxter | 2017-06-14 10:03:59 -0700 (Wed, 14 Jun 2017) | 3 lines
    Replaces print_concs_fluxes.c
    A    print_concs_dconcs.c

---
+++ print_concs_dconcs.c
@@ -0,0 +1,70 @@
+#include "boltzmann_structs.h"
+
+#include "update_rxn_likelihoods.h"
+#include "print_concs_dconcs.h"
+int print_concs_dconcs(struct state_struct *state,int ny,
+		       double *dconcs, 
+		       double *concs, 
+		       double time, 
+		       double h,
+		       int    step,
+		       int    origin) {
+/* 
+  Debugging routine to print concentrations and delta concentrations, dconcs,
+  for each molecule.
+  Called by: ode23tb 
+  Calls      update_rxn_likelihoods, fprintf
+*/
+  struct molecule_struct *molecule;  
+  double *conc_to_count;
+  double *counts;
+  double *forward_rxn_likelihoods;
+  double *reverse_rxn_likelihoods;
+  char   *molecules_text;
+  FILE *lfp;
+  FILE *efp;
+  int  i;
+  int  success;
+  int  nrxns;
+  int  padi;
+  success = 1;
+  lfp =  state->lfp;
+  molecule = state->sorted_molecules;
+  molecules_text = state->molecules_text;
+  nrxns          = state->number_reactions;
+  conc_to_count  = state->conc_to_count;
+  forward_rxn_likelihoods = state->ode_forward_lklhds;
+  reverse_rxn_likelihoods = state->ode_reverse_lklhds;
+  counts                  = state->ode_counts;
+  if (lfp) {
+    if (origin <= 2) {
+      fprintf(lfp," time = %le, h = %le, step = %d, origin = %d\n"
+	      "Species\tindex\tconcentration\t    count    \t    flux\n",
+	      time,h,step,origin);
+    } else {
+      fprintf(lfp," time = %le, h = %le, step = %d, origin = %d\n"
+	      "Species\tindex\tconcentration\t    count    \t    delta_conc\n",
+	      time,h,step,origin);
+    }
+    for (i=0;i<ny;i++) {
+      counts[i] = concs[i] * conc_to_count[i];
+    }
+    success = update_rxn_likelihoods(state,counts,forward_rxn_likelihoods,
+				     reverse_rxn_likelihoods);
+    for (i=0;i<ny;i++) {
+      fprintf(lfp,"%s\t%d\t%le\t%le\t%le\n",
+	      (char*)&molecules_text[molecule->string],
+	      i,concs[i],counts[i],dconcs[i]);
+      molecule   += 1; /* Caution address arithmetic here. */
+    }
+    if (success) {
+      fprintf(lfp,"rxn_n\tfrwrd_lklhd\trvrs_lklhd\n");
+      for (i=0;i<nrxns;i++) {
+	fprintf(lfp,"%d\t%le\t%le\n",i,forward_rxn_likelihoods[i],
+		reverse_rxn_likelihoods[i]);
+      }
+      fflush(lfp);
+    }
+  }
+  return(success);
+}
