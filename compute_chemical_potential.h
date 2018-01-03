b428d5be495730eb4a42e27a06878c9273f1a15f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:35 -0800

    r2329 | dbaxter | 2013-06-07 07:58:44 -0700 (Fri, 07 Jun 2013) | 3 lines
    Header for compute_chemical_potential.c
    A    compute_chemical_potential.h

---
+++ compute_chemical_potential.h
@@ -0,0 +1,36 @@
+/* compute_chemical_potential.h
+******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+The terms and conditions of the License may be found in
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+/*
+ * compute_chemical_potential.h
+ *
+ *  Created on: May 2, 2013
+ *      Author:  Dennis G. Thomas
+ *      Modified by Doug Baxter on 5/31, added header comment.
+*/     
+
+#ifndef _COMPUTE_CHEMICAL_POTENTIAL_H_
+#define _COMPUTE_CHEMICAL_POTENTIAL_H_ 1
+
+extern int compute_chemical_potential(struct formation_energy_struct *formation_energies);
+
+#endif /* _COMPUTE_CHEMICAL_POTENTIAL_H_ */
