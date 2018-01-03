37806fa5d4c96c2771b476bbaabae899b98571de
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:17 -0800

    r1946 | dbaxter | 2012-12-12 09:48:24 -0800 (Wed, 12 Dec 2012) | 4 lines
    Modified ordering of fields for biocellion interface.
    Added some identifying fields to state for biocellion interface.
    M    state_struct.h

--- state_struct.h
+++ state_struct.h
@@ -1,6 +1,79 @@
+/* 
+  So we will lay out the state struct a little differently.
+  We want to flatten the state structure, and make it self describing.
+  At the beginning there will be a set of scalars describing sizes
+  and constansts to be used in the code, followed by a partitioning
+
+  Let us make the following partitions
+  1, for the size parameters and floating point constants.
+
+  2. for arrays/structs that are both inputs and outputs (4 of these)
+     and 2 scalars, dg_forward and entropy for the whole system
+     current_concentrations,
+     bndry_flux_concs,
+     vgrng_state
+     vgrng2_state
+
+
+ 
+  3. for arrays/structs that are strictly inputs (there are 6 of these
+     not counting string pointers).
+     reactions,
+     reactions_matrix,
+     sorted_molecules,
+     ke,
+     activities,
+     sorted_compartments
+
+
+  4. for work space arrays and structs.
+     unsorted_molecules,
+     unsorted_compartments.
+     future_concentrations,
+     rxn_fire,
+     cmpts_map,
+     no_op_likelihood,
+     forward_rxn_likelihood,
+     reverse_rxn_likelihood,
+     forward_rxn_log_likelihood_ratio,
+     reverse_rxn_log_likelihood_ratio,
+     rxn_likelihood_ps,
+     rxn_view_likelihoods,
+     rev_rxn_view_likelihoods,
+     dg0s,
+     free_energy,
+
+
+  a partition would have a number of entities as an 8 byte first element,
+  a total length for the second 8 byte element followed by 
+  offset, length pairs for each of the entities. 
+  followed by the entities.
+
+  So the super partition would look like 
+
+    4L 
+    total_length_in_8_byte_words_of_the_state_struct
+    10L
+    total_length_in_8byte_words_of_partition_1_size_params_and_fp_constants
+
+    10 + total_length_in_8byte_words_of_partition_1_size_params_and_fp_constants    
+    total_length_in_8byte_words_of_partition_2_2way_data
+
+    10 + total_length_in_8byte_words_of_partition_1_size_params_and_fp_constants    +     total_length_in_8byte_words_of_partition_2_2way_data
+    total_length_in_8byte_words_of_partition_3_input_data
+
+    10 + total_length_in_8byte_words_of_partition_1_size_params_and_fp_constants    +     total_length_in_8byte_words_of_partition_2_2way_data
+    +     total_length_in_8byte_words_of_partition_3_input_data
+    total_length_in_8byte_words_of_partition_4_work_space.
+
+
+    Partition 1 is a little different in that all the quantities are
+    scalars, we will make them all 8 byte quantites.
+    First quantity will be num_scalars (includes itself).
+*/
 /* state_struct.h 
 *******************************************************************************
-Boltzmann
+Boltsman
 
 Pacific Northwest National Laboratory, Richland, WA 99352.
 
@@ -23,19 +96,102 @@ specific language governing permissions and limitations under the License.
 #ifndef __STATE_STRUCT__
 #define __STATE_STRUCT__ 1
 struct state_struct {
-  struct rxn_struct *reactions; /* sizeof(rxn_struct) * number of reactions */
-  struct rxn_matrix_struct *reactions_matrix; 
+  int64_t agent_type;
+  int64_t state_length;         	
+  int64_t thread_id;
+  double  x_coord;
+  double  y_coord;
+  double  z_coord;
+  int64_t num_partitions;       	
+  int64_t scalars_length;       	
+  int64_t scalars_offset;       	
+  int64_t two_way_data_length;  	
+  int64_t two_way_data_offset;  	
+  int64_t incoming_data_length; 	
+  int64_t incoming_data_offset; 	
+  int64_t work_space_length;    	
+  int64_t work_space_offset;    	
+  int64_t num_scalars;          	
+  int64_t number_reactions;     	
+  int64_t number_molecules;            
+  int64_t unique_molecules;     	
+  int64_t use_activities;       	
+  int64_t molecules_or_conc;      
+  int64_t warmup_steps;           
+  int64_t record_steps;           
+  int64_t print_output;
+  int64_t number_compartments;  	
+  int64_t unique_compartments;  	
+
+  int64_t align_len;
+  int64_t align_mask;
+  int64_t max_filename_len;       
+  int64_t max_param_line_len;     
+  int64_t num_rxn_file_keywords;  
+  int64_t free_energy_format;     
+  int64_t rxn_view_freq;          
+  int64_t rxn_view_hist_lngth;    
+  int64_t lklhd_view_freq;        
+  int64_t conc_view_freq;         
+  int64_t reaction_file_length;   
+  int64_t rxn_title_space;        
+  int64_t pathway_space;          
+  int64_t compartment_space;      
+  int64_t molecules_space;        
+  int64_t num_fixed_concs;      	
+  int64_t usage;                  
+  int64_t max_molecule_len;     	
+  int64_t min_molecule_len;     	
+  int64_t max_compartment_len;  	
+  int64_t min_compartment_len;  	
+
+
+  double  ideal_gas_r;
+  double  temp_kelvin;
+  double  ph;
+  double  ionic_strength;
+  double  rt;
+  double  m_r_rt;
+  double  m_rt;
+  double  cal_gm_per_joule;
+  double  joule_per_cal_gm;
+  double  default_initial_conc;
+  double  dg_forward;
+  double  entropy;
+
+  /* two way data (modified) */
+  double  *dg_forward_p; /* scalar */        /* 8 */             
+  double  *entropy_p;    /* scalar */        /* 16 */             
+  double  *current_concentrations; /* len = unique_molecules */
+  double  *bndry_flux_concs;       /* len = unique_molecules */
+  struct  vgrng_state_struct *vgrng_state; /* len = 13 */
+  struct  vgrng_state_struct *vgrng2_state;/* len = 13 */ 
+  /* 
+    2*sizeof(double) + unique_molecules * 2 * sizeof(double) 
+    + 2 * sizeof(vgrng_state_struct)
+  */
+  /*
+    Incoming data not modified, depends only on agent type.
+    (2*number_reactions + unique_molecules) * sizeof(double)
+  */
+  double  *dg0s;      /* len = number_reactions */
+  double  *ke;        /* len = number_reactions */
+  double  *activities; /* len = unique_molecules */
+  /* sizeof(rxn_struct) * number of reactions */
+  struct rxn_struct *reactions; 
   /* 
      ((number_reactions + 1) * sizeof(int64_t)) +
-     number_molecules * ((3*sizeof(int64_t)) + sizeof(char*))
+     number_molecules * (4*sizeof(int64_t))
   */
-  struct molecules_matrix_struct *molecules_matrix;
-  struct istring_elem_struct *unsorted_molecules; /* 24 * number_molecules */
-  struct istring_elem_struct *sorted_molecules;   /* 24 * number_molecules */
-  struct istring_elem_struct *unsorted_cmpts;    /* 24 * number_compartments */
-  struct istring_elem_struct *sorted_cmpts;      /* 24 * number_compartments */
-  struct vgrng_state_struct  *vgrng_state; /* 13 * 8 = 104 bytes */
-  struct vgrng_state_struct  *vgrng2_state; /* 13 * 8 = 104 bytes */
+  struct rxn_matrix_struct *reactions_matrix; 
+  /* sizeof(istring_elem_struct) * unique_molecules */
+  struct istring_elem_struct *sorted_molecules;   
+  /* sizeof(istring_elem_struct) * unique_compartments */
+  struct istring_elem_struct *sorted_cmpts;
+  /*
+    Auxilliary data read in by boltzmann_init, not needed by
+    boltzman_run unless printing is enabled.
+  */
   char *params_file;      /* max_filename_len */
   char *reaction_file;    /* max_filename_len */
   char *init_conc_file;   /* max_filename_len */
@@ -49,123 +205,59 @@ struct state_struct {
   char *rxn_view_file;    /* max_filename_len */
   char *bndry_flux_file;  /* max_filename_len */
   char *output_dir;       /* max_filename_len */
-  char *rxn_buffer;       /* rxn_buff_len */
-  char *param_buffer;     /* max_param_line_len */
-  char *param_key;        /* max_param_line_len>>1 */
-  char *param_value;      /* max_param_line_len>>1 */
-  char *rxn_file_keyword_buffer;  /* 144 chars */
-  char *rxn_title_text;           /* rxn_title_space */
-  char *pathway_text;             /* pathway_space   */
-  char *compartment_text;         /* compartment_space */
-  char *molecules_text;           /* molecules_space */
-  char *molecule_name;            /* max_molecule_len */
-  char *compartment_name;         /* max_compartment_len */
-  char *raw_molecules_text;       /* molecules_space */
-  char **rxn_file_keywords;       /* 12 * sizeof(char*) */
-  int64_t *rxn_file_keyword_lengths; /* 12 */
-  int64_t *transpose_work;           /* unique_molecules+1 */
-  int64_t *compartment_ptrs;         /* unique_compartments + 1 */
-  int64_t reaction_file_length;
-  int64_t print_output;
-  int64_t align_len;
-  int64_t align_mask;
-  int64_t max_filename_len;
-  int64_t max_param_line_len;
-  int64_t rxn_buff_len;
-  int64_t rxn_title_len;
-  int64_t pathway_len;
-  int64_t compartment_len;
-  int64_t molecules_len;
-  int64_t rxn_title_space;
-  int64_t pathway_space;
-  int64_t compartment_space;
-  int64_t molecules_space;
-  /* These are just local variables used in parse_reactions_file.
-  int64_t rxn_title_pos;
-  int64_t pathway_pos;
-  int64_t compartment_pos;
-  int64_t molecules_pos;
-  int64_t mixed_case_pos;
+  /* 2*sizeof(double) + 
+     ((unique_molecules * 3) + 2*number_reactions) * sizeof(double)+
+     2*sizeof(vgrng_struct)  + 
+     (sizeof(rxn_struct) * number of reactions) + 
+     sizeof(rxn_matrix_struct) +
+     ((number_reactions + 1) * sizeof(int64_t)) +
+     (number_molecules * ((3*sizeof(int64_t)) + sizeof(char*))) +
+     (sizeof(istring_elem_struct) * (unique_molecules+unqiue_compartments))) +
+     (num_state_files * max_filename_len) +
+     rxn_title_space + 	
+     pathway_space + 
+     compartment_space + 
+     molecules_space +
+     
+     max(molecules_space+ ,
+     (sizeof(istring_elem_struct) * (number_molecules+number_compartments))) +
+	     
+     ((unqiue_molecules + 6*number_reactions + 1)*sizeof(double)
+     + print_output ? 
+     ((rxn_view_hist_length *(2*number_reactions+1)) * sizeof(double)
+     + ((2*number_reactions+2)*sizeof(int))) : 0))
   */
-  int64_t usage;
-  double  ideal_gas_r;
-  double  temp_kelvin;
-  double  ph;
-  double  ionic_strength;
-  double  rt;
-  double  m_r_rt;
-  double  m_rt;
-  double  cal_gm_per_joule;
-  double  joule_per_cal_gm;
-  double  default_initial_conc;
-  double  dg_forward;
-  double  entropy;
+  char *rxn_title_text;   /* rxn_title_space */
+  char *pathway_text;     /* pathway_space   */
+  char *compartment_text; /* compartment_space */
+  char *molecules_text;   /* molecules_space */
   /*
-  double  small_nonzero;
+    Workspace only.
   */
-  double  *dg0s;                   /* number_reactions */
-  double  *ke;                     /* number_reactions */
-  double  *activities;             /* number_reactions */
-  double  *current_concentrations; /* unique_molecules */
-  double  *bndry_flux_concs;       /* unique_molecules */
+  struct istring_elem_struct *unsorted_molecules;
+  struct istring_elem_struct *unsorted_cmpts;
+  int64_t *compartment_ptrs;
+  int64_t *rxn_file_keyword_lengths;
+  char    **rxn_file_keywords;
+  char    *rxn_file_keyword_buffer; /* 144 */
+  char    *param_buffer; /*  2* max_param_line_len */
+  char    *raw_molecules_text; /* molecules_space */
   double  *future_concentrations;  /* unique_molecules */
   double  *free_energy;            /* number_reactions */
   double  *forward_rxn_likelihood; /* number_reactions */
   double  *reverse_rxn_likelihood; /* number_reactions */
-  double  *rxn_likelihood_ps;      /* number_ractions + 1 */
+  double  *rxn_likelihood_ps;      /* number_reactions + 1 */
   double  *forward_rxn_log_likelihood_ratio; /* number_reactions */
   double  *reverse_rxn_log_likelihood_ratio; /* number_reactions */
-  double  *rxn_view_likelihoods;    /* rxn_view_hist_length * number_reactions */
-  double  *rev_rxn_view_likelihoods; /* rxn_view_hist_length * number_reactions */
+  /*
+    Workspace only if printing. (debugging);
+  */
   double  *no_op_likelihood;       /* rxn_view_hist_length */
+  /* rxn_view_hist_length * number_reactions */
+  double  *rxn_view_likelihoods;    
+  /* rxn_view_hist_length * number_reactions */
+  double  *rev_rxn_view_likelihoods; 
   int  *rxn_fire;                  /* (number_reactions * 2) + 2*/
-
-  int64_t  number_reactions;
-  /* free energy_format, 0 for none, 1 for negative log likelihoods,
-     2 for KJ/mol, 3 for Kcal/mol.
-  */
-  int64_t  free_energy_format;
-  
-
-  int64_t  number_compartments;
-  int64_t  unique_compartments;
-
-  int64_t  number_molecules;
-  int64_t  unique_molecules;
-
-  int64_t  max_molecule_len;
-  int64_t  min_molecule_len;
-
-  int64_t  max_compartment_len;
-  int64_t  min_compartment_len;
-
-  int64_t  molecules_or_conc;
-  int64_t  num_rxn_file_keywords;
-
-  int64_t  warmup_steps;
-  int64_t  record_steps;
-
-  /*
-    reaction view history frequency.
-    Default value is 0: don't display.
-  */
-  int64_t  rxn_view_freq;
-  /*
-    Likelihood transpose history length:
-    rxn_view_hist_lngth = 
-       floor((record_steps + rxn_view_freq - 1)/rxn_view_freq);
-  */
-  int64_t  rxn_view_hist_lngth;
-
-  int64_t  lklhd_view_freq;
-  int64_t  conc_view_freq;
-
-  int64_t  num_fixed_concs;
-  int64_t  use_activities;
-
-#ifdef TIMING_ON
-  struct timing_struct timing_data;
-#endif
   FILE *rxn_fp;
   FILE *conc_fp;
   FILE *out_fp;
@@ -176,6 +268,10 @@ struct state_struct {
   FILE *rxn_view_fp;
   FILE *bndry_flux_fp;
   FILE *lfp;
+  /*
+    Instrumentation.     
+  */
+  struct timing_struct *timing_data;
 }
 ;
 #endif
