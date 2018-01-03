4863973af4e3bb88f12ae5de7769f12769c6c6c5
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:12 -0800

    r1788 | dbaxter | 2012-10-08 11:21:53 -0700 (Mon, 08 Oct 2012) | 4 lines
    Splitting the update of bndry_fluxes out of the rxn_conc_update routine
    so as to only fire the flux update after a reaction has been chosen.
    A    bndry_flux_update.c

---
+++ bndry_flux_update.c
@@ -0,0 +1,87 @@
+/* bndry_flux_update.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "boltzmann_structs.h"
+
+#include "bndry_flux_update.h"
+
+int bndry_flux_update(int rxn_no, int direction,
+		      struct state_struct *state) {
+  /*
+    Compute the change in boundary fluxes for reaction rxn_no,
+    in the forward direction if direction > 0, otherwise in
+    the reverse direction.
+    Called by choose_rxn.
+    Calls:
+  */
+  struct istring_elem_struct *sorted_molecules;
+  struct istring_elem_struct *molecule;
+  struct rxn_matrix_struct   *rxns_matrix;
+  double *bndry_flux_concs;
+  int64_t *rxn_ptrs;
+  int64_t *molecules_indices;
+  int64_t *coefficients;
+
+  int     nu_molecules;
+  int     success;
+
+  int     j;
+  int     k;
+			 
+  success           = 1;
+  bndry_flux_concs  = state->bndry_flux_concs;
+  nu_molecules      = state->unique_molecules;
+  sorted_molecules  = state->sorted_molecules;
+  rxns_matrix       = state->reactions_matrix;
+  rxn_ptrs          = rxns_matrix->rxn_ptrs;
+  molecules_indices = rxns_matrix->molecules_indices;
+  coefficients      = rxns_matrix->coefficients;
+  /*
+    The following could be done with an memmove
+  */
+  if (direction > 0) {
+    for (j=rxn_ptrs[rxn_no];j<rxn_ptrs[rxn_no+1];j++) {
+      k = molecules_indices[j];
+      molecule = (struct istring_elem_struct *) &sorted_molecules[k];
+      if (molecule->variable == 0) {
+        bndry_flux_concs[k] += (double)coefficients[j];
+      }
+    } 
+  } else {
+    for (j=rxn_ptrs[rxn_no];j<rxn_ptrs[rxn_no+1];j++) {
+      k = molecules_indices[j];
+      molecule = (struct istring_elem_struct *) &sorted_molecules[k];
+      if (molecule->variable == 0) {
+        bndry_flux_concs[k] -= (double)coefficients[j];
+      }
+    }
+  }
+  return(success);
+}
