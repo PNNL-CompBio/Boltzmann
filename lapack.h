2395e893cbfa44ee00443b73b2c08a35a8d581ca
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:26 -0800

    r3654 | dbaxter | 2016-11-23 09:04:45 -0800 (Wed, 23 Nov 2016) | 3 lines
    Header file for the lapack routines.
    A    lapack.h

---
+++ lapack.h
@@ -0,0 +1,10 @@
+#ifndef _LAPAPCK_H_
+#define _LAPAPCK_H_ 1
+#include "lsame.h"
+#include "dtrsm.h"
+#include "dlaswp.h"
+#include "dgetrf2.h"
+#include "dgetrf.h"
+#include "dgetrs.h"
+#include "dgemm.h"
+#endif
