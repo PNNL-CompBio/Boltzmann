e31eef5848e82f41f38b37bff2ad7301e383f15e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:18 -0800

    r4912 | dbaxter | 2017-11-27 16:55:39 -0800 (Mon, 27 Nov 2017) | 3 lines
    Documetation correction.
    M    lr6_approximate_delta_concs.c

--- lr6_approximate_delta_concs.c
+++ lr6_approximate_delta_concs.c
@@ -319,7 +319,7 @@ int lr6_approximate_delta_concs(struct state_struct *state,
       } else {
 	flux[i] = 0.0;
       }
-      molecule += 1; /* Caution address arithmetic heare. */
+      molecule += 1; /* Caution address arithmetic here. */
     } /* end for (i...) */
   } /* end if success */
 #ifdef DBG
