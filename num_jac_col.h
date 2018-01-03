faa4d56221fc8d6ae1487fa88bde8564d66eedf2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:20 -0800

    r3563 | dbaxter | 2016-08-31 16:38:42 -0700 (Wed, 31 Aug 2016) | 3 lines
    Header file for num_jac_col.c
    A    num_jac_col.h

---
+++ num_jac_col.h
@@ -0,0 +1,19 @@
+#ifndef _NUM_JAC_COL_H_
+#define _NUM_JAC_COL_H_ 1
+extern int num_jac_col(struct state_struct *state,
+	   	       int ny, int j,
+		       double *y,
+		       double *f,
+		       double delj,
+		       double threshj,
+		       double *y_counts,
+		       double *fdel,
+		       double *fdiff,
+		       double *forward_rxn_likelihoods,
+		       double *reverse_rxn_likelihoods,
+		       double *dfdy_colj,
+		       double *absfdiffmax_p,
+		       double *absfvaluerm_p,
+		       double *absfdelrm_p,
+		       double *infnormdfdy_colj_p);
+#endif
