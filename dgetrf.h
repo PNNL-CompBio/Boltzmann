d704dbb0827bf40f0f97c8794fd928681fb4a71d
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:24 -0800

    r4979 | dbaxter | 2017-12-13 17:50:48 -0800 (Wed, 13 Dec 2017) | 4 lines
    Added const for integers that are inputs.n
    matching cvodes decls.
    M    dgetrf.h

--- dgetrf.h
+++ dgetrf.h
@@ -1,4 +1,4 @@
 #ifndef _DGETRF_H_
 #define _DGETRF_H_ 1
-extern void dgetrf_(int *m, int *n, double *a,int *lda, int *ipivot, int *info);
+extern void dgetrf_(const int *m, const int *n, double *a, const int *lda, int *ipivot, int *info);
 #endif
