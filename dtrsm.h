570b269a1714aaac85529c07fd38e4055e68cf35
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:30 -0800

    r3712 | dbaxter | 2016-12-08 14:44:15 -0800 (Thu, 08 Dec 2016) | 3 lines
    Added _ for compatability with fortran library interfaces.
    M    dtrsm.h

--- dtrsm.h
+++ dtrsm.h
@@ -1,6 +1,6 @@
 #ifndef _DTRSM_H_ 
 #define _DTRSM_H_ 1
-extern void dtrsm(char *side_p, char *uplo_p, char *transa_p, char *diag_p,
+extern void dtrsm_(char *side_p, char *uplo_p, char *transa_p, char *diag_p,
 		  int *m_p, int *n_p, double *alpha_p, double *a,
 		  int *lda_p, double *b, int *ldb_p);
 #endif
