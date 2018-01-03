eb1e02efead6f85e860ad56e7b14f66f8e6f2c0e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:52 -0800

    r4709 | dbaxter | 2017-11-07 18:07:37 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for crs_column_sort_rows.c
    A    crs_column_sort_rows.h

---
+++ crs_column_sort_rows.h
@@ -0,0 +1,11 @@
+#ifndef _CRS_COLUMN_SORT_ROWS_H_
+#define _CRS_COLUMN_SORT_ROWS_H_ 1
+extern void crs_column_sort_rows(int m,
+				 int n,
+				 double *a, 
+				 int *ia, 
+				 int *ja, 
+				 double *at, 
+				 int *iat,
+				 int *jat);
+#endif
