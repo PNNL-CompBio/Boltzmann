d13c0bec847dc60b83794f99b182946ca7a22294
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:07 -0800

    r4223 | dbaxter | 2017-06-14 09:42:27 -0700 (Wed, 14 Jun 2017) | 3 lines
    Removed flattend_state argument.
    M    run_init.h

--- run_init.h
+++ run_init.h
@@ -1,4 +1,4 @@
 #ifndef _RUN_INIT_H_
 #define _RUN_INIT_H_ 1
-extern int run_init(struct state_struct *state, struct state_struct **flattened_state);
+extern int run_init(struct state_struct *state);
 #endif
