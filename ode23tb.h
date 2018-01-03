7d704afe0a632b4e2d9318fa1fc664971dd079ae
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:25 -0800

    r3646 | dbaxter | 2016-11-22 15:07:27 -0800 (Tue, 22 Nov 2016) | 3 lines
    Added normcontrol argument.
    M    ode23tb.h

--- ode23tb.h
+++ ode23tb.h
@@ -1,5 +1,6 @@
 #ifndef _ODE23TB_H_
 #define _ODE23TB_H_ 1
 extern int ode23tb (struct state_struct *state, double *counts,
-		    double htry, int nonnegative);
+		    double htry, int nonnegative,
+		    int normcontrol);
 #endif
