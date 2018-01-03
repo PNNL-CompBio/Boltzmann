6fb8b49e9063e679b7ed88d848bda09c20eca855
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:50 -0800

    r4685 | dbaxter | 2017-11-07 17:36:13 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for dcrsng_mag_sort.c
    A    dcrsng_mag_sort.h

---
+++ dcrsng_mag_sort.h
@@ -0,0 +1,8 @@
+#ifndef _DCRSNG_MAG_SORT_H_
+#define _DCRSNG_MAG_SORT_H_ 1
+extern void dcrsng_mag_sort(int n,
+			    double *row,
+			    int *index,
+			    double *srow,
+			    int *sindex);
+#endif
