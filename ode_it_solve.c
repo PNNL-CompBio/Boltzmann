90585cde07ec848b391b0e8196be48dc89da3f48
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:29 -0800

    r3694 | dbaxter | 2016-12-08 14:24:03 -0800 (Thu, 08 Dec 2016) | 4 lines
    Completely reworked use blas and vec_* routines to ease debugging, but also
    should improve performance.
    M    ode_it_solve.c

--- ode_it_solve.c
+++ ode_it_solve.c
@@ -5,6 +5,10 @@
 #include "ce_approximate_fluxes.h"
 #include "blas.h"
 #include "lapack.h"
+#include "vec_abs.h"
+#include "vec_div.h"
+#include "vec_mul.h"
+#include "vec_max.h"
 
 #include "ode_it_solve.h"
 int ode_it_solve(struct state_struct *state,
@@ -14,7 +18,8 @@ int ode_it_solve(struct state_struct *state,
 		 double *y,
 		 double *z,
 		 double *del,
-		 double *znew,
+		 double *rhs,       
+		 double *scratch,   /* used to compute candnrm */
 		 double *y_count,
 		 double *forward_rxn_likelihoods,
 		 double *reverse_rxn_likelihoods,
@@ -26,7 +31,7 @@ int ode_it_solve(struct state_struct *state,
 		 int    *iter_count_p) {
   /*
     Iteratively solve nonlinear  Mz = h*f(t,v+d*z) where y = v+d*z.
-    del, znew, y_count, forward_rxn_likelihoods, 
+    del, rhs, scratch,  y_count, forward_rxn_likelihoods, 
     and reverse_reaction_likeklihoods are scratch vectors of length ny.    
     computes a result in z, and also modifies y.
     miter and ipivot contain the LU factorization of I-d*h*dfdy,
@@ -45,6 +50,7 @@ int ode_it_solve(struct state_struct *state,
   double newnrm;
   double oldnrm;
   double kappa_rtol;
+  double tenth_kappa_rtol;
   double cap;
   double del_scale;
   double cand_rate;
@@ -53,6 +59,12 @@ int ode_it_solve(struct state_struct *state,
   double *dbl_ptr;
   double yi;
   double zi;
+  double hundred_eps;
+  double local_h;
+  double local_d;
+  double minus_one;
+  double one;
+  double rate_ratio;
 
   int64_t eps_hex;
   int itfail;
@@ -71,7 +83,11 @@ int ode_it_solve(struct state_struct *state,
   int origin;
 
   int iter_count;
+  int inc1;
+
+  int exit_not;
   int padi;
+
   char  trans_chars[8];
   char  *trans;
   FILE *lfp;
@@ -79,157 +95,235 @@ int ode_it_solve(struct state_struct *state,
 /*
 */
 #define DBG 1
-  max_iter     = 5;
-  kappa        = 0.5;
-  itfail       = 0;
-  base_rxn     = state->base_reaction;
-  conc_to_count = state->conc_to_count;
-  ny            = state->nunique_molecules;
-  lfp           = state->lfp;
-  kappa_rtol    = kappa * rtol;
-  eps_hex       = 0x3CB0000000000000L;
-  dbl_ptr       = (double *)&eps_hex;
-  eps           = *dbl_ptr;
-  minnrm = 0.0;
-  rate   = *rate_p;
-  iter_count = *iter_count_p;
+  max_iter     	   = 5;
+  kappa        	   = 0.5;
+  itfail       	   = 0;
+  base_rxn     	   = state->base_reaction;
+  conc_to_count    = state->conc_to_count;
+  ny               = state->nunique_molecules;
+  lfp              = state->lfp;
+  kappa_rtol       = kappa * rtol;
+  tenth_kappa_rtol = 0.05 * rtol;
+  eps_hex          = 0x3CB0000000000000L;
+  dbl_ptr          = (double *)&eps_hex;
+  eps              = *dbl_ptr;
+  inc1             = 1;
+  rate             = *rate_p;
+  hundred_eps      = 100.0 * eps;
+  local_h          = h;
+  local_d          = d;
+  minus_one        = -1.0;
+  one              = 1.0;
+  /*
+    scratch <- |y|
+  */
+  vec_abs(&ny,scratch,y);
+  /*
+    scratch <- 100 * eps * |y|
+  */
+  dscal_(&ny,&hundred_eps,scratch,&inc1);
+  /*
+    scratch <- scratch ./ wt
+  */
+  vec_div(&ny,scratch,scratch,wt);
+  /*
+    minnrm = 100 * eps * norm(y ./ wt,inf);
+  */
+  /*
   for (i=0;i<ny;i++) {
     cand_nrm = 100 * eps * fabs(y[i])/wt[i];
     if (cand_nrm > minnrm) {
       minnrm = cand_nrm;
     }
   }
+  */
+  minnrm = scratch[idamax_(&ny,scratch,&inc1)-1];
+  iter_count = 0;
   trans_chars[0] = 'N';
   trans = &trans_chars[0];
   /*
   oldnrm = minnrm;
   */
-  for (iter = 0; iter<max_iter;iter++) {
+  exit_not = 1;
+  for (iter = 0; ((iter<max_iter) && exit_not);iter++) {
+    iter_count = iter + 1;
+    /*
+      Compute counts to be used in computing concentration derivative.
     for (i=0;i<ny;i++) {
       y_count[i] = y[i]*conc_to_count[i];
     }
+      y_count <- y .* conc_to_count
+    */
+    vec_mul(&ny,y_count,y,conc_to_count);
+    /*
+      rhs <- y', approximate fluxes.
+    */
+    
     flux_scaling = compute_flux_scaling(state,y);
     ce_approximate_fluxes(state,y_count,forward_rxn_likelihoods, 
 		       reverse_rxn_likelihoods,
-		       znew,flux_scaling,base_rxn);
+		       rhs,flux_scaling,base_rxn);
+    /*
 #ifdef DBG
     if (lfp) {
       fprintf(lfp,"ode_it_solve: after call to ce_approximate_fluxes:\n");
       nsteps = 0;
       origin = 7;
-      print_concs_fluxes(state,ny,znew,y,y_count,
+      print_concs_fluxes(state,ny,rhs,y,y_count,
 			 forward_rxn_likelihoods,
 			 reverse_rxn_likelihoods,t,h,nsteps,origin);
     }
 #endif    
-
+    */
+    /*
     approximate_fluxes(state,y_count,forward_rxn_likelihoods, 
 		       reverse_rxn_likelihoods,
-		       znew,flux_scaling,base_rxn);
-
+		       rhs,flux_scaling,base_rxn);
+    */
+    /*
 #ifdef DBG
     if (lfp) {
       fprintf(lfp,"ode_it_solve: after call to approximate_fluxes:, flux_scaling = %le\n",flux_scaling);
       nsteps = 0;
       origin = 7;
-      print_concs_fluxes(state,ny,znew,y,y_count,
+      print_concs_fluxes(state,ny,rhs,y,y_count,
 			 forward_rxn_likelihoods,
 			 reverse_rxn_likelihoods,t,h,nsteps,origin);
     }
-#endif    
-    newnrm = 0.0;
-    for (i=0;i<ny;i++) {
-      znew[i] = h * znew[i];
-      del[i]  = znew[i] - z[i];
-    }
+#endif
+    */
+    /*
+      for (i=0;i<ny;i++) {
+        rhs[i] = h * rhs[i];
+        del[i]  = rhs[i] - z[i];
+      }
+      rhs <- y' * h
+    */
+    dscal_(&ny,&local_h,rhs,&inc1);
+    /*
+      rhs <- y' * h - z
+      could use a daymx (y<- a*y-x) routine here with y = rhs, a = h, and x = z)
+    */
+    daxpy_(&ny,&minus_one,z,&inc1,rhs,&inc1);
+    /*
+      del <- rhs;
+    */
+    dcopy_(&ny,rhs,&inc1,del,&inc1);
+    /*
+      Solve Miter * del = rhs for del
+    */
     nrhs = 1;
-    dgetrs(trans,&ny,&nrhs,miter,&ny,ipivot,del,&ny,&info);
+    dgetrs_(trans,&ny,&nrhs,miter,&ny,ipivot,del,&ny,&info);
     if (info != 0) {
       if (lfp) {
 	fprintf (lfp,"ode_it_solve: Error nonzero return code from dgetrs was %s\n",info);
 	fflush(lfp);
       }
       itfail = 1;
-      break;
-    }
-    for (i=0;i<ny;i++) {
-      zi      = z[i];
-      z[i]    = zi + del[i];
-      yi      = y[i];
-      y[i]    = yi + d * del[i];
-      /*
-	Here again we need to not let y be negative.
-	We do assume that y[i] is nonnegative on input,
-	maybe should test that hypothesis.
-      */
-      /*
-      if (y[i] < 0.0) {
-	y[i] = .5*yi;
-	del[i] = -(.5 * yi) / d;
-	z[i] = zi + del[i];
-      }
-      */
-      del_scale = fabs(y[i]);
-      if (wt[i] > del_scale) {
-	del_scale = wt[i];
-      }
-      cand_nrm = fabs(del[i])/del_scale;
-      if (cand_nrm > newnrm) {
-	newnrm = cand_nrm;
-      }
-    } /* end for (i...) */
-    if (newnrm < minnrm) {
-      iter_count = iter + 1;
-      break;
+      exit_not = 0;
     } else {
-      if (iter == 0) {
-	if (rate > 0) {
-	  errit = newnrm * rate/(1.0 - rate);
-	  if (errit <= .1 * kappa_rtol) {
-	    iter_count = iter+1;
-	    break;
-	  } 
-	} else {
-	  rate = 0;
-	}
+      /*
+	dgetrs solve succeeded.
+	vec_add could be used here.
+	z <- z + del;
+      */
+      daxpy_(&ny,&one,del,&inc1,z,&inc1);
+      /*
+	y <- y + d * del
+      */
+      daxpy_(&ny,&d,del,&inc1,y,&inc1);
+      /*
+      newnrm = norm(del ./ max(wt,abs(y)),inf);
+      scratch <- |y|
+      */
+      vec_abs(&ny,scratch,y);
+      /*
+	scratch <- max (|y|,wt))
+      */
+      vec_max(&ny,scratch,scratch,wt);
+      /*
+	scratch <- del ./  max(|y|,st)
+      */
+      vec_div(&ny,scratch,del,scratch);
+      newnrm = fabs(scratch[idamax_(&ny,scratch,&inc1)-1]);
+      /*
+      newnrm = 0.0;
+      for (i=0;i<ny;i++) {
+        zi      = z[i];
+        z[i]    = zi + del[i];
+        yi      = y[i];
+        y[i]    = yi + d * del[i];
+
+        del_scale = fabs(y[i]);
+        if (wt[i] > del_scale) {
+      	del_scale = wt[i];
+        }
+        cand_nrm = fabs(del[i])/del_scale;
+        if (cand_nrm > newnrm) {
+      	newnrm = cand_nrm;
+        }
+      } // end for (i...) 
+      */
+      if (newnrm <= minnrm) {
+	exit_not = 0;
       } else {
-	if (newnrm > (0.9 * oldnrm)) {
-	  itfail = 1;
-	  iter_count = iter+1;
-	  break;
-	} else {
-	  p9_old_rate = 0.9 * rate;
-	  cand_rate   = newnrm/oldnrm;
-	  if (p9_old_rate > cand_rate) {
-	    cand_rate = p9_old_rate;
-	  }
-	  rate = cand_rate;
-	  errit = newnrm * rate/(1.0 - rate);
-	  if (errit <= kappa_rtol) {
-	    iter_count = iter+1;
-	    break;
-	  } else {
-	    if (iter == max_iter) {
-	      itfail = 1;
-	      iter_count = iter+1;
-	      break;
-	    } else {
-	      cap = 1.0;
-	      for (i=iter;i<max_iter-1;i++) {
-		cap = cap * rate;
-	      }
-	      if (kappa_rtol < errit*cap) {
-		itfail = 1;
-		iter_count = iter+1;
-		break;
-	      }
+	if (iter == 0) {
+	  /*
+	    First iteration.
+	  */
+	  if (rate > 0.0) {
+	    errit = newnrm * rate / ( 1.0 - rate );
+	    if (errit <= tenth_kappa_rtol) {
+	      exit_not = 0;
 	    }
+	  } else {
+	    rate = 0.0;
 	  }
-	}
-      }
-    }
+	} else {
+	  /*
+	    Not first iteration.
+	    if newnrm has grown by more than 10 % fail.
+	  */
+	  if (newnrm > 0.9*oldnrm) {
+	    itfail = 1;
+	    exit_not = 0;
+	  } else {
+	    /*
+	      newnrm not too large.
+	    */
+	    rate = 0.9 * rate;
+	    errit = newnrm/oldnrm;
+	    rate = (rate > errit) ? rate : errit;
+	    errit = newnrm * (rate/ (1.0 - rate));
+	    if (errit <= kappa_rtol) {
+	      /*
+		Converged.
+	      */
+	      exit_not = 0;
+	    } else {
+	      if (iter == (max_iter - 1)) {
+		/* 
+		   last iteration and not converged. 
+		*/
+		itfail   = 1;
+		exit_not = 0;
+	      } else {
+		cap = 1.0;
+		for (i=iter;i<max_iter-1;i++) {
+		  cap = cap * rate;
+		}
+		if (kappa_rtol < (errit * cap)) {
+		  itfail   = 1;
+		  exit_not = 0;
+		}
+	      } /* end else not last iter */
+	    } /* end else not converged */
+	  } /* end else newnrm not more than 10% above oldnrm */
+	} /* end else not first iteration */
+      } /* end else newnrm > minnrm */
+    } /* end else dgetrs succeeded */
     oldnrm = newnrm;
-  } /* end for iter */
+  } /* end for (iter ... )*/
   *rate_p = rate;
   *iter_count_p = iter_count;
   return(itfail);
