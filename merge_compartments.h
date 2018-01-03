ae64767150e8471f4325c655ec89aae005f95013
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:17 -0800

    r1935 | dbaxter | 2012-12-07 17:52:41 -0800 (Fri, 07 Dec 2012) | 3 lines
    Added compartment_text argument for changing char* fields to int64_t offsets.
    M    merge_compartments.h

--- merge_compartments.h
+++ merge_compartments.h
@@ -23,5 +23,6 @@ specific language governing permissions and limitations under the License.
 extern int merge_compartments(struct istring_elem_struct *list1,
 			      struct istring_elem_struct *list2,
 			      struct istring_elem_struct *mlist,
+			      char *compartment_text,
 			      int l1,
 			      int l2);
