791bc8e41043a1277cf35ef47c666548ff7a6da2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:48 -0800

    r3978 | dbaxter | 2017-02-07 13:58:35 -0800 (Tue, 07 Feb 2017) | 3 lines
    Fixed ifdef variable.
    M    lr3_approximate_delta_concs.h

--- lr3_approximate_delta_concs.h
+++ lr3_approximate_delta_concs.h
@@ -1,8 +1,9 @@
-#ifndef _LR_APPROXIMATE_DELTA_CONCS_H_
-#define _LR_APPROXIMATE_DELTA_CONCS_H_ 1
-extern int lr3_approximate_delta_concs(struct state_struct *state, double *counts,
-				      double *forward_rxn_likelihoods,
-				      double *reverse_rxn_likelihoods, 
-				      double *flux, double flux_scaling,
-				      int base_rxn, int choice);
+#ifndef _LR3_APPROXIMATE_DELTA_CONCS_H_
+#define _LR3_APPROXIMATE_DELTA_CONCS_H_ 1
+extern int lr3_approximate_delta_concs(struct state_struct *state, 
+				       double *counts,
+				       double *forward_rxn_likelihoods,
+				       double *reverse_rxn_likelihoods, 
+				       double *flux, double flux_scaling,
+				       int base_rxn, int choice);
 #endif
