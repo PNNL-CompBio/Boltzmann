f823935f71636308f2c3578780e23e1450df0d0e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:20 -0800

    r4367 | dbaxter | 2017-06-20 17:39:08 -0700 (Tue, 20 Jun 2017) | 3 lines
    Header file for lr8_approximate_delta_concs.c
    A    lr8_approximate_delta_concs.h

---
+++ lr8_approximate_delta_concs.h
@@ -0,0 +1,7 @@
+#ifndef _LR8_APPROXIMATE_DELTA_CONCS_H_
+#define _LR8_APPROXIMATE_DELTA_CONCS_H_ 1
+extern int lr8_approximate_delta_concs(struct state_struct *state,
+				       double *concs,
+				       double *flux,
+				       int choice);
+#endif
