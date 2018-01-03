bc2cd36e13e24dc0955e4eccb2a7d31979734b16
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:27 -0800

    r3670 | dbaxter | 2016-12-01 10:01:28 -0800 (Thu, 01 Dec 2016) | 3 lines
    Linux System include files.
    A    system_includes.h

---
+++ system_includes.h
@@ -0,0 +1,16 @@
+#ifndef _SYSTEM_INCLUDES_H_
+#define _SYSTEM_INCLUDES_H_ 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
