e23ab00da94e258245908f5fb30bfb501602d342
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:09 -0800

    r1687 | dbaxter | 2012-08-17 09:31:28 -0700 (Fri, 17 Aug 2012) | 4 lines
    Routine to locate a compartment separator ":" in
    a compound molecule:compartment name.
    A    find_colon.c

---
+++ find_colon.c
@@ -0,0 +1,48 @@
+/* find_colon.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "find_colon.h"
+int find_colon(char *rctnts) {
+  /*
+    Called by: parse_reactions_file
+    Return the position of a the leftmost colon in a string.
+    Returns -1 if no colon was found.
+  */
+  int i;
+  int colon_loc;
+  colon_loc = -1;
+  for (i=0;i<strlen(rctnts);i++) {
+    if (rctnts[i] == ':') {
+      colon_loc = i;
+      break;
+    }
+  }
+  return(colon_loc);
+}
