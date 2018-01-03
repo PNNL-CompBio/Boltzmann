43b3edc4b71a3e2903d99b21538196b1f06199d8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:35 -0800

    r4531 | dbaxter | 2017-09-20 10:12:07 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_state.h
    A    boltzmann_flatten_state.h

---
+++ boltzmann_flatten_state.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_FLATTEN_STATE_H_
+#define _BOLTZMANN_FLATTEN_STATE_H_ 1
+extern int boltzmann_flatten_state (struct state_struct **state_p,
+				    void **fstate_p,
+				    int direction, FILE *lfp);
+#endif
