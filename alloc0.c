37a947dd1165ef5fdb7b5670271872632e1ae7c3
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:28 -0800

    r4448 | dbaxter | 2017-08-22 17:49:22 -0700 (Tue, 22 Aug 2017) | 13 lines
    In preparation for boltzmann_flatten_state interface,
    added setup parameter, to allow avoidance of allocating
    
    filenames,
    solvent string,
    keyword_buffer,
    rxn_rile_keyword_buffer,
    rxn_keywords,
    rxn_file_keyword_lengths
    
    when setup = 0
    M    alloc0.c

--- alloc0.c
+++ alloc0.c
@@ -25,12 +25,15 @@ specific language governing permissions and limitations under the License.
 #include "boltzmann_set_filename_ptrs.h"
 
 #include "alloc0.h"
-int alloc0(struct state_struct **statep) {
+int alloc0(struct state_struct **statep, int setup) {
   /*
     Allocate space for boltzmann_state data and the associated state
     file names. And the parameter file input buffer line, and
     the random number generator state.
-    Called by: boltzmann_init
+    If setup is 0 only allocate the state struct, as the
+    other pieces are only used in setp. 
+    If setup is 1 allocate all the pieces
+    Called by: boltzmann_init, boltzmann_flatten_state
     Calls    : calloc, fprintf, fflush (intrinsic)
   */
   struct state_struct bltzs;
@@ -53,8 +56,8 @@ int alloc0(struct state_struct **statep) {
 
   ask_for           = (int64_t)sizeof(bltzs);
   one_l             = (int64_t)1;
-  max_file_name_len = (int64_t)4096;
-  max_param_line_len = (int64_t)4096;
+  max_file_name_len = (int64_t)128;
+  max_param_line_len = (int64_t)128;
   success           = 1;
   usage             = ask_for;
   state             = (struct state_struct *)calloc(one_l,ask_for);
@@ -85,107 +88,113 @@ int alloc0(struct state_struct **statep) {
       fflush(stderr);
     }
   }
-  if (success) {
-    state->max_filename_len   = max_file_name_len;
-    boltzmann_set_filename_ptrs(state);
-    /*
-    state->reaction_file      = state->params_file + max_file_name_len;
-    state->init_conc_file     = state->reaction_file + max_file_name_len;
-    state->input_dir          = state->init_conc_file + max_file_name_len;
-    state->output_file        = state->input_dir + max_file_name_len;
-    state->log_file           = state->output_file + max_file_name_len;
-    state->output_dir         = state->log_file + max_file_name_len;
-    state->counts_out_file    = state->output_dir + max_file_name_len;
-    state->ode_concs_file     = state->counts_out_file + max_file_name_len;
-    state->rxn_lklhd_file     = state->ode_concs_file + max_file_name_len;
-    state->free_energy_file   = state->rxn_lklhd_file + max_file_name_len;
-    state->restart_file       = state->free_energy_file + max_file_name_len;
-    state->rxn_view_file      = state->restart_file + max_file_name_len;
-    state->bndry_flux_file    = state->rxn_view_file + max_file_name_len;
-    state->pseudoisomer_file  = state->bndry_flux_file + max_file_name_len;
-    state->compartment_file   = state->pseudoisomer_file + max_file_name_len;
-    state->sbml_file          = state->compartment_file + max_file_name_len;
-    state->ms2js_file         = state->sbml_file + max_file_name_len;
-    state->kg2js_file         = state->ms2js_file + max_file_name_len;
-    */
+  if (setup) {
+    if (success) {
+      state->max_filename_len   = max_file_name_len;
+      boltzmann_set_filename_ptrs(state);
+      /*
+	state->reaction_file      = state->params_file + max_file_name_len;
+	state->init_conc_file     = state->reaction_file + max_file_name_len;
+	state->input_dir          = state->init_conc_file + max_file_name_len;
+	state->output_file        = state->input_dir + max_file_name_len;
+	state->log_file           = state->output_file + max_file_name_len;
+	state->output_dir         = state->log_file + max_file_name_len;
+	state->counts_out_file    = state->output_dir + max_file_name_len;
+	state->ode_concs_file     = state->counts_out_file + max_file_name_len;
+	state->rxn_lklhd_file     = state->ode_concs_file + max_file_name_len;
+	state->free_energy_file   = state->rxn_lklhd_file + max_file_name_len;
+	state->restart_file       = state->free_energy_file + max_file_name_len;
+	state->rxn_view_file      = state->restart_file + max_file_name_len;
+	state->bndry_flux_file    = state->rxn_view_file + max_file_name_len;
+	state->pseudoisomer_file  = state->bndry_flux_file + max_file_name_len;
+	state->compartment_file   = state->pseudoisomer_file + max_file_name_len;
+	state->sbml_file          = state->compartment_file + max_file_name_len;
+	state->ms2js_file         = state->sbml_file + max_file_name_len;
+	state->kg2js_file         = state->ms2js_file + max_file_name_len;
+      */
 
-    state->max_param_line_len = max_param_line_len;
-    ask_for                    = max_param_line_len << 1;
-    usage                      += ask_for;
-    state->param_buffer       = (char *)calloc(one_l,ask_for);
-    if (state->param_buffer == NULL) {
-      success = 0;
-      fprintf(stderr,
-	      "alloc0: unable to allocate %lld bytes for state->param_buffer.\n",
-	      ask_for);
-      fflush(stderr);
-    } 
+      state->max_param_line_len = max_param_line_len;
+      ask_for                    = max_param_line_len << 1;
+      usage                      += ask_for;
+      state->param_buffer       = (char *)calloc(one_l,ask_for);
+      if (state->param_buffer == NULL) {
+	success = 0;
+	fprintf(stderr,
+		"alloc0: unable to allocate %lld bytes for state->param_buffer.\n",
+		ask_for);
+	fflush(stderr);
+      } 
+    }
   }
-  if (success) {
-    ask_for = ((int64_t)64) * sizeof(char);
-    usage += ask_for;
-    solvent_string = (char *)calloc(one_l,ask_for);
-    if (solvent_string) {
-      state->solvent_string = solvent_string;
-    } else {
-      fprintf(stderr,"alloc0: Error unable to allocate %lld bytes of space "
-	      "for solvent_string\n",ask_for);
-      fflush(stderr);
-      success = 0;
+  if (setup) {
+    if (success) {
+      ask_for = ((int64_t)64) * sizeof(char);
+      usage += ask_for;
+      solvent_string = (char *)calloc(one_l,ask_for);
+      if (solvent_string) {
+	state->solvent_string = solvent_string;
+      } else {
+	fprintf(stderr,"alloc0: Error unable to allocate %lld bytes of space "
+		"for solvent_string\n",ask_for);
+	fflush(stderr);
+	success = 0;
+      }
     }
   }
   /*
     Allocate space for processing the reactions file.
   */
-  if (success) {
-    keyword_buffer_length = (int64_t)256;
-    state->keyword_buffer_length = keyword_buffer_length;
-    ask_for = keyword_buffer_length;
-    usage   += ask_for;
-    rxn_keyword_buff = (char *)calloc(one_l,ask_for);
-    if (rxn_keyword_buff) {
-      state->rxn_file_keyword_buffer = rxn_keyword_buff;
-    } else {
-      fprintf(stderr,
-	      "alloc0: Error, unable to allocate %lld bytes for "
-	      "rxn_file_keyword_buffer\n",
-	      ask_for);
-      fflush(stderr);
-      success = 0;
+  if (setup) {
+    if (success) {
+      keyword_buffer_length = (int64_t)256;
+      state->keyword_buffer_length = keyword_buffer_length;
+      ask_for = keyword_buffer_length;
+      usage   += ask_for;
+      rxn_keyword_buff = (char *)calloc(one_l,ask_for);
+      if (rxn_keyword_buff) {
+	state->rxn_file_keyword_buffer = rxn_keyword_buff;
+      } else {
+	fprintf(stderr,
+		"alloc0: Error, unable to allocate %lld bytes for "
+		"rxn_file_keyword_buffer\n",
+		ask_for);
+	fflush(stderr);
+	success = 0;
+      }
+    }
+    if (success) {
+      num_rxn_file_keywords = 16;
+      state->num_rxn_file_keywords = num_rxn_file_keywords;
+      ask_for = ((int64_t)num_rxn_file_keywords) * ((int64_t)sizeof(char *));
+      usage   += ask_for;
+      rxn_keywords = (char **)calloc(one_l,ask_for);
+      if (rxn_keywords) {
+	state->rxn_file_keywords = rxn_keywords;
+      } else {
+	fprintf(stderr,
+		"alloc0: Error, unable to allocate %lld bytes for "
+		"rxn_file_keywords\n",
+		ask_for);
+	fflush(stderr);
+	success = 0;
+      }
+    }
+    if (success) {
+      ask_for = ((int64_t)num_rxn_file_keywords) * ((int64_t)sizeof(int64_t));
+      usage += ask_for;
+      rxn_file_keyword_lengths = (int64_t *)calloc(one_l,ask_for);
+      if (rxn_file_keyword_lengths) {
+	state->rxn_file_keyword_lengths = rxn_file_keyword_lengths;
+      } else {
+	fprintf(stderr,
+		"alloc0: Error, unable to allocate %lld bytes for "
+		"rxn_file_keyword_lengths\n",
+		ask_for);
+	fflush(stderr);
+	success = 0;
+      }
     }
   }
-  if (success) {
-    num_rxn_file_keywords = 16;
-    state->num_rxn_file_keywords = num_rxn_file_keywords;
-    ask_for = ((int64_t)num_rxn_file_keywords) * ((int64_t)sizeof(char *));
-    usage   += ask_for;
-    rxn_keywords = (char **)calloc(one_l,ask_for);
-    if (rxn_keywords) {
-      state->rxn_file_keywords = rxn_keywords;
-    } else {
-      fprintf(stderr,
-	      "alloc0: Error, unable to allocate %lld bytes for "
-	      "rxn_file_keywords\n",
-	      ask_for);
-      fflush(stderr);
-      success = 0;
-    }
-  }
-  if (success) {
-    ask_for = ((int64_t)num_rxn_file_keywords) * ((int64_t)sizeof(int64_t));
-    usage += ask_for;
-    rxn_file_keyword_lengths = (int64_t *)calloc(one_l,ask_for);
-    if (rxn_file_keyword_lengths) {
-      state->rxn_file_keyword_lengths = rxn_file_keyword_lengths;
-    } else {
-      fprintf(stderr,
-	      "alloc0: Error, unable to allocate %lld bytes for "
-	      "rxn_file_keyword_lengths\n",
-	      ask_for);
-      fflush(stderr);
-      success = 0;
-    }
-    state->usage = usage;
-  }
+  state->usage = usage;
   return(success);
 }
