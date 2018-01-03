57e1400a557a99591edd27d30508c5906f127aa7
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:26 -0800

    r2141 | dbaxter | 2013-02-27 10:46:18 -0800 (Wed, 27 Feb 2013) | 3 lines
    Routine to return the number_of_reaction_files field of a superstate struct.
    A    boltzmann_number_of_reaction_files.c

---
+++ boltzmann_number_of_reaction_files.c
@@ -0,0 +1,53 @@
+/* boltzmann_number_of_reaction_files.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "djb_timing_b.h"
+#include "boltzmann_structs.h"
+
+#include "boltzmann_number_of_reaction_files.h"
+int boltzmann_number_of_reaction_files(struct super_state_struct *super_state) {
+  /*
+    Return the number of reaction files represented in the superstate.
+    Called by : User (part of API).
+  */
+  int result;
+  int padi;
+  if (super_state) {
+    result = (int)super_state->number_of_reaction_files;
+  } else {
+    result = 0;
+  }
+  return (result);
+}
