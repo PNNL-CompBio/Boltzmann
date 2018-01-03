ee20923b4ed3b71d738e68be57b431ac95928be3
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:16 -0800

    r4892 | dbaxter | 2017-11-27 13:19:50 -0800 (Mon, 27 Nov 2017) | 4 lines
    Removed htry, nonnegative, norm_control, print_concs and choice arguments,
    as those are fields in state or state->ode23tb_params
    M    ode23tb.h

--- ode23tb.h
+++ ode23tb.h
@@ -1,8 +1,4 @@
 #ifndef _ODE23TB_H_
 #define _ODE23TB_H_ 1
-extern int ode23tb (struct state_struct *state, double *counts,
-		    double htry, int nonnegative,
-		    int normcontrol,
-		    int print_concs,
-		    int choice);
+extern int ode23tb (struct state_struct *state, double *concs);
 #endif
