ee53084405cbdd3eee7eb78ab7403a356f4ddc29
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:38 -0800

    r2384 | dbaxter | 2013-07-15 17:11:09 -0700 (Mon, 15 Jul 2013) | 3 lines
    Updated with many new fields.
    M    echo_params.c

--- echo_params.c
+++ echo_params.c
@@ -34,20 +34,28 @@ int echo_params (FILE *lfp, struct state_struct *state) {
   int pad1;
   success = 1;
   if (lfp) {
-    fprintf(lfp,"state->params_file    	   = %s\n",state->params_file);
-    fprintf(lfp,"state->reaction_file  	   = %s\n",state->reaction_file);
-    fprintf(lfp,"state->init_conc_file 	   = %s\n",state->init_conc_file);
-    fprintf(lfp,"state->input_dir      	   = %s\n",state->input_dir);
-    fprintf(lfp,"state->output_file    	   = %s\n",state->output_file);
-    fprintf(lfp,"state->log_file      	   = %s\n",state->log_file);
-    fprintf(lfp,"state->output_dir     	   = %s\n",state->output_dir);
-    fprintf(lfp,"state->align_len          = %ld\n",state->align_len);
-    fprintf(lfp,"state->max_filename_len   = %ld\n",state->max_filename_len);
-    fprintf(lfp,"state->max_param_line_len = %ld\n",state->max_param_line_len);
-    fprintf(lfp,"state->ideal_gas_r        = %le\n",state->ideal_gas_r);
-    fprintf(lfp,"state->temp_kelvin        = %le\n",state->temp_kelvin);
-    fprintf(lfp,"state->ph                 = %le\n",state->ph);
-    fprintf(lfp,"state->ionic_strength     = %le\n",state->ionic_strength);
+    fprintf(lfp,"state->params_file    	       = %s\n",state->params_file);
+    fprintf(lfp,"state->reaction_file  	       = %s\n",state->reaction_file);
+    fprintf(lfp,"state->init_conc_file 	       = %s\n",state->init_conc_file);
+    fprintf(lfp,"state->input_dir      	       = %s\n",state->input_dir);
+    fprintf(lfp,"state->output_file    	       = %s\n",state->output_file);
+    fprintf(lfp,"state->log_file      	       = %s\n",state->log_file);
+    fprintf(lfp,"state->concs_out_file         = %s\n",state->concs_out_file);
+    fprintf(lfp,"state->rxn_lklhd_file         = %s\n",state->rxn_lklhd_file);
+    fprintf(lfp,"state->free_energy_file       = %s\n",state->free_energy_file);
+    fprintf(lfp,"state->restart_file           = %s\n",state->restart_file);
+    fprintf(lfp,"state->rxn_view_file          = %s\n",state->rxn_view_file);
+    fprintf(lfp,"state->bndry_flux_file        = %s\n",state->bndry_flux_file);
+    fprintf(lfp,"state->pseudoisomer_file      = %s\n",
+	    state->pseudoisomer_file);
+    fprintf(lfp,"state->output_dir     	       = %s\n",state->output_dir);
+    fprintf(lfp,"state->align_len              = %ld\n",state->align_len);
+    fprintf(lfp,"state->max_filename_len       = %ld\n",state->max_filename_len);
+    fprintf(lfp,"state->max_param_line_len     = %ld\n",state->max_param_line_len);
+    fprintf(lfp,"state->ideal_gas_r            = %le\n",state->ideal_gas_r);
+    fprintf(lfp,"state->temp_kelvin            = %le\n",state->temp_kelvin);
+    fprintf(lfp,"state->ph                     = %le\n",state->ph);
+    fprintf(lfp,"state->ionic_strength         = %le\n",state->ionic_strength);
   }
   return (success);
 }
