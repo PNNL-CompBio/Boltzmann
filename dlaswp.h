cdbac69d7970aad3eadf37e9034fb8c99a8f6393
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:23 -0800

    r3622 | dbaxter | 2016-11-22 14:47:03 -0800 (Tue, 22 Nov 2016) | 3 lines
    header file for dlaswp.c
    A    dlaswp.h

---
+++ dlaswp.h
@@ -0,0 +1,10 @@
+#ifndef _DLASWP_H_
+#define _DLASWP_H_ 1
+extern void dlaswp( int *n_p,
+		    double *a,
+		    int *lda_p,
+		    int *k1_p,
+		    int *k2_p,
+		    int *ipiv,
+		    int *incx_p);
+#endif
