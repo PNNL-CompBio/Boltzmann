eca05abea6fe5df8e378ec7aa2cc4fd65f381fb6
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:25 -0800

    r4419 | dbaxter | 2017-07-11 07:40:43 -0700 (Tue, 11 Jul 2017) | 5 lines
    Added counts_or_concs and count_or_conc arguments,
    counts_or_concs is a vector of counts if count_or_conc = 1, of
    concentrations if count_or_conc = 0.
    M    update_regulation.h

--- update_regulation.h
+++ update_regulation.h
@@ -1,4 +1,6 @@
 #ifndef _UPDATE_REGULATION_H_
 #define _UPDATE_REGULATION_H_ 1
-extern int update_regulation (struct state_struct *state, int rxn);
+extern int update_regulation (struct state_struct *state, int rxn,
+			      double *counts_or_concs,
+			      int count_or_conc);
 #endif
