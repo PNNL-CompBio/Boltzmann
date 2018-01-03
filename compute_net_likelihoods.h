cf488ba764c20d47e35e0408c035c40eff1cbf0f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:42 -0800

    r3904 | dbaxter | 2017-01-24 14:44:39 -0800 (Tue, 24 Jan 2017) | 4 lines
    Added arguments forward_rxn_likelihood, reverse_rxn_likelihood, and
    net_likelihood.
    M    compute_net_likelihoods.h

--- compute_net_likelihoods.h
+++ compute_net_likelihoods.h
@@ -1,4 +1,7 @@
 #ifndef _COMPUTE_NET_LIKELIHOODS_H_
 #define _COMPUTE_NET_LIKELIHOODS_H_ 1
-extern int compute_net_likelihoods(struct state_struct *state);
+extern int compute_net_likelihoods(struct state_struct *state,
+				   double *forward_rxn_likeklihood,
+				   double *reverse_rxn_likeklihood,
+				   double *net_likelihood);
 #endif
