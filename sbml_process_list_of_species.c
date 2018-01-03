177270a8363e2241986278d55d9651f1cf9f07d8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:04 -0800

    r2898 | dbaxter | 2014-10-15 17:18:16 -0700 (Wed, 15 Oct 2014) | 4 lines
    Added sbml_start_species_def call, sbml_parse_species_key_value call and
    sbml_generate_init_conc_line call.
    M    sbml_process_list_of_species.c

--- sbml_process_list_of_species.c
+++ sbml_process_list_of_species.c
@@ -25,10 +25,9 @@ specific language governing permissions and limitations under the License.
 
 #include "count_ws.h"
 #include "count_nws.h"
-#include "sbml_key_value.h"
-#include "sbml_lookup_species_attribute.h"
-#include "compartment_lookup.h"
-#include "translate_ms_2_js.h"
+#include "sbml_start_species_def.h"
+#include "sbml_parse_species_key_value.h"
+#include "sbml_generate_init_conc_line.h"
 
 #include "sbml_process_list_of_species.h"
 int sbml_process_list_of_species(FILE *sbml_fp,
@@ -41,38 +40,27 @@ int sbml_process_list_of_species(FILE *sbml_fp,
     Called by: parse_sbml
     Calls:     fprintf, feof, fgets, fflush, strcpy, strcmp, strncmp,
                count_ws, count_nws,
-	       sbml_key_value,
-               sbml_lookup_species_attribute
+	       sbml_start_species_def,
+	       sbml_parse_species_key_value,
+               sbml_generate_init_conc_line
   */
-  struct state_struct pseudo_state;
   struct compartment_struct *compartment;
-  struct ms2js_struct *ms2js_data;
+  struct t2js_struct *ms2js_data;
   char comp_c[1024];
   char species_c[1024];
-  char units_c[1024];
-  char *key_c[1024];
-  char *value_c[1024];
-  char *species_name_c[1024];
-  char *key;
-  char *value;
+  char species_name_c[1024];
+  char kegg_id_c[1024];
+  char *kegg_id;
   char *comp;
   char *species;
-  char *tspecies;
   char *species_name;
-  char *units;
   char bcf[8];
-  char vc[8];
   char *concs_file;
   char *bc;
   char *line;
   
   double init_amt;
-  double mole;
-  double millimole;
-  double micromole;
-  double nanomole;
-  double picomole;
-  double femtomole;
+  double init_conc;
   double multiplier;
   double recip_volume;
   double default_recip_volume;
@@ -81,58 +69,29 @@ int sbml_process_list_of_species(FILE *sbml_fp,
   int in_species_tag;
 
   int in_list_of_species;
-  int is_a_conc;
-  
   int success;
-  int tl;
-
-  int max_key_len;
-  int max_val_len;
 
   int nb;
   int ns;
 
-  int tag;
-  int ci;
+  int in_species_group;
+  int i;
 
-  int translate_from_modelseed;
-  int padi;
-   
-
+  int tl;
+  int species_count;
+  
   FILE *lfp;
   FILE *error_fp;
   FILE *concs_fp;
-  FILE *extra_fp;
   FILE *id_name_fp;
 
-  pseudo_state.compartment_text = state->compartment_text;
-  pseudo_state.sorted_cmpts = state->sorted_compartments;
-  pseudo_state.nunique_compartments = state->num_cmpts;
   ms2js_data  = state->ms2js_data;
   success     = 1;
-  key         = (char *)&(key_c[0]);
-  value       = (char *)&(value_c[0]);
   comp        = (char *)&(comp_c[0]);
   species     = (char *)&(species_c[0]);
   species_name = (char *)&(species_name_c[0]);
-  units       = (char *)&(units_c[0]);
+  kegg_id     = (char *)&(kegg_id_c[0]);
   bc          = (char *)&bcf[0];
-  vc[0]       = 'F';
-  vc[1]       = 'V';
-  max_key_len = 1023;
-  max_val_len = 1023;
-  mole = 1.0;
-  millimole = 1.e-3;
-  micromole = 1.e-6;
-  nanomole  = 1.e-9;
-  picomole  = 1.e-12;
-  femtomole = 1.e-15;
-  /*
-    For now we hardware translate_from_modelseed to be 1,
-    assuming the sbml files are using the modelseed id's.
-    Could become a parameter later.
-  */
-  translate_from_modelseed = 1;
 
   lfp         = state->log_fp;
   concs_fp    = state->concs_fp;
@@ -142,233 +101,131 @@ int sbml_process_list_of_species(FILE *sbml_fp,
   } else {
     error_fp = lfp;
   }
-  if (success) {
-    /*
-      For now use the default volume for the volume line, we will 
-      read in the individual compartment volumes in the
-      listOfCompartments section.
+  /*
+    For now use the default volume for the volume line, we will 
+    read in the individual compartment volumes in the
+    listOfCompartments section.
+  */
+  default_recip_volume = 1.0e15;
+  fprintf(concs_fp,"VOLUME 1.0e-15\n");
+  /*
+    At some point in time we will want to compute the units field
+    from the substanceUnits field but for now us 1.e-9
+  */
+  fprintf(concs_fp,"CONC_UNITS 1.0e-9\n");
+  in_list_of_species = 1;
+  in_species_tag = 0;
+  in_species_group = 0;
+  species_count = 0;
+  while (!feof(sbml_fp) && (in_list_of_species == 1)) {
+    /* 
+       Now we are in the listOfSpecies section.    
     */
-    fprintf(concs_fp,"VOLUME 1.0e-15\n");
+    fgets(sbml_buffer,sbml_buffer_len,sbml_fp);
+    line = sbml_buffer;
+    nb = count_ws(line);
+    line += nb; /* Caution address arithmetic */
     /*
-      At some point in time we will want to compute the units field
-      from the substanceUnits field but for now us 1.e-9
+      First check for an </listOfSpecies> tag.
     */
-    fprintf(concs_fp,"CONC_UNITS 1.0e-9\n");
-    comp[0] = '\0';
-    species[0] = '\0';
-    species_name[0] = '\0';
-    init_amt=0;
-    units[0] = '\0';
-    variable     = 1;
-    in_species_tag = 0;
-    in_list_of_species = 1;
-    while (!feof(sbml_fp) && (in_list_of_species == 1)) {
-      /* 
-	 Now we are in the listOfSpecies section.    
-      */
-      fgets(sbml_buffer,sbml_buffer_len,sbml_fp);
-      line = sbml_buffer;
-      if (in_species_tag == 0) {
-	nb = count_ws(line);
-	line += nb; /* Caution address arithmetic */
-	/*
-	  First check for an </listOfSpecies> tag.
-	*/
-	if (strncmp(line,"</listOfSpecies>",16) == 0) {
-	  in_list_of_species = 0;
-	}  else {
+    if (strncmp(line,"</listOfSpecies>",16) == 0) {
+      in_list_of_species = 0;
+    } else {
+      if (in_species_group == 0) {
+	if (in_species_tag == 0) {
 	  if (strncmp(line,"<species",8) == 0) {
-	    in_species_tag = 1;
-	    multiplier = 1.0;
+	    /*
+	      Initialize species attributes and set in_species_tag
+	      and in_species group to 1.
+	    */
+	    sbml_start_species_def(species,species_name,
+				   kegg_id,comp,
+				   default_recip_volume,
+				   &multiplier,
+				   &recip_volume,
+				   &init_conc,
+				   &init_amt,
+				   &variable,
+				   &in_species_tag,
+				   &in_species_group);
 	    line +=8; /* Caution address arithmetic */
 	    /*
 	      Skip over white space.
 	    */
 	    nb = count_ws(line);
 	    line += nb; /* Caution address arithmetic */
-	  }
-	}
-      }
+	  } /* end if <species line */
+	} /* end if not in <species tag */
+      } /* end if not in <species></species> group */
       if (in_species_tag) {
         ns = count_nws(line);
         while (ns != 0) {
-          /*
-            Need a routine to scan for keyword "=" "string" triples returning
-            full length allowing whitespace before keyword, between any of the
-            three tokens and replacing all whitespace in string with 
-            underscores, sbml_key_value.
-          */
           /*
             check for end tag.
           */
           if ((strncmp(line,"/>",2)==0) || (line[0] == '>')) {
             ns = 0;
             in_species_tag = 0;
-            /*
-	      Generate line for concs.in file.
-	      First convert the amount to a concentration.
-	      Also for sbml files that have names where the only
-	      real chemical species information is stored 
-	      (i.e. the YLI_y7.2.xml file) we need to print
-	      a (species_id, species_name) file for analysis and
-	      assignment of json id's.
-            */
-            if (comp[0] != '\0') {
-	      /*
-		We had a non null compartment.
-	      */
-	      init_amt *= multiplier * recip_volume;
-	      fprintf(concs_fp,"%s:%s\t%le\t%c\n",
-		      tspecies,comp,init_amt,vc[variable]);
-            } else {
-	      /*
-		Compartment did not exist.
-	      */
-	      init_amt *= default_recip_volume;
-	      fprintf(concs_fp,"%s\t%le\t%c\n",
-		      tspecies,init_amt,vc[variable]);
-            }
-	    if (species_name[0] != '\0') {
-	      fprintf(id_name_fp,"%s\t%s\n",species,species_name);
-	      species_name[0] = '\0';
-	    }
-	    multiplier = 1.0;
-	    recip_volume = default_recip_volume;
           } else {
             /*
 	      Not the end of species  tag.
             */
-            tl = sbml_key_value(line,key,value,max_key_len,max_val_len);
+	    tl = sbml_parse_species_key_value(state,line,
+					      default_recip_volume,
+					      bc,comp,
+					      species,species_name,
+					      &recip_volume,&multiplier,
+					      &init_amt,&init_conc,
+					      &variable);
             if (tl <= 0) {
+	      /*
+		we have reached the end of this line in the file,
+		but not yet the closing tag of <species .
+	      */
               ns = 0;
             } else {
               line += tl;
-              tag = sbml_lookup_species_attribute(key);
-              if (tag >= 0) {
-		switch (tag) {
-		case 0:
-		  /*
-		    boundary_condition: not used yet.
-		  */
-		  strcpy (bc,value);
-		  break;
-		case 1:
-		  /*
-		    compartment:
-		  */
-		  strcpy(comp,value);
-		  ci = compartment_lookup(comp,&pseudo_state);
-		  if (ci >= 0) {
-		    compartment = &state->sorted_compartments[ci];
-		    recip_volume = compartment->recip_volume;
-		  } else {
-		    recip_volume = default_recip_volume;
-		  }
-		  break;
-		case 2:
-		  /*
-		    constant
-		  */
-		  if (strcmp(value,"true") == 0) {
-		    variable = 0;
-		  } else {
-		    variable = 1;
-		  }
-		  break;
-		case 3:
-		  /*
-		    hasOnlySubstanceUnits
-		    We do not actually use this now,
-		    we assume this is false.
-		  */
-		  if (strcmp(value,"true") == 0) {
-		    multiplier = state->recip_avogadro;
-		  }
-		  break;
-		case 4:
-		  /*
-		    Species id
-		  */
-		  strcpy(species,value);
-		  if (translate_from_modelseed) {
-		    tspecies = translate_ms_2_js(ms2js_data,species);
-		    if (tspecies == NULL) {
-		      if (lfp) {
-			fprintf(lfp,"sbml_process_list_of_species: Warning:"
-				" species %s did not have a json translation"
-				" using as is.\n",
-				species);
-			fflush(lfp);
-		      }
-		      tspecies = species;
-		    }
-		  } else {
-		    tspecies = species;
-		  }
-
-		  break;
-		case 5:
-		  /*
-		    initialAmount
-		  */
-		  sscanf(value,"%le",&init_amt);
-		  break;
-		case 6:
-		  /*
-		    Species name
-		    Here this is not part of the sbml standard
-		    but the xml file we got this was the only
-		    place the actual chemical name was mentioned.
-		    The 
-		  */
-		  strcpy(species_name,value);
-		  break;
-		case 7:
-		  /*
-		    substanceUnits
-		  */
-		  strcpy(units,value);
-		  if (strcmp(value,"mole") == 0)  {
-		    multiplier = mole;
-		  } else {
-		    if (strcmp(value,"millimole") == 0) {
-		      multiplier = millimole;
-		    } else {
-		      if (strcmp(value,"micromole") == 0) {
-			multiplier = micromole;
-		      } else {
-			if (strcmp(value,"nanomole") == 0) {
-			  multiplier = nanomole;
-			} else {
-			  if (strcmp(value,"picomole") == 0) {
-			    multiplier = picomole;
-			  } else {
-			    if (strcmp(value,"femtomole") == 0) {
-			      multiplier = femtomole;
-			    } else {
-			      fprintf(error_fp,"sbml_process_list_of_species: "
-				      "Error, unrecognized unit for ammount "
-				      "was %s\n",value);
-			      fflush(error_fp);
-			      multiplier = mole;
-			    }
-			  }
-			}
-		      }
-		    }
-		  }
-		  break;
-		} /* end switch */  
-	      } else {
-		fprintf(error_fp,"sbml_process_list_of_species: "
-			"Error found unexpected key :%s\n",key);
-		fflush(error_fp);
-	      }
             } /* end else we found a keyword=value triple. */
-          } /* end else we did not find and end species tag */
+          } /* end else we did not find an end of the species tag */
         } /* end while (ns != 0) */
-      } /* end if (in_species_tag) */
-    } /* end while (!feof...) */
-  } /* end if open of concs.in file succeeded. */
+      } else {
+	/*
+	  Not in species tag.
+	  Check for an end of species group tag, "</species>".
+	*/
+	if (strncmp(line,"</species>",10) == 0) {
+	  /*
+	    Generate species line and translation map.
+	  */
+	  sbml_generate_init_conc_line(state,species_count,
+				       species,species_name,
+				       kegg_id,comp,
+				       multiplier,
+				       recip_volume,
+				       init_conc,init_amt,
+				       variable);
+	  species_count += 1;
+	  in_species_group = 0;
+	} else {
+	  /*
+	    Not at end of <species></species> group
+	    look for a kegg_id if we have not gotten one.;
+	  */
+	  if (kegg_id[0] == '\0') {
+	    if (strncmp(line,"<rdf:li rdf:resource=\"http://identifiers.org/kegg.compound/",59)==0) {
+	      line += 59;
+	      sscanf(line,"%s",kegg_id);
+	      for (i=0;i<strlen(kegg_id);i++) {
+		if (kegg_id[i] == '"') {
+		  kegg_id[i] = '\0';
+		  break;
+		}
+	      }
+	    }
+	  } /* end if no kegg_id yet */
+	} /* end else not at end of <species></species> group */
+      } /* end else not in species tag. */
+    } /* end else not at </listOfSpecies> tag */ 
+  } /* end while (!feof...) */
   return (success);
 }
