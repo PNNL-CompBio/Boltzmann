8af116f62115763c42f3d6a3cfb1d54b2cdde975
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:41 -0800

    r4590 | dbaxter | 2017-09-26 17:58:19 -0700 (Tue, 26 Sep 2017) | 3 lines
    Header file for
    A    boltzmann_save_agent_data.h

---
+++ boltzmann_save_agent_data.h
@@ -0,0 +1,5 @@
+#ifndef _BOLTZMANN_SAVE_AGENT_DATA_H_
+#define _BOLTZMANN_SAVE_AGENT_DATA_H_ 1
+extern int boltzmann_save_agent_data(struct state_struct *state, 
+				     void *agent_data);
+#endif
