272f6e2a229e41c7bb0aee4f57157f4e846799a0
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:52 -0800

    r4713 | dbaxter | 2017-11-07 18:13:53 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for dcrsng_mag_merge.c
    A    dcrsng_mag_merge.h

---
+++ dcrsng_mag_merge.h
@@ -0,0 +1,8 @@
+#ifndef _DCRSNG_MAG_MERGE_H_
+#define _DCRSNG_MAG_MERGE_H_ 1
+extern void dcrsng_mag_merge(double *list1, int *index1,
+			     double *list2, int *index2,
+			     double *mlist, int *mindex,
+			     int l1,
+			     int l2);
+#endif
