d342e94fb3bd8cc4129eb2504ebdc8b6ab6786d1
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:16 -0800

    r4893 | dbaxter | 2017-11-27 13:20:18 -0800 (Mon, 27 Nov 2017) | 3 lines
    Added ode23tb_params_struct.h
    M    boltzmann_structs.h

--- boltzmann_structs.h
+++ boltzmann_structs.h
@@ -51,4 +51,5 @@ specific language governing permissions and limitations under the License.
 #include "t2js_struct.h"
 #include "sbml2bo_struct.h"
 #include "cvodes_params_struct.h"
+#include "ode23tb_params_struct.h"
 #endif
