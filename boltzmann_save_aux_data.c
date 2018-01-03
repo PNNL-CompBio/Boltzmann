39a2dd939a3e6932630763d0d9270149558ebd2f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:34 -0800

    r4519 | dbaxter | 2017-09-20 09:36:30 -0700 (Wed, 20 Sep 2017) | 4 lines
    Routine to save the filenames and string data from the reactions.dat file
    to a flattened text structure.
    A    boltzmann_save_aux_data.c

---
+++ boltzmann_save_aux_data.c
@@ -0,0 +1,37 @@
+/* boltzmann_save_aux_data.c
+*******************************************************************************
+Boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include "boltzmann_structs.h"
+#include "boltzmann_flatten_aux_data.h"
+#include "boltzmann_save_aux_data.h"
+int boltzmann_save_aux_data(struct state_struct *state, void **aux_data_p) {
+  /*
+    Create a contiguous copy of strings in the boltzman state struct.
+    Called by: biocellion/user, boltzman_test_save_load
+    Calls:     boltzmann_flatten_aux_data
+  */
+  int direction;
+  int success;
+  direction = 0;
+  success = boltzmann_flatten_aux_data(state,aux_data_p,direction);
+  return(success);
+}
