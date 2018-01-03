79006f36dd5931a1524de024857b5fb3e3ae2829
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:24 -0800

    r4972 | dbaxter | 2017-12-05 18:14:03 -0800 (Tue, 05 Dec 2017) | 3 lines
    New version of state_struct.h (4971) added coeff_sum field.
    M    alloc0.c

--- alloc0.c
+++ alloc0.c
@@ -133,7 +133,7 @@ int alloc0(struct state_struct **statep, int setup) {
     usage = state->usage;
   }
   if (success && setup) {
-    state->version_no = 4895; // Checkin revision of state_struct.h
+    state->version_no = 4971; // Checkin revision of state_struct.h
     /*
       Allocate_space for reading the parameter file.
     */
