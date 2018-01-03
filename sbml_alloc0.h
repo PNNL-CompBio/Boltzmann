0c22a0c80f4b535189e9e7fed0b3ddef0304e343
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:57 -0800

    r2791 | dbaxter | 2014-06-13 14:40:39 -0700 (Fri, 13 Jun 2014) | 4 lines
    Back to original form splitting out code in sbml_alloc2 and removing
    arguments num_modelseed_ids and length_ms2js_strings arguments.
    M    sbml_alloc0.h

--- sbml_alloc0.h
+++ sbml_alloc0.h
@@ -21,3 +21,4 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
 extern int sbml_alloc0(struct sbml2bo_struct **state_p);
+
