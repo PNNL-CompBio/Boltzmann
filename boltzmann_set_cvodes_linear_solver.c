6c0a7a1ff64a81ccf725882ab08457855d41cfca
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:27 -0800

    r5010 | dbaxter | 2017-12-13 18:21:37 -0800 (Wed, 13 Dec 2017) | 3 lines
    Added cvodes_params_struct.h include.
    M    boltzmann_set_cvodes_linear_solver.c

--- boltzmann_set_cvodes_linear_solver.c
+++ boltzmann_set_cvodes_linear_solver.c
@@ -1,5 +1,6 @@
 #include "boltzmann_structs.h"
 #include "boltzmann_cvodes_headers.h"
+#include "cvodes_params_struct.h"
 #include "boltzmann_check_cvdls_errors.h"
 /*
 #include "boltzmann_check_cvsls_errors.h"
