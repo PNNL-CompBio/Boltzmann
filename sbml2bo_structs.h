72c13cb0384ea82ba0895e63e99d775b90db00ec
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:55 -0800

    r2758 | dbaxter | 2014-05-27 14:04:10 -0700 (Tue, 27 May 2014) | 3 lines
    Includes needed by sbml2bo stuff.
    A    sbml2bo_structs.h

---
+++ sbml2bo_structs.h
@@ -0,0 +1,54 @@
+/* sbml2bo_structs.h 
+  
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+*/
+#ifndef __SBML2BO_STRUCTS__
+#define __SBML2BO_STRUCTS__ 1
+/*
+  System include files.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/*
+  Maybe only for linux systems.
+*/
+#ifdef LIBUNWIND
+#include <libunwind.h>
+#include <unwind.h>
+#endif
+
+#include "djb_timing_b.h"
+/*
+  Data structures include files.
+*/
+#include "sbml2bo_struct.h"
+#include "sbml_cmpt_struct.h"
+#include "sbml_species_struct.h"
+#endif
