35bc0547e99ec70369035c4f2dd5d1f63b322c46
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:04 -0800

    r2892 | dbaxter | 2014-10-15 17:03:31 -0700 (Wed, 15 Oct 2014) | 5 lines
    Added arguments num_keg_ids, length_kg2js_strings.
    Changed ms2js_struct to t2js_struct.
    Added kg2js fields for translating and sorting.
    M    sbml_alloc2.c

--- sbml_alloc2.c
+++ sbml_alloc2.c
@@ -27,57 +27,114 @@ specific language governing permissions and limitations under the License.
 
 int sbml_alloc2(struct sbml2bo_struct *state,
 		int64_t num_modelseed_ids,
-		int64_t length_ms2js_strings) {
+		int64_t length_ms2js_strings,
+		int64_t num_kegg_ids,
+		int64_t length_kg2js_strings) {
   /*
     Allocate the sbml2bo_struct and its file name fields.
     Called by: sbml_to_boltzmann, sbml2bo
     Calls:     calloc
   */
-  struct ms2js_struct ms2js_instance;
-  struct ms2js_struct *ms2js_data;
+  struct t2js_struct t2js_instance;
+  struct t2js_struct *ms2js_data;
+  struct t2js_struct *kg2js_data;
   int64_t ask_for;
   int64_t one_l;
   char **ms_ids;
-  char **js_ids;
-  char *sbml_file;
-  char *concs_in_file;
-  char *rxns_dat_file;
-  char *cmpts_dat_file;
-  char *ms2js_file;
-  char *log_file;
+  char **mjs_ids;
+  char **kegg_ids;
+  char **kjs_ids;
+  char **json_ids;
   char *ms2js_strings;
-  int     max_file_name_len;
+  char *kg2js_strings;
   int     success;
-  int     num_files;
   int     padi;
+  FILE *lfp;
   success = 1;
   one_l   = (int64_t)1;
-  ask_for = (int64_t)sizeof(ms2js_instance);
-  ms2js_data = (struct ms2js_struct*)calloc(one_l,ask_for);
+  lfp     = state->log_fp;
+  ask_for = (int64_t)sizeof(t2js_instance);
+  ms2js_data = (struct t2js_struct*)calloc(one_l,ask_for);
+  if (lfp == NULL) {
+    lfp = stderr;
+  }
   if (ms2js_data == NULL) {
     success = 0;
   }
   if (success) {
     state->ms2js_data = ms2js_data;
+    ask_for = (int64_t)sizeof(t2js_instance);
+    kg2js_data = (struct t2js_struct*)calloc(one_l,ask_for);
+    if (kg2js_data == NULL) {
+      success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for kg2js_data\n",ask_for);
+      fflush(lfp);
+    }
+  }
+  if (success) {
+    state->kg2js_data = kg2js_data;
     ask_for = length_ms2js_strings;
     ms2js_strings = (char*)calloc(one_l,ask_for);
     if (ms2js_strings == NULL) {
       success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for ms2js_strings\n",ask_for);
+      fflush(lfp);
     }
   }
   if (success) {
-    ms2js_data->ms2js_strings = ms2js_strings;
+    ms2js_data->strings = ms2js_strings;
     ask_for = num_modelseed_ids * (sizeof(char *) << 1);
     ms_ids  = (char **)calloc(one_l,ask_for);
     if (ms_ids == NULL) {
       success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for ms_ids\n",ask_for);
+      fflush(lfp);
     }
   }
   if (success) {
-    ms2js_data->ms_ids = ms_ids;
-    ms2js_data->js_ids = (char **)&ms_ids[num_modelseed_ids]; /* Caution address arithmetic */
-    ms2js_data->num_modelseed_ids = num_modelseed_ids;
-    ms2js_data->length_ms2js_strings = length_ms2js_strings;
+    ms2js_data->dictionary_ids = ms_ids;
+    ms2js_data->json_ids = (char **)&ms_ids[num_modelseed_ids]; 
+    ms2js_data->num_ids = num_modelseed_ids;
+    ms2js_data->length_strings = length_ms2js_strings;
+  }
+  if (success) {
+    ask_for = length_kg2js_strings;
+    kg2js_strings = (char*)calloc(one_l,ask_for);
+    if (kg2js_strings == NULL) {
+      success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for kg2js_strings\n",ask_for);
+      fflush(lfp);
+    }
+  }
+  if (success) {
+    kg2js_data->strings = kg2js_strings;
+    ask_for = num_kegg_ids * (sizeof(char *) << 1);
+    kegg_ids = (char **)calloc(one_l,ask_for);
+    if (kegg_ids == NULL) {
+      success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for kegg_ids\n",ask_for);
+      fflush(lfp);
+    }
+  }
+  if (success) {
+    kg2js_data->dictionary_ids = kegg_ids;
+    kg2js_data->json_ids = (char **)&kegg_ids[num_kegg_ids]; 
+    kg2js_data->num_ids = num_kegg_ids;
+    kg2js_data->length_strings = length_kg2js_strings;
+    /*
+      We need a list of sorted json id's for the json id's matching a kegg id.
+    */
+    ask_for = (int64_t)((num_kegg_ids << 1) + num_kegg_ids) * sizeof(char *);
+    json_ids = calloc(one_l,ask_for);
+    if (json_ids == NULL) {
+      success = 0;
+      fprintf(lfp,"sbml_alloc2: Error unable to allocate %ld bytes for json_ids and sort_json_scratch\n",ask_for);
+      fflush(lfp);
+    }
+  }
+  if (success) {
+    state->json_ids = json_ids;
+    state->sort_json_ids_scratch = (char*)&json_ids[num_kegg_ids];
   }
   return(success);
 }
