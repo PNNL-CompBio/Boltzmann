eac43d489029b1793a31fabb1d0c272f8f42b13c
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:32 -0800

    r4500 | dbaxter | 2017-09-20 09:16:06 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_oneway_data.c
    A    boltzmann_flatten_oneway_data.h

---
+++ boltzmann_flatten_oneway_data.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_FLATTEN_ONEWAY_DATA_H_
+#define _BOLTZMANN_FLATTEN_ONEWAY_DATA_H_ 1
+extern int boltzmann_flatten_oneway_data(struct state_struct *state,
+					 void *fstate, int direction,
+					 int *word_pos_p, FILE *lfp);
+#endif
