b18371a45efd9dce0ed9ee742c0582b7b482ee0c
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:04 -0800

    r2889 | dbaxter | 2014-10-15 16:50:49 -0700 (Wed, 15 Oct 2014) | 3 lines
    Added setting of kg2js_file field.
    M    boltzmann_set_filename_ptrs.c

--- boltzmann_set_filename_ptrs.c
+++ boltzmann_set_filename_ptrs.c
@@ -52,4 +52,5 @@ void boltzmann_set_filename_ptrs(struct state_struct *statep) {
   statep->compartment_file   = statep->pseudoisomer_file + max_file_name_len;
   statep->sbml_file          = statep->compartment_file + max_file_name_len;
   statep->ms2js_file         = statep->sbml_file + max_file_name_len;
+  statep->kg2js_file         = statep->ms2js_file + max_file_name_len;
 }
