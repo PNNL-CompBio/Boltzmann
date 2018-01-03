adae0b6a6ae923ce5b92fc54af625f64def36f5e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:35 -0800

    r3773 | dbaxter | 2016-12-25 17:59:17 -0800 (Sun, 25 Dec 2016) | 3 lines
    Documentation change.
    M    sbml_count_cmpts.c

--- sbml_count_cmpts.c
+++ sbml_count_cmpts.c
@@ -26,7 +26,7 @@ specific language governing permissions and limitations under the License.
 #include "sbml_count_cmpts.h"
 int sbml_count_cmpts(struct sbml2bo_struct *sbml_state) {
   /*
-    Called by: sbml_to_boltzmans, sbml2bo
+    Called by: sbml_to_boltzmann, sbml2bo
     Calls:     sbml_find_section, fopen, fprintf, fflush, fclose
   */
   char sbml_buffer_c[2048];
