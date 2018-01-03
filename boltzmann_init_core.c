673f8a7ce0eac3adf29a625320e279dbe08e4880
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:48 -0800

    r3979 | dbaxter | 2017-02-08 10:05:25 -0800 (Wed, 08 Feb 2017) | 3 lines
    Removed unused declarations.
    M    boltzmann_init_core.c

--- boltzmann_init_core.c
+++ boltzmann_init_core.c
@@ -29,25 +29,8 @@ int boltzmann_init_core(struct state_struct *boot_state, struct state_struct **f
 	       run_init
   */
   struct state_struct *state;
-  struct state_struct *stateq;
-  struct formation_energy_struct *formation_energies;
-  struct vgrng_state_struct *vgrng_state;
-  struct vgrng_state_struct *vgrng2_state;
-  double *dg0s;
-  double *free_energy;
-  double *activities;
-  int64_t vgrng_start;
-  int64_t i;
-
   int success;
-  int vgrng_start_steps;
-
   int print_output;
-  int padi;
-  
-  FILE *rxn_echo_fp;
-  FILE *bndry_flux_fp;
-  FILE *lfp;
 
   state   = boot_state;
   print_output = state->print_output;
