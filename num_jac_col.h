f79734d5129e1b22870b6170b1f6cedbe6481d9e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:23 -0800

    r3610 | dbaxter | 2016-11-01 10:48:11 -0700 (Tue, 01 Nov 2016) | 4 lines
    Reordered call arguments and added rowmax_p as an argument to
    also return the rowmax value (for aid in debugging)
    M    num_jac_col.h

--- num_jac_col.h
+++ num_jac_col.h
@@ -2,6 +2,7 @@
 #define _NUM_JAC_COL_H_ 1
 extern int num_jac_col(struct state_struct *state,
 	   	       int ny, int j,
+		       int *rowmax_p,
 		       double *y,
 		       double *f,
 		       double *delj_p,
@@ -12,8 +13,8 @@ extern int num_jac_col(struct state_struct *state,
 		       double *forward_rxn_likelihoods,
 		       double *reverse_rxn_likelihoods,
 		       double *dfdy_colj,
-		       double *absfdiffmax_p,
 		       double *absfvaluerm_p,
 		       double *absfdelrm_p,
+		       double *absfdiffmax_p,
 		       double *infnormdfdy_colj_p);
 #endif
