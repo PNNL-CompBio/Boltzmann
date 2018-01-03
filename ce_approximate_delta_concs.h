9a202270a57cc28f9a3368b33bfca6af73f5f2c4
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:58 -0800

    r4090 | dbaxter | 2017-02-27 13:36:27 -0800 (Mon, 27 Feb 2017) | 5 lines
    Replaced counts argument with concs.
    Removed forward_rxn_likelihoods, reverse_rxn_likelihoods, flux_scaling, and
    base_rxn arguments. These are now taken from state structure.
    M    ce_approximate_delta_concs.h

--- ce_approximate_delta_concs.h
+++ ce_approximate_delta_concs.h
@@ -1,10 +1,7 @@
 #ifndef _CE_APPROXIMATE_DELTA_CONCS_H_
 #define _CE_APPROXIMATE_DELTA_CONCS_H_ 1
 extern int ce_approximate_delta_concs(struct state_struct *state, 
-				      double *counts,
-				      double *forward_rxn_likelihoods,
-				      double *reverse_rxn_likelihoods, 
-				      double *flux, double flux_scaling,
-				      int base_rxn,
+				      double *concs,
+				      double *flux,
 				      int choice);
 #endif
