3f8b7a44f9d7be60eb256a9094dfae4e76e07200
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:57 -0800

    r4771 | dbaxter | 2017-11-08 15:29:51 -0800 (Wed, 08 Nov 2017) | 4 lines
    Added logic to avoid calling memcpy with 0 size,
    Bug fix list -> list1 in line 39.
    M    isort.c

--- isort.c
+++ isort.c
@@ -39,13 +39,15 @@ void isort(int n,int *list, int *scratch) {
 	if (l2 > step) {
 	  l2 = step;
 	}
-	imerge((int*)&list1[j],(int*)&list[j+step],(int*)&list2[j],l1,l2);
+	imerge((int*)&list1[j],(int*)&list1[j+step],(int*)&list2[j],l1,l2);
       } /* end for j */
       ln = n & (next_step - 1);
       if (ln <= step) {
 	move_size = ln * l_4;
 	k = n - ln;
-	memcpy((void*)&list2[k],(void*)&list1[k],move_size);
+	if (move_size > 0) {
+	  memcpy((void*)&list2[k],(void*)&list1[k],move_size);
+	}
       }
       itemp = list1;
       list1 = list2;
