e3c84440e4aa10082a39c9dcd8f15013b2fe529a
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:07 -0800

    r4820 | dbaxter | 2017-11-14 16:52:17 -0800 (Tue, 14 Nov 2017) | 4 lines
    Routine to sort pairs of elements in a vector putting the result in a
    different vector.
    A    dsort_pairs.c

---
+++ dsort_pairs.c
@@ -0,0 +1,27 @@
+#include "system_includes.h"
+#include "dsort_pairs.h"
+void dsort_pairs(int n,double *list, double *sorted_pairs) {
+  /*
+    Sort pairs of doubles into ascending order, with result
+    sorted pairs put into sorted_pairs.
+  */
+  double temp;
+  int i;
+  int nm1;
+  int n_odd;
+  int padi;
+  nm1 = n - 1;
+  for (i=0;i<nm1;i+=2) {
+    if (list[i] > list[i+1]) {
+      sorted_pairs[i] = list[i+1];
+      sorted_pairs[i+1] = list[i];
+    } else {
+      sorted_pairs[i] = list[i];
+      sorted_pairs[i+1] = list[i+1];
+    }
+  }
+  n_odd = n & 1;
+  if (n_odd) {
+    sorted_pairs[n-1] = list[n-1];
+  }
+}
