2574a72dd8c7a650c43a8e1807541c3531eb8ece
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:06 -0800

    r1559 | dbaxter | 2012-06-20 19:14:01 -0700 (Wed, 20 Jun 2012) | 3 lines
    binary search in a sorted list for the least upper bound index for a value.
    A    binary_search_l_u_b.c

---
+++ binary_search_l_u_b.c
@@ -0,0 +1,66 @@
+/* binary_search_l_u_b.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "binary_search_l_u_b.h"
+int binary_search_l_u_b (double *d, double v, int n) {
+  /*
+    find smallest j such that d[j] >= v;
+    assumes d[n-1] > v, for (0<=j<n);
+
+    Called by : boltzmann_run_sim
+    Calls"
+  */
+  int low;
+  int mid;
+  int high;
+  int result;
+
+  if (v <= d[0]) {
+    result = 0;
+  } else {
+    if (v > d[n-2]) {
+      result = n-1;
+    } else {
+      low = 0;
+      high = n-1;
+      mid = (low+high)>>1;
+      while (mid != low) {
+	if (d[mid] >= v) {
+	  high = mid;
+	} else {
+	  low = mid;
+	}
+	mid = (low+high)>>1;
+      }
+      result = high;
+    }
+  }
+  return (result);
+}
