b2272fa4146b36a85d4c089a16b4f05c47beb091
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:14 -0800

    r4873 | dbaxter | 2017-11-20 11:25:49 -0800 (Mon, 20 Nov 2017) | 3 lines
    Added stuff to print counts header too.
    M    ode_print_concs_header.c

--- ode_print_concs_header.c
+++ ode_print_concs_header.c
@@ -38,44 +38,93 @@ void ode_print_concs_header(struct state_struct *state) {
   char *molecules_text;
   char *compartment_text;
   char *ode_concs_file;
+  char *ode_counts_file;
   int i;
   int ci;
   int nu_molecules;
-  int padi;
+  int print_concs_or_counts;
+  int concs_opened;
+  int counts_opened;
   FILE *ode_concs_fp;
+  FILE *ode_counts_fp;
   FILE *lfp;
+  FILE *efp;
   nu_molecules     = state->nunique_molecules;
   cur_molecule     = state->sorted_molecules;
   cur_cmpts        = state->sorted_compartments;
   molecules_text   = state->molecules_text;
   compartment_text = state->compartment_text;
   ode_concs_file   = state->ode_concs_file;
+  ode_counts_file  = state->ode_counts_file;
+  print_concs_or_counts = state->print_concs_or_counts;
   lfp              = state->lfp;
+  concs_opened = 0;
+  counts_opened = 0;
 
-  ode_concs_fp = fopen(ode_concs_file,"w+");
-  state->ode_concs_fp  = ode_concs_fp;
-  if (ode_concs_fp == NULL) {
-    if (lfp) {
-      fprintf(lfp,"ode_print_concs_header: Error could not open %s\n",
-	      ode_concs_file);
-      fflush(lfp);
+  if (print_concs_or_counts & 2) {
+    ode_concs_fp = fopen(ode_concs_file,"w+");
+    state->ode_concs_fp  = ode_concs_fp;
+    if (ode_concs_fp == NULL) {
+      if (lfp) {
+	fprintf(lfp,"ode_print_concs_header: Error could not open %s\n",
+		ode_concs_file);
+	fflush(lfp);
+      }
+    } else {
+      concs_opened = 1;
     }
-  } else {
+  }
+  if (print_concs_or_counts & 1) {
+    ode_counts_fp = fopen(ode_counts_file,"w+");
+    state->ode_counts_fp = ode_counts_fp;
+    if (ode_counts_fp == NULL) {
+      if (lfp) {
+	fprintf(lfp,"ode_print_concs_header: Error could not open %s\n",
+		ode_counts_file);
+	fflush(lfp);
+    
+      }
+    } else {
+      counts_opened = 1;
+    }
+  }
+  if (concs_opened) {
     fprintf(ode_concs_fp,"Time");
+  }
+  if (counts_opened) {
+    fprintf(ode_counts_fp,"Time");
+  }
+  if (concs_opened || counts_opened) {
     for (i=0;i<nu_molecules;i++) {
       if ((cur_molecule->solvent == 0) || (cur_molecule->variable == 1)) {
       	ci = cur_molecule->c_index;
       	molecule = (char*)&molecules_text[cur_molecule->string];
-      	fprintf(ode_concs_fp,"\t%s",molecule);
+	if (concs_opened) {
+	  fprintf(ode_concs_fp,"\t%s",molecule);
+	}
+	if (counts_opened) {
+	  fprintf(ode_counts_fp,"\t%s",molecule);
+	}
       	if (ci > 0) {
-      		cur_cmpt   = (struct compartment_struct *)&(cur_cmpts[ci]);
-      		cmpt_string = (char*)&compartment_text[cur_cmpt->string];
-      		fprintf(ode_concs_fp,":%s",cmpt_string);
+	  cur_cmpt   = (struct compartment_struct *)&(cur_cmpts[ci]);
+	  cmpt_string = (char*)&compartment_text[cur_cmpt->string];
+	  if (concs_opened) {
+	    fprintf(ode_concs_fp,":%s",cmpt_string);
+	  }
+	  if (counts_opened) {
+	    fprintf(ode_counts_fp,":%s",cmpt_string);
+	  }
       	} 
       }
       cur_molecule += 1; /* caution address arithmetic.*/
     }
-    fprintf(ode_concs_fp,"\n");
-    fflush(ode_concs_fp);
-  }
+    if (concs_opened) {
+      fprintf(ode_concs_fp,"\n");
+      fflush(ode_concs_fp);
+    }
+    if (counts_opened) {
+      fprintf(ode_counts_fp,"\n");
+      fflush(ode_counts_fp);
+    }
+  } /* end if (counts_opened || concs_opened) */
 }
