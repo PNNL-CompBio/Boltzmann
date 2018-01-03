5e4d9d3c9ce7c6f21aea267a61900f5a49e8412b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:59 -0800

    r2818 | dbaxter | 2014-06-13 15:30:45 -0700 (Fri, 13 Jun 2014) | 3 lines
    Replaced sbml2bo_structs.h with boltzmann_structs.h
    M    sbml_lookup_reaction_attribute.c

--- sbml_lookup_reaction_attribute.c
+++ sbml_lookup_reaction_attribute.c
@@ -21,7 +21,7 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
 
-#include "sbml2bo_structs.h"
+#include "boltzmann_structs.h"
 
 #include "sbml_lookup_reaction_attribute.h"
 
