24ebbc35c0ceee3973650718148637af50765504
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:14 -0800

    r1845 | dbaxter | 2012-10-22 17:17:44 -0700 (Mon, 22 Oct 2012) | 3 lines
    Header file ofr print_boundary_flux.c
    A    print_boundary_flux.h

---
+++ print_boundary_flux.h
@@ -0,0 +1,23 @@
+/* print_boundary_flux.h
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+extern void print_boundary_flux(struct state_struct *state);
