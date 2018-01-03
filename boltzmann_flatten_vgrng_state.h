ba5fefcd8146d8841febba380c13b15699759525
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:33 -0800

    r4514 | dbaxter | 2017-09-20 09:31:14 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_vgrng_state.c
    A    boltzmann_flatten_vgrng_state.h

---
+++ boltzmann_flatten_vgrng_state.h
@@ -0,0 +1,8 @@
+#ifndef _BOLTZMANN_FLATTEN_VGRNG_STATE_H_
+#define _BOLTZMANN_FLATTEN_VGRNG_STATE_H_ 1
+extern int boltzmann_flatten_vgrng_state(int64_t *fvgrng_state,
+					 struct vgrng_state_struct *vgrng_state,
+					 int direction,
+					 int *vgrng_state_size);
+#endif
+
