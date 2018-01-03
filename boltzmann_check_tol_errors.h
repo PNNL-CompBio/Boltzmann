8e49d7d8401734581584509adc5b53a251087a6e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:51 -0800

    r4699 | dbaxter | 2017-11-07 17:56:38 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for boltzmann_check_tol_errors.c
    A    boltzmann_check_tol_errors.h

---
+++ boltzmann_check_tol_errors.h
@@ -0,0 +1,5 @@
+#ifndef _BOLTZAMANN_CHECK_TOL_ERRORS_H_
+#define _BOLTZAMANN_CHECK_TOL_ERRORS_H_ 1
+extern int boltzmann_check_tol_errors(int flag, void *cvode_mem,
+				      struct state_struct *state);
+#endif
