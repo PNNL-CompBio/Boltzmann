63be2a99114bcc3b7a9135bb8a3716f4a6793697
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:15 -0800

    r3254 | dbaxter | 2016-02-02 11:24:40 -0800 (Tue, 02 Feb 2016) | 3 lines
    Header file for parse_rxn_list_line.c
    A    parse_rxn_list_line.h

---
+++ parse_rxn_list_line.h
@@ -0,0 +1,4 @@
+#ifndef _PARSE_RXN_LIST_LINE_H_
+#define _PARSE_RXN_LIST_LINE_H_ 1
+int parse_rxn_list_line(struct state_struct *local_state, struct boot_state_struct *boot_state, int64_t rxn_file);
+#endif
