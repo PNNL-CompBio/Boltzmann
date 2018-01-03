c177eded5e2ba44626d2caf20093c414595a30c9
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:41 -0800

    r3888 | dbaxter | 2017-01-21 16:35:06 -0800 (Sat, 21 Jan 2017) | 4 lines
    Routine to print the boundary flux as per the net likelihoods for
    fixed concentration species.
    A    print_net_lklhd_bndry_flux.c

---
+++ print_net_lklhd_bndry_flux.c
@@ -0,0 +1,36 @@
+#include "boltzmann_structs.h"
+
+#include "print_net_lklhd_bndry_flux.h"
+void print_net_lklhd_bndry_flux(struct state_struct *state, double time) {
+  /*
+    Print the net likelihood boundary fluxes.
+    Called by: ode23tb
+    Calls: fopen, fprint, fflush
+  */
+  struct molecule_struct *molecule;
+  int64_t nunique_molecules;
+  double  *net_lklhd_bndry_flux;
+
+  int i;
+  int padi;
+
+  FILE *nl_bndry_flx_fp;
+  FILE *lfp;
+
+  nunique_molecules = state->nunique_molecules;
+  molecule  = state->sorted_molecules;
+  net_lklhd_bndry_flux = state->net_lklhd_bndry_flux;
+  nl_bndry_flx_fp  = state->nl_bndry_flx_fp;
+  if (nl_bndry_flx_fp != NULL) {
+    fprintf(nl_bndry_flx_fp,"%le",time);
+    for (i=0;i<nunique_molecules;i++) {
+      if (molecule->variable == 0) {
+	fprintf(nl_bndry_flx_fp,"\t%le",net_lklhd_bndry_flux[i]);
+      }
+      molecule += 1; /* Caution address arithmetic; */
+    } /* end for (i...) */
+    fprintf(nl_bndry_flx_fp,"\n");
+    fflush(nl_bndry_flx_fp);
+  } /* end if (nl_bndry_flux_fp != NULL) */
+}
+
