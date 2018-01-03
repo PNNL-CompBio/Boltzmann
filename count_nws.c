d1e04ada9f4fd48a2da4bc7263096941d1f4d5b1
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:31 -0800

    r2251 | dbaxter | 2013-03-20 17:29:11 -0700 (Wed, 20 Mar 2013) | 4 lines
    Removing includes also found in boltzmann_structs.h
    Added include for boltzmann_structs.h
    M    count_nws.c

--- count_nws.c
+++ count_nws.c
@@ -20,13 +20,7 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <float.h>
-#include <signal.h>
-#include <unistd.h>
+#include "boltzmann_structs.h"
 
 #include "count_nws.h"
 
