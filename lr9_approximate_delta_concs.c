6b944736700ab364a3569eff071ab8d482c1cf48
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:23 -0800

    r4963 | dbaxter | 2017-12-05 18:07:29 -0800 (Tue, 05 Dec 2017) | 4 lines
    Changed from computing sum_coeff on the fly to computing it in
    parse_reactions_file once, and with h2o coefficients in tact.
    M    lr9_approximate_delta_concs.c

--- lr9_approximate_delta_concs.c
+++ lr9_approximate_delta_concs.c
@@ -79,6 +79,9 @@ int lr9_approximate_delta_concs(struct state_struct *state,
   int64_t *rxn_ptrs;
   int64_t *molecule_indices;
   int64_t *rcoefficients;
+
+  int *coeff_sum;
+
   int num_species;
   int num_rxns;
 
@@ -117,6 +120,7 @@ int lr9_approximate_delta_concs(struct state_struct *state,
   activities       = state->activities;
   forward_lklhd    = state->ode_forward_lklhds;
   reverse_lklhd    = state->ode_reverse_lklhds;
+  coeff_sum        = state->coeff_sum;
   molecules_matrix = state->molecules_matrix;
   molecules_ptrs   = molecules_matrix->molecules_ptrs;
   rxn_indices      = molecules_matrix->reaction_indices;
@@ -198,7 +202,6 @@ int lr9_approximate_delta_concs(struct state_struct *state,
       rt = 1.0;
       tr = 1.0;
       tp = 1.0;
-      sum_coeff = 0;
       for (j=rxn_ptrs[i];j<rxn_ptrs[i+1];j++) {
 	mi = molecule_indices[j];
 	molecule = (struct molecule_struct *)&molecules[mi];
@@ -209,7 +212,6 @@ int lr9_approximate_delta_concs(struct state_struct *state,
 	/*
 	*/
 	klim = coefficients[j];
-	sum_coeff += klim;
 	conc_mi = concs[mi];
 	if (klim < 0) {
 	  for (k=0;k<(-klim);k++) {
@@ -226,6 +228,7 @@ int lr9_approximate_delta_concs(struct state_struct *state,
       keq_adj = 1.0;
       rkeq_adj = 1.0;
       multiplier = 1.0;
+      sum_coeff = coeff_sum[i];
       if (sum_coeff > 0) {
 	multiplier = recip_volume;
       } else {
