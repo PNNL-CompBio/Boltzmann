ed251d30959c23f4da13ba2982a6de71887a0d2b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:20 -0800

    r2009 | dbaxter | 2013-01-10 16:52:52 -0800 (Thu, 10 Jan 2013) | 5 lines
    Routine to extract unique molecules names from an istring list.
    The guts of unique_molecules.c also to be used in  building
    global molecules dictionary.
    A    unique_molecules_core.c

---
+++ unique_molecules_core.c
@@ -0,0 +1,114 @@
+/* unique_molecules_core.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "boltzmann_structs.h"
+
+#include "unique_molecules_core.h"
+int unique_molecules_core(int nzr,
+			  struct istring_elem_struct *sorted_molecules,
+			  int64_t *molecules_indices,
+			  char *molecules_text,
+			  int64_t *nunique_molecules,
+			  int64_t *sum_molecule_len,
+			  int64_t align_len,
+			  int64_t align_mask) {
+  /*
+    Remove duplicates from the sorted_molecules list
+    and set the column_indices fields in the
+    reactions_matrix appropriately.
+    Called by: boltzmann_init
+    Calls:     strcmp (intrinsic)
+  */
+  struct istring_elem_struct *cur_molecule;
+  struct istring_elem_struct *umolecules_next;
+  char *cstring;
+  char *sstring;
+  int64_t molecule_len;
+  int64_t m_size;
+  int64_t pad_size;
+  
+  int i;
+  int success;
+
+  int nu_molecules;
+  int padi;
+
+  int ni;
+  int cni;
+
+  success = 1;
+  molecule_len = (int64_t)0;
+  /* loop over sorted molecules. */
+  nu_molecules = 0;
+  molecules_indices[sorted_molecules->m_index] = nu_molecules;
+  sorted_molecules->m_index = 0;
+  cur_molecule = sorted_molecules;
+  cstring = NULL;
+  if (cur_molecule->string >= 0) {
+    cstring = (char *)&molecules_text[cur_molecule->string];
+    m_size = strlen(cstring);
+    pad_size = (align_len - (m_size & align_mask)) & align_mask;
+    molecule_len += (m_size + pad_size);
+  }
+  /*
+    This translation has already been done in translate_compartments.
+  cur_molecule->c_index = compartment_indices[cur_molecule->c_index];
+  */
+  cni = sorted_molecules->c_index;
+  sorted_molecules += 1; /* Caution address arithmetic. */
+  umolecules_next  = sorted_molecules;
+  for (i=1;i<nzr;i++) {
+    ni = sorted_molecules->c_index;
+    sstring = NULL;
+    if (sorted_molecules->string >= 0) {
+      sstring = (char *)&molecules_text[sorted_molecules->string];
+    }
+    if ((ni != cni)  ||
+	(strcmp(sstring,cstring) != 0)) {
+      cstring = sstring;
+      m_size = strlen(cstring);
+      pad_size = (align_len - (m_size & align_mask)) & align_mask;
+      molecule_len += (int64_t)(m_size + pad_size);
+      nu_molecules += 1;
+      cur_molecule = sorted_molecules;
+      umolecules_next->string = cur_molecule->string;
+      umolecules_next->m_index = nu_molecules;
+      umolecules_next->c_index = ni;
+      cni = ni;
+      umolecules_next += 1; /* Caution address arithmetic. */
+    }
+    molecules_indices[sorted_molecules->m_index] = nu_molecules;
+    sorted_molecules += 1; /* Caution address arithmetic. */
+  }
+  *nunique_molecules = nu_molecules + 1;
+  *sum_molecule_len = molecule_len;
+
+  return(success);
+}
