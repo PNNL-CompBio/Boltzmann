a4d67e1ffb444dc27f0ea16fca9434ebe3a6c1b8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:17 -0800

    r4334 | dbaxter | 2017-06-19 13:44:10 -0700 (Mon, 19 Jun 2017) | 3 lines
    Header file fore ode23tb_normyp_o_wt.c
    A    ode23tb_normyp_o_wt.h

---
+++ ode23tb_normyp_o_wt.h
@@ -0,0 +1,5 @@
+#ifndef _ODE23TB_NORMYP_O_WT_H_
+#define _ODE23TB_NORMYP_O_WT_H_ 1
+extern double ode23tb_normyp_o_wt(int norm_control, int ny, double normyp,
+				  double *yp, double *wt);
+#endif
