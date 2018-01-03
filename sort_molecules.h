a3909c350eeffbaefbaaa20af18805b88831c791
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:17 -0800

    r1936 | dbaxter | 2012-12-07 17:53:14 -0800 (Fri, 07 Dec 2012) | 3 lines
    Added molecules_text argument for changing char* fields to int64_t offsets.
    M    sort_molecules.h

--- sort_molecules.h
+++ sort_molecules.h
@@ -22,4 +22,5 @@ specific language governing permissions and limitations under the License.
 ******************************************************************************/
 extern int sort_molecules(struct istring_elem_struct **unsorted_istrings,
 			  struct istring_elem_struct **sorted_istrings,
+			  char *molecules_text,
 			  int n);
