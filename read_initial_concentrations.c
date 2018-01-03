08aedccdc122824fa26a26dec6d60a2d49d00c5e
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:05 -0800

    r1510 | dbaxter | 2012-06-13 16:04:53 -0700 (Wed, 13 Jun 2012) | 3 lines
    species -> molecules.
    M    read_initial_concentrations.c

--- read_initial_concentrations.c
+++ read_initial_concentrations.c
@@ -29,7 +29,7 @@ specific language governing permissions and limitations under the License.
 
 #include "boltzmann_structs.h"
 
-#include "species_lookup.h"
+#include "molecules_lookup.h"
 
 #include "read_initial_concentrations.h"
 int read_initial_concentrations(struct state_struct *state) {
@@ -37,15 +37,15 @@ int read_initial_concentrations(struct state_struct *state) {
     Read the concs.in file for initial concentrations, and
     set the concentrations array.
     Called by: boltzmann
-    Calls:     species_lookup
+    Calls:     molecules_lookup
   */
   double  conc;
   double *concs;
-  int64_t species_buff_len;
+  int64_t molecules_buff_len;
   int success;
   int nzr;
 
-  int nu_species;
+  int nu_molecules;
   int i;
 
   int nscan;
@@ -56,38 +56,38 @@ int read_initial_concentrations(struct state_struct *state) {
 
   int si;
   int padi;
-  char *species_buffer;
-  char *species_name;
+  char *molecules_buffer;
+  char *molecule_name;
   char *fgp;
   
   FILE *conc_fp;
-  nu_species = state->unique_species;
-  species_buff_len = state->rxn_buff_len;
-  species_buffer   = state->rxn_buffer;
-  species_name     = state->species_name;
+  nu_molecules = state->unique_molecules;
+  molecules_buff_len = state->rxn_buff_len;
+  molecules_buffer   = state->rxn_buffer;
+  molecule_name     = state->molecule_name;
   concs            = state->concentrations;
   success = 1;
-  for (i=0;i<nu_species;i++) {
+  for (i=0;i<nu_molecules;i++) {
     concs[i] = -1.0;
   }
   conc_fp = fopen("concs.in","r");
   if (conc_fp) {
     while (!feof(conc_fp)) {
-      fgp = fgets(species_buffer,species_buff_len,conc_fp);
+      fgp = fgets(molecules_buffer,molecules_buff_len,conc_fp);
       if (fgp) {
-	ll = strlen(species_buffer);
-	nscan = sscanf(species_buffer,"%s %le",species_name,&conc);
+	ll = strlen(molecules_buffer);
+	nscan = sscanf(molecules_buffer,"%s %le",molecule_name,&conc);
 	if (nscan == 2) {
-	  if (strcmp(species_name,"*") == 0) {
+	  if (strcmp(molecule_name,"*") == 0) {
 	    state->default_initial_conc = conc;
 	  } else {
-	    si = species_lookup(species_name,state);
-	    if ((si >=0) && si < nu_species) {
+	    si = molecules_lookup(molecule_name,state);
+	    if ((si >=0) && si < nu_molecules) {
 	      concs[si] = conc;
 	    } else {
 	      fprintf(stderr,"read_initial_concentrations: Error "
-		      "unrecognized species in conc.in was %s\n",
-		      species_name);
+		      "unrecognized molecules in conc.in was %s\n",
+		      molecule_name);
 	      fflush(stderr);
 	    }
 	  }
@@ -97,12 +97,12 @@ int read_initial_concentrations(struct state_struct *state) {
   } else {
     fprintf(stderr,
 	    "read_initial_concentrations: Warning unable to open concs.in ."
-	    " Will use %le for all species\n",state->default_initial_conc);
+	    " Will use %le for all molecules\n",state->default_initial_conc);
     fflush(stderr);
   }
   if (success) {
     conc = state->default_initial_conc;
-    for (i=0;i<nu_species;i++) {
+    for (i=0;i<nu_molecules;i++) {
       if (concs[i] < 0.0) {
 	concs[i] = conc;
       }
