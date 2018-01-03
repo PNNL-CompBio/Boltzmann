40688e322102ce93a6340a1817589f7f41fb20ac
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:35 -0800

    r4521 | dbaxter | 2017-09-20 09:37:56 -0700 (Wed, 20 Sep 2017) | 3 lines
    Added #ifndef logic.
    M    count_nws.h

--- count_nws.h
+++ count_nws.h
@@ -20,4 +20,7 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
+#ifndef _COUNT_NWS_H_
+#define _COUNT_NWS_H_ 1
 extern int count_nws(char *line);
+#endif
