054b903e5a382d945fd2e13329c3b8533dc0e03f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:16 -0800

    r4314 | dbaxter | 2017-06-14 12:02:45 -0700 (Wed, 14 Jun 2017) | 4 lines
    allocate space for printing reaction_view data. This space used to be
    allocated as part of flatten_state
    A    alloc9.c

---
+++ alloc9.c
@@ -0,0 +1,128 @@
+#include "boltzmann_structs.h"
+#include "alloc9.h"
+int alloc9(struct state_struct *state) {
+  /*
+    Allocate workspace needed for printing reaction view pieces in
+    the record loop of boltzmann_run.
+    Allocates space for and sets
+         no_op_likelihood, 
+         rxn_view_likelihoods,
+	 rev_rxn_view_likelihoods,
+	 rxn_fire,
+	 rxn_mat_row 
+    pointers in state
+
+    Called by: run_init
+    Calls:     calloc, fprintf, fflush
+  */
+  double  *no_op_likelihood;
+  double  *rxn_view_likelihoods;
+  double  *rev_rxn_view_likelihoods;
+  int64_t *rxn_fire;
+  int64_t usage;
+  int     *rxn_mat_row;
+  int64_t rxn_view_hist_length;
+  int64_t number_reactions;
+  int64_t number_species;
+  int64_t align_len;
+  int64_t align_mask;
+  int64_t data_pad;
+  int64_t ask_for;
+  int64_t one_l;
+
+  int     success;
+  int     padi;
+
+  FILE    *lfp;
+  FILE    *efp;
+  
+  align_len            = state->align_len;
+  align_mask           = state->align_mask;
+  rxn_view_hist_length = state->rxn_view_hist_length;
+  number_reactions     = state->number_reactions;
+  number_species       = state->nunique_molecules;
+  lfp                  = state->lfp;
+  success              = 1;
+  usage = (int64_t)0;
+  one_l = (int64_t)1;
+  
+  ask_for = rxn_view_hist_length * sizeof(double);
+  data_pad = (align_len - (ask_for & align_mask)) & align_mask;
+  ask_for += data_pad;
+  usage   += ask_for;
+  no_op_likelihood = (double *)calloc(one_l,ask_for);
+  if (no_op_likelihood == NULL) {
+    success = 0;
+    if (lfp) {
+      fprintf(lfp,"alloc9: Error unable to allocate %lld bytes for no_op_likelihood, turning off printing\n", ask_for);
+      fflush(lfp);
+    }
+    state->print_output = 0;
+  }
+  if (success) {
+    state->no_op_likelihood = no_op_likelihood;
+    ask_for = rxn_view_hist_length * sizeof(double) * number_reactions;
+    data_pad = (align_len - (ask_for & align_mask)) & align_mask;
+    ask_for += data_pad;
+    usage += ask_for;
+    rxn_view_likelihoods = (double *)calloc(one_l,ask_for);
+    if (rxn_view_likelihoods == NULL) {
+      success = 0;
+      if (lfp) {
+	fprintf(lfp,"alloc9: Error unable to allocate %lld bytes for rxn_view_likelihoods, turning off printing\n", ask_for);
+	fflush(lfp);
+      }
+      state->print_output = 0;
+    }
+  }
+  if (success) {
+    state->rxn_view_likelihoods = rxn_view_likelihoods;
+    usage+= ask_for;
+    rev_rxn_view_likelihoods = (double *)calloc(one_l,ask_for);
+    if (rev_rxn_view_likelihoods == NULL) {
+      success = 0;
+      if (lfp) {
+	fprintf(lfp,"alloc9: Error unable to allocate %lld bytes for rev_rxn_view_likelihoods, turning off printing\n", ask_for);
+	fflush(lfp);
+      }
+      state->print_output = 0;
+    }
+  }
+  if (success) {
+    state->rev_rxn_view_likelihoods = rev_rxn_view_likelihoods;
+    ask_for = (number_reactions + 1) * 2 * sizeof(int64_t);
+    data_pad = (align_len - (ask_for & align_mask)) & align_mask;
+    ask_for += data_pad;
+    usage += ask_for;
+    rxn_fire = (int64_t*)calloc(one_l,ask_for);
+    if (rxn_fire == NULL) {
+      success = 0;
+      if (lfp) {
+	fprintf(lfp,"alloc9: Error unable to allocate %lld bytes for rxn_fire, turning off printing\n", ask_for);
+	fflush(lfp);
+      }
+      state->print_output = 0;
+    }
+  }
+  if (success) {
+    state->rxn_fire = rxn_fire;
+    ask_for = number_species * sizeof(int);
+    data_pad = (align_len - (ask_for & align_mask)) & align_mask;
+    ask_for += data_pad;
+    usage += ask_for;
+    rxn_mat_row = (int*)calloc(one_l,ask_for);
+    if (rxn_mat_row == NULL) {
+      success = 0;
+      if (lfp) {
+	fprintf(lfp,"alloc9: Error unable to allocate %lld bytes for rxn_mat_row, turning off printing\n", ask_for);
+	fflush(lfp);
+      }
+      state->print_output = 0;
+    }
+  }
+  if (success) {
+    state->rxn_mat_row = rxn_mat_row;
+  }
+  state->usage += usage;
+  return(success);
+}
