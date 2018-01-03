6e064d9ef2e49209287646b4d9248d5f70885307
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:27 -0800

    r5008 | dbaxter | 2017-12-13 18:20:12 -0800 (Wed, 13 Dec 2017) | 4 lines
    Added boltzmann_cvodes_headers.h and cvodes_params_struct.h includes.
    and removed blas.h include.
    M    build_newton_matrix.c

--- build_newton_matrix.c
+++ build_newton_matrix.c
@@ -1,5 +1,7 @@
 #include "boltzmann_structs.h"
-#include "blas.h"
+#include "boltzmann_cvodes_headers.h"
+#include "cvodes_params_struct.h"
+/*#include "blas.h"*/
 #include "build_newton_matrix.h"
 int  build_newton_matrix(struct state_struct *state,
 			 double gamma,
