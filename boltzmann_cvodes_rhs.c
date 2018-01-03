22739bf7b9be49d8e59c6431debeec4a8f0b4191
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:50 -0800

    r4679 | dbaxter | 2017-11-07 17:29:00 -0800 (Tue, 07 Nov 2017) | 3 lines
    Cvodes interface to approximate_delta_concs.
    A    boltzmann_cvodes_rhs.c

---
+++ boltzmann_cvodes_rhs.c
@@ -0,0 +1,24 @@
+#include "boltzmann_structs.h"
+#include "boltzmann_cvodes_headers.h"
+#include "approximate_delta_concs.h"
+#include "boltzmann_cvodes_rhs.h"
+int boltzmann_cvodes_rhs(double t, N_Vector y, N_Vector y_dot,
+			 void *user_data) {
+  /*
+    Called by: Cvode, boltzmann_cvodes
+    Calls:     approximate_delta_concs
+  */
+  struct state_struct *state;
+  
+  int choice;
+  int ret_code;
+  double *concs;
+  double *flux;
+  state = (struct state_struct *)user_data;
+  choice = state->delta_concs_choice;
+  concs = NV_DATA_S(y);
+  flux  = NV_DATA_S(y_dot);
+  ret_code = 0;
+  approximate_delta_concs(state, concs, flux, choice);
+  return(ret_code);
+}
