44f1f884e41c09c0a6f15c7faabb07b1ac8f83a0
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:25 -0800

    r2114 | dbaxter | 2013-02-26 14:54:36 -0800 (Tue, 26 Feb 2013) | 3 lines
    Changed type of super_statep argument to super_state_struct *.
    M    boltzmann_boot_check.h

--- boltzmann_boot_check.h
+++ boltzmann_boot_check.h
@@ -20,4 +20,5 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int boltzmann_boot_check(int64_t *super_statep, FILE *lfp);
+extern int boltzmann_boot_check(struct super_state_struct *super_statep, 
+				FILE *lfp);
