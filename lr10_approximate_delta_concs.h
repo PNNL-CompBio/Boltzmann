1b339b6401fcb6d2cc2db8cb5d05b98124521a32
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:58 -0800

    r4775 | dbaxter | 2017-11-09 09:51:31 -0800 (Thu, 09 Nov 2017) | 3 lines
    Header file for lr10_approximate_delta_concs.c
    A    lr10_approximate_delta_concs.h

---
+++ lr10_approximate_delta_concs.h
@@ -0,0 +1,7 @@
+#ifndef _LR10_APPROXIMATE_DELTA_CONCS_H_
+#define _LR10_APPROXIMATE_DELTA_CONCS_H_ 1
+extern int lr10_approximate_delta_concs(struct state_struct *state,
+					double *concs,
+					double *flux,
+					int choice);
+#endif
