f88883448c4fca19199e44e78fabe82397352368
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:00 -0800

    r4137 | dbaxter | 2017-04-14 14:05:34 -0700 (Fri, 14 Apr 2017) | 14 lines
    Removed formation_energies struct as most fields were in state anyway.
    Added min_molecule_dg0tf field to state to accomodate.
    Commented out calls to alloc6.
    Modified calls to compute_molecule_dg0tfs,
      compute_molecular_partition_probability,
        (and commented it out as "molecule_probabilities" is not used
         elsewhere except in compute_chemical_potential which is also unused),
      compute_chemical_potential
        (and commented it out as molecule_chemical_potentials is not used
         elsewhere).
      compute_reaction_dg0,
      and unalloc6
    M    compute_standard_energies.c

--- compute_standard_energies.c
+++ compute_standard_energies.c
@@ -25,16 +25,20 @@ specific language governing permissions and limitations under the License.
 #include "size_pseudoisomer_file.h"
 #include "alloc5.h"
 #include "parse_pseudoisomer_dg0f_file.h"
+/*
 #include "alloc6.h"
+*/
 #include "compute_molecule_dg0tfs.h"
+/*
 #include "compute_molecular_partition_probability.h"
 #include "compute_chemical_potential.h"
+*/
 #include "compute_reaction_dg0.h"
 #include "unalloc6.h"
 
+
 #include "compute_standard_energies.h"
-int compute_standard_energies(struct state_struct *state,
-		       struct formation_energy_struct **formation_energies_p) {
+int compute_standard_energies(struct state_struct *state) {
   /*
     Called by: enery_init
     Calls size_pseudoisomer_file,
@@ -47,111 +51,84 @@ int compute_standard_energies(struct state_struct *state,
 	  compute_reaction_dg0
 	  unalloc6
   */
-  struct formation_energy_struct *formation_energies;
+  struct pseudoisomer_struct *pseudoisomers;
+  char *pseudoisomer_strings;
   int64_t num_pseudoisomers;
   int64_t length_pseudoisomer_strings;
+  void   *pointers[4];
   int success;
-  success = 1;
-  if (*formation_energies_p == NULL) {
+  int nu_molecules;
+  /*
+  FILE *lfp;
+  FILE *efp;
+  lfp = state->lfp;
+  nu_molecules = state->nunique_molecules;
+  */
+  /*
+    First determine the size of the pseudoisomers file.
+  */
+  success = size_pseudoisomer_file(state,
+				   &num_pseudoisomers,
+				   &length_pseudoisomer_strings);
+  if (success) {
     /*
-      Formation_energies_p is null means the pseudoisomer data base has
-      not yet been initialized.
-      First determine the size of the pseudoisomers file.
+      Now allocate the formation_energies struct and its fields 
+      needed to parse and sort the pseudoisomer file.
     */
-    success = size_pseudoisomer_file(state,
-				     &num_pseudoisomers,
-				     &length_pseudoisomer_strings);
-    if (success) {
-      /*
-	Now allocate the formation_energies struct and its fields 
-	needed to parse and sort the pseudoisomer file.
-      */
-      success = alloc5(num_pseudoisomers,
-		       length_pseudoisomer_strings,
-		       state->align_len,
-		       &state->usage,
-		       &formation_energies);
-    }
-    if (success) {
-      /*
-	Read in formation energies from pseudoisomer file.
-      */
-      *formation_energies_p = formation_energies;
-      formation_energies->num_pseudoisomers = num_pseudoisomers;
-      formation_energies->length_pseudoisomer_strings = 
-	length_pseudoisomer_strings;
-      success = parse_pseudoisomer_dg0f_file(formation_energies->pseudoisomers,
-					     formation_energies->pseudoisomer_strings,
-					     state->pseudoisomer_file,
-					     num_pseudoisomers,
-					     state->align_len);
-    } 
-    if (success) {
-      /*
-	Sort the pseudoisomer data by json_cpd_name, priority and dg0f value.
-      success = sort_pseudoisomers(formation_energies);
-      */
-    }
-    /*
-    if (success  && formation_energies->pseudoisomer_scratch) {
-      free(formation_energies->pseudoisomer_scratch);
-    }
-    */
-  } else {
-    formation_energies = *formation_energies_p;
+    success = alloc5(num_pseudoisomers,
+		     length_pseudoisomer_strings,
+		     state->align_len,
+		     &state->usage,
+		     pointers);
   }
   if (success) {
     /*
-      Now we need to allocate space for computing the reaction dg0fs.
-      First we need to fill the relevant fields of the formation_energies
-      struct from the state struct.
+      Read in formation energies from pseudoisomer file.
     */
-    formation_energies->reactions           = state->reactions;
-    formation_energies->reactions_matrix    = state->reactions_matrix;
-    formation_energies->sorted_molecules    = state->sorted_molecules;
-    formation_energies->current_counts      = state->current_counts;
-    formation_energies->number_reactions    = state->number_reactions;
-    formation_energies->nunique_molecules   = state->nunique_molecules;
-    formation_energies->align_len           = state->align_len;
-    formation_energies->align_mask          = state->align_mask;
-    formation_energies->max_filename_len    = state->max_filename_len;
-    formation_energies->use_pseudoisomers   = state->use_pseudoisomers;
-    formation_energies->print_output        = state->print_output;
-
-
-    formation_energies->ideal_gas_r         = state->ideal_gas_r;
-    formation_energies->temp_kelvin         = state->temp_kelvin;
-    formation_energies->rt                  = state->rt;
-    formation_energies->m_r_rt              = state->m_r_rt;
-    formation_energies->m_rt                = state->m_rt;
-    formation_energies->cals_per_joule      = state->cals_per_joule;
-    formation_energies->joules_per_cal      = state->joules_per_cal;
-    formation_energies->ph                  = state->ph;
-    formation_energies->ionic_strength      = state->ionic_strength;
-    formation_energies->molecules_text      = state->molecules_text;
-    formation_energies->molecule_dg0tfs     = state->molecule_dg0tfs;
-    formation_energies->molecule_probabilities = state->molecule_probabilities;
-    formation_energies->molecule_chemical_potentials = 
-      state->molecule_chemical_potentials;
-
-    formation_energies->log_fp              = state->lfp;
-
-    success = alloc6(formation_energies,&state->usage);
-  }
-  if (success) {
-    success = compute_molecule_dg0tfs(formation_energies);
-  }
-  if (success) {
-    success = compute_molecular_partition_probability(formation_energies);
-  }
-  if (success) {
-    success = compute_chemical_potential(formation_energies);
+    pseudoisomers = (struct pseudoimser_struct *)pointers[0];
+    pseudoisomer_strings = (char *)pointers[1];
+    success = parse_pseudoisomer_dg0f_file(pseudoisomers,
+					   pseudoisomer_strings,
+					   state->pseudoisomer_file,
+					   num_pseudoisomers,
+					   state->align_len);
   } 
+  /*
   if (success) {
-    success = compute_reaction_dg0(formation_energies);
+    success = alloc6(nu_molecules,
+                     (void**)&pointers[2],&state->usage,lfp);
   }
   if (success) {
-    success = unalloc6(formation_energies);
+    //Sort the pseudoisomer data by json_cpd_name, priority and dg0f value.
+    success = sort_pseudoisomers(num_pseudoisomers,pointers);
+  }
+  */
+  if (success) {
+    success = compute_molecule_dg0tfs(state,
+				      pseudoisomers,
+				      pseudoisomer_strings,
+				      num_pseudoisomers);
+  }
+  if (success) {
+    success = unalloc6(2,pointers);
+    /*
+    success = unalloc6(2,(void**)&pointers[2]);
+    */
+  }
+  /*
+    // These two calls are coupled as compute_chemical_potential
+    // needs the molecule_probabilities.
+    // However molecule_probabilities and molecule_chemical_potentials
+    // are not currently used anywhwere else so we comment these out.
+  if (success) {
+    success = compute_molecular_partition_probability(state);
+  }
+  if (success) {
+    success = compute_chemical_potential(state);
+  } 
+  */
+  if (success) {
+    success = compute_reaction_dg0(state);
   }
   return(success);
 }
