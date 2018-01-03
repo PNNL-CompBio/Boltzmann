0263996577d0b8e5933702db0fdd397f7c25b5ad
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:53 -0800

    r4725 | dbaxter | 2017-11-07 18:21:41 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for boltzmann_print_cvode_error.h
    A    boltzmann_print_cvode_error.h

---
+++ boltzmann_print_cvode_error.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_PRINT_CVODE_ERROR_H_
+#define _BOLTZMANN_PRINT_CVODE_ERROR_H_ 1
+extern void boltzmann_print_cvode_error(int flag,
+					void *cvode_mem,  
+					struct state_struct *state);
+#endif
