085d4e1c1aec5d20ff2e8d3ba39673bbb273e71d
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:30 -0800

    r5036 | dbaxter | 2017-12-13 18:38:34 -0800 (Wed, 13 Dec 2017) | 4 lines
    Put stuff in to use cvodes_params->p for ke when compute_sensitivities is
    1 and ode_solver_choice is 1.
    M    lr10_approximate_delta_concs.c

--- lr10_approximate_delta_concs.c
+++ lr10_approximate_delta_concs.c
@@ -1,4 +1,6 @@
 #include "boltzmann_structs.h"
+#include "boltzmann_cvodes_headers.h"
+#include "cvodes_params_struct.h"
 #include "get_counts.h"
 #include "update_regulations.h"
 #include "lr10_approximate_delta_concs.h"
@@ -38,6 +40,7 @@ int lr10_approximate_delta_concs(struct state_struct *state,
     choice                      IOI   Not used by this routine.
 
   */
+  struct  cvodes_params_struct *cvodes_params;
   struct  molecule_struct *molecules;
   struct  molecule_struct *molecule;
   /*
@@ -93,6 +96,9 @@ int lr10_approximate_delta_concs(struct state_struct *state,
   int use_regulation;
   int count_or_conc;
 
+  int compute_sensitivities;
+  int ode_solver_choice;
+
   FILE *lfp;
   FILE *efp;
   /*
@@ -127,6 +133,16 @@ int lr10_approximate_delta_concs(struct state_struct *state,
   counts           = state->ode_counts;
   conc_to_count    = state->conc_to_count;
   use_regulation   = state->use_regulation;
+  ode_solver_choice = state->ode_solver_choice;
+  compute_sensitivities = state->compute_sensitivities;
+  if ((ode_solver_choice == 1) && compute_sensitivities) {
+    cvodes_params = state->cvodes_params;
+    ke = cvodes_params->p;
+    rke = cvodes_params->rp;
+    for (i=0;i<num_rxns;i++) {
+      rke[i] = 1.0/ke[i];
+    }
+  }
   /*
   recip_avogadro   = state->recip_avogadro;
   */
