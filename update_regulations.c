fc2708a72d25096bad2691e92747360dd6309ee9
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:25 -0800

    r4420 | dbaxter | 2017-07-11 07:41:54 -0700 (Tue, 11 Jul 2017) | 6 lines
    Added counts_or_concs and count_or_conc arguments,
    counts_or_concs is a vector of counts if count_or_conc = 1, of
    concentrations if count_or_conc = 0.
    Updated call to "update_regulation"
    M    update_regulations.c

--- update_regulations.c
+++ update_regulations.c
@@ -2,9 +2,13 @@
 
 #include "update_regulation.h"
 #include "update_regulations.h"
-int update_regulations(struct state_struct * state) {
+int update_regulations(struct state_struct * state, double *counts_or_concs,
+		       int count_or_conc) {
   /*
-    Update all the regulated activities.
+    Update all the regulated activities, based on the counts_or_concs
+    vector of counts (if count_or_conc == 1) or concentrations if 
+    (count_or_conc != 1).
+
     Called by: candidate_rxn, 
                compute_delta_g_forward_entropy_free_energy
     Calls:     update_regulation
@@ -16,7 +20,7 @@ int update_regulations(struct state_struct * state) {
   number_reactions = (int)state->number_reactions;
   success          = 1;
   for (rxn = 0;((rxn < number_reactions) && success);rxn++) {
-    success= update_regulation(state,rxn);
+    success= update_regulation(state,rxn,counts_or_concs,count_or_conc);
   }
   return(success);
 }
