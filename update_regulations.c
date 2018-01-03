282939af5eabdc62ea79bef520a48a9d021d5dcc
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:13 -0800

    r4870 | dbaxter | 2017-11-20 11:22:54 -0800 (Mon, 20 Nov 2017) | 3 lines
    Dcoumentation change.
    M    update_regulations.c

--- update_regulations.c
+++ update_regulations.c
@@ -11,6 +11,12 @@ int update_regulations(struct state_struct * state, double *counts_or_concs,
 
     Called by: candidate_rxn, 
                compute_delta_g_forward_entropy_free_energy
+	       lr8_approximate_delta_concs,
+	       lr9_approximate_delta_concs,
+	       lr10_approximate_delta_concs,
+	       lr11_approximate_delta_concs,
+	       lr12_approximate_delta_concs,
+	       lr8_approximate_jacobian,
     Calls:     update_regulation
   */
   int number_reactions;
