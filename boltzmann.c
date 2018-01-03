af201f07554d27d13dcb9c498559a8f5241e0db5
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:31 -0800

    r4484 | dbaxter | 2017-09-19 16:08:18 -0700 (Tue, 19 Sep 2017) | 3 lines
    Just a formattin change on #include line that also started a comment.
    M    boltzmann.c

--- boltzmann.c
+++ boltzmann.c
@@ -20,7 +20,8 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-#include "boltzmann_structs.h"/*
+#include "boltzmann_structs.h"
+/*
   Maybe only for linux systems.
 */
 #ifdef LIBUNWIND
