4ce8c87021508d7e1d9e3886bae30fde4cb750c4
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:43 -0800

    r3907 | dbaxter | 2017-01-24 14:57:38 -0800 (Tue, 24 Jan 2017) | 3 lines
    Added net_likelihood argument.
    M    print_net_likelihoods.h

--- print_net_likelihoods.h
+++ print_net_likelihoods.h
@@ -1,4 +1,6 @@
 #ifndef _PRINT_NET_LIKELIHOODS_H_
 #define _PRINT_NET_LIKELIHOODS_H_ 1
-extern void print_net_likelihoods(struct state_struct *state, double time);
+extern void print_net_likelihoods(struct state_struct *state, 
+				  double *net_likelihood,
+				  double time);
 #endif
