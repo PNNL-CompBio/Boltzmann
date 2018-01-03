731359a905784f111de9d88f2eb1616ff59f22f2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:31 -0800

    r5054 | dbaxter | 2017-12-27 10:45:49 -0800 (Wed, 27 Dec 2017) | 3 lines
    Header file for ode_test_steady_state.c
    A    ode_test_steady_state.h

---
+++ ode_test_steady_state.h
@@ -0,0 +1,7 @@
+#ifndef _ODE_TEST_STEADY_STATE_H_
+#define _ODE_TEST_STEADY_STATE_H_ 1
+extern int ode_test_steady_state(struct state_struct *state,
+				 int ny,
+				 double *y,
+				 double *f);
+#endif
