1e4598f0b915910b04a6efa1c502e43600334ece
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:16 -0800

    r3527 | dbaxter | 2016-08-31 14:01:36 -0700 (Wed, 31 Aug 2016) | 4 lines
    Routine to update reaction likelilhoods without also getting their logs.
    Used by the ode23tb and children routines.
    A    update_rxn_likelihoods.c

---
+++ update_rxn_likelihoods.c
@@ -0,0 +1,47 @@
+#include "boltzmann_structs.h"
+#include "rxn_likelihoods.h"
+#include "update_rxn_likelihoods.h"
+int update_rxn_likelihoods(struct state_struct *state, double *counts,
+			   double *forward_rxn_likelihood,
+			   double *reverse_rxn_likelihood) {
+  /*
+    Update the forward_rxn_likelihood, and reverse_rxn_likelihood
+    vectors input counts field.
+
+    Called by: deq_run, approximate_fluxes
+    Calls      rxn_likelihoods
+
+    Arguments:
+    Name        TMF       Description
+    state       G*I       pointer to the boltzmann state structure.
+                          Used fields are :
+                             number_reactions
+			     reactions and its subfields
+    counts      D*I       Vector of length nunique_molecules with 
+                          molecule counts for each species.
+    forward_rxn_likelihood
+                D*O       Vector of length number_reactions of the
+		          forward reaction likelihoods
+
+    reverse_rxn_likelihood
+                D*O       Vector of length number_reactions of the
+		          reverse reaction likelihoods
+  */
+  double *current_counts;
+  int success;
+  int forward;
+
+  int reverse;
+  int padi;
+  
+  success       		   = 1;
+  forward       		   = 1;
+  reverse       		   = -1;
+  /*
+  */
+  rxn_likelihoods(counts,forward_rxn_likelihood,
+		  state,forward);
+  rxn_likelihoods(counts,reverse_rxn_likelihood,
+		  state,reverse);
+  return(success);
+}
