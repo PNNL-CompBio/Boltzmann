5db957c071b885799e887f153c291f7a1b98f9f4
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:10 -0800

    r3171 | dbaxter | 2016-01-27 17:23:51 -0800 (Wed, 27 Jan 2016) | 3 lines
    Header file for save_and_count_local_state.c
    A    save_and_count_local_state.h

---
+++ save_and_count_local_state.h
@@ -0,0 +1,4 @@
+#ifndef _SAVE_AND_COUNT_LOCAL_STATE_H_
+#define _SAVE_AND_COUNT_LOCAL_STATE_H_ 1
+extern int save_and_count_local_state(struct state_struct *local_state,struct boot_state_struct *boot_state, int rxn_num);
+#endif
