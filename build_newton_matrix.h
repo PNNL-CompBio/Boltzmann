98daf019ee6d6c5c9249d2cde926ba69ab540be3
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:55 -0800

    r4747 | dbaxter | 2017-11-07 18:40:20 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for build_newton_matrix.h
    A    build_newton_matrix.h

---
+++ build_newton_matrix.h
@@ -0,0 +1,6 @@
+#ifndef _BUILD_NEWTON_MATRIX_H_
+#define _BUILD_NEWTON_MATRIX_H_ 1
+extern int build_newton_matrix(struct state_struct *state,
+			       double gamma,
+			       int choice);
+#endif
