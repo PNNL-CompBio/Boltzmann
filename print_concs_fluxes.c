e9072f9d107ad600a4666423c701732c533ec639
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:21 -0800

    r3587 | dbaxter | 2016-10-13 15:05:27 -0700 (Thu, 13 Oct 2016) | 4 lines
    If origin <= 2 we are printing out a flux vector,
    else we are printing out the change in concentration vector, z (h*yp)
    M    print_concs_fluxes.c

--- print_concs_fluxes.c
+++ print_concs_fluxes.c
@@ -2,10 +2,15 @@
 
 #include "print_concs_fluxes.h"
 int print_concs_fluxes(struct state_struct *state,int ny,
-		       double *fluxes, double *concs, 
+		       double *fluxes, 
+		       double *concs, 
+		       double *counts,
 		       double *forward_rxn_likelihoods,
 		       double *reverse_rxn_likelihoods,
-		       double time, double h) {
+		       double time, 
+		       double h,
+		       int    step,
+		       int    origin) {
 /* 
   Debugging routine to print concentraions and fluxes for each molecule.
   Called by: ode23tb 
@@ -25,12 +30,19 @@ int print_concs_fluxes(struct state_struct *state,int ny,
   molecules_text = state->molecules_text;
   nrxns          = state->number_reactions;
   if (lfp) {
-    fprintf(lfp," time = %le, h = %le\n"
-	    "Molecule\tindex\tconcentration\tflux\n",time,h);
+    if (origin <= 2) {
+      fprintf(lfp," time = %le, h = %le, step = %d, origin = %d\n"
+	      "Species\tindex\tconcentration\t    count    \t    flux\n",
+	      time,h,step,origin);
+    } else {
+      fprintf(lfp," time = %le, h = %le, step = %d, origin = %d\n"
+	      "Species\tindex\tconcentration\t    count    \t    delta_conc\n",
+	      time,h,step,origin);
+    }
     for (i=0;i<ny;i++) {
-      fprintf(lfp,"%s\t%d\t%le\t%le\n",
+      fprintf(lfp,"%s\t%d\t%le\t%le\t%le\n",
 	      (char*)&molecules_text[molecule->string],
-	      i,concs[i],fluxes[i]);
+	      i,concs[i],counts[i],fluxes[i]);
       molecule   += 1; /* Caution address arithmetic here. */
     }
     fprintf(lfp,"rxn_n\tfrwrd_lklhd\trvrs_lklhd\n");
