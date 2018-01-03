1ddd7afedbc19846191764fa7969c96ebefa76c5
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:07 -0800

    r4224 | dbaxter | 2017-06-14 09:42:58 -0700 (Wed, 14 Jun 2017) | 3 lines
    Removed flattened_state argument.
    M    boltzmann_init_core.h

--- boltzmann_init_core.h
+++ boltzmann_init_core.h
@@ -1,4 +1,4 @@
 #ifndef _BOLTZMANN_INIT_CORE_H_
 #define _BOLTZMANN_INIT_CORE_H_ 1
-extern int boltzmann_init_core(struct state_struct *boot_state, struct state_struct **flattened_state);
+extern int boltzmann_init_core(struct state_struct *state);
 #endif
