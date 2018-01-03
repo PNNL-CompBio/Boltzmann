5de85c584fa75e671bcd684b26f00d3638ce7875
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:31 -0800

    r3726 | dbaxter | 2016-12-12 16:13:57 -0800 (Mon, 12 Dec 2016) | 3 lines
    Header file for ce_approximate_delta_concs.c
    A    ce_approximate_delta_concs.h

---
+++ ce_approximate_delta_concs.h
@@ -0,0 +1,10 @@
+#ifndef _CE_APPROXIMATE_DELTA_CONCS_H_
+#define _CE_APPROXIMATE_DELTA_CONCS_H_ 1
+extern int ce_approximate_delta_concs(struct state_struct *state, 
+				      double *counts,
+				      double *forward_rxn_likelihoods,
+				      double *reverse_rxn_likelihoods, 
+				      double *flux, double multiplier,
+				      int base_rxn,
+				      int choice);
+#endif
