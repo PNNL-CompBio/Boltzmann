52451534e9f77ada79df570b5fd75cbc0ed4806a
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:25 -0800

    r4987 | dbaxter | 2017-12-13 17:56:20 -0800 (Wed, 13 Dec 2017) | 3 lines
    Added concs argument.
    M    boltzmann_cvodes_init.h

--- boltzmann_cvodes_init.h
+++ boltzmann_cvodes_init.h
@@ -1,4 +1,5 @@
 #ifndef _BOLTZMANN_CVODES_INIT_H_
 #define _BOLTZMANN_CVODES_INIT_H_ 1
-extern int boltzmann_cvodes_init(void *mem_cvode,struct state_struct *state);
+extern int boltzmann_cvodes_init(void *mem_cvode,struct state_struct *state,
+				 double *concs);
 #endif
