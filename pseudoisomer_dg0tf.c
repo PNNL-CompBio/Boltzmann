32eeb737a86adc67dfc294052f27cca562cb537c
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:05 -0800

    r4190 | dbaxter | 2017-05-09 09:54:08 -0700 (Tue, 09 May 2017) | 3 lines
    Minor aligment change of 1 line of code.
    M    pseudoisomer_dg0tf.c

--- pseudoisomer_dg0tf.c
+++ pseudoisomer_dg0tf.c
@@ -52,7 +52,7 @@ double pseudoisomer_dg0tf(double ph,
   */
   is_sqrt   = sqrt(ionic_str);
   deltag_tf = deltag0 - (((nh*mrt)*log(10))*ph)-((2.91482*((z*z)-nh)*is_sqrt)/
-					   (1.0+1.6*is_sqrt));
+						 (1.0+1.6*is_sqrt));
 
   /*
     cout << "[CalcFreeEnergy(speciesDeltaG_tf)] species deltaG0 = " << deltaG0 << " kJ/mol" << endl;
