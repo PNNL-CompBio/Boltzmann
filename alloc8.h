b586c3e12c4359519d175f94d1ac52d73b7c707d
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:36 -0800

    r4540 | dbaxter | 2017-09-20 10:18:49 -0700 (Wed, 20 Sep 2017) | 3 lines
    Added some comments, but backed out from adding arguments just yet.
    M    alloc8.h

--- alloc8.h
+++ alloc8.h
@@ -1,4 +1,10 @@
 #ifndef _ALLOC8_H_
 #define _ALLOC8_H_ 1
+/*
+extern int alloc8(struct state_struct *state,
+		  int direction,
+		  void *workspace,
+		  void **next_addr);
+*/
 extern int alloc8(struct state_struct *state);
 #endif
