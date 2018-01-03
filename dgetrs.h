e73e9a4e24e4bdc9c4211eb5bb83531b871897e0
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:25 -0800

    r4984 | dbaxter | 2017-12-13 17:54:07 -0800 (Wed, 13 Dec 2017) | 4 lines
    Added length arguments for character variables to make it a fortran
    compatible call.
    M    dgetrs.h

--- dgetrs.h
+++ dgetrs.h
@@ -1,5 +1,5 @@
 #ifndef _DGETRS_H_
 #define _DGETRS_H_ 1
 extern void dgetrs_(char *trans, int *n, int *nrhs, double *a, int *lda, 
-		   int *ipivot, double *b, int *ldb, int *info);
+		    int *ipivot, double *b, int *ldb, int *info, int len_trans);
 #endif
