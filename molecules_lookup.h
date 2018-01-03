a32f18937542feb6e083ae17bb157d9178067444
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:07 -0800

    r1605 | dbaxter | 2012-08-05 11:48:54 -0700 (Sun, 05 Aug 2012) | 3 lines
    Added compartment_index argument.
    M    molecules_lookup.h

--- molecules_lookup.h
+++ molecules_lookup.h
@@ -20,4 +20,6 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int molecules_lookup(char *molecule_name, struct state_struct *state);
+extern int molecules_lookup(char *molecule_name, 
+			    int compartment_index,
+			    struct state_struct *state);
