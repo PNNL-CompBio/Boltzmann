08d461bdf994332a9f12bfae302ffb4bb9f3f2f5
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:47 -0800

    r4660 | dbaxter | 2017-10-11 17:15:10 -0700 (Wed, 11 Oct 2017) | 4 lines
    Changing the output_filename to be the base name for all output files
    (and the concs.in file if not specified) instead of the rxns.dat file.
    M    create_output_filenames.c

--- create_output_filenames.c
+++ create_output_filenames.c
@@ -5,7 +5,7 @@ int create_output_filenames(struct state_struct *state) {
     Create output file names for the counts_out, rxn_likelihood,
     free_energey, boundary_flux, concs_in, cmpts_dat rxns_dat, id_name,
     restart, rxn_view and rxn_mat files. In the process set the
-    rxn_filename_base_length field of state.
+    output_filename_base_length field of state.
     Called by: io_size_init
     Calls:     strlen,strncpy,strcpy,fprintf,fflush
   */
@@ -31,12 +31,13 @@ int create_output_filenames(struct state_struct *state) {
   char *dg0ke_filename;
   char *dictionary_filename;
   char *aux_data_filename;
-  int64_t rxn_filename_base_length;
-  int rxn_filename_length;
+  int64_t output_filename_base_length;
+  int output_filename_length;
   int success;
   int i;
   int no_slash_or_dot;
-
+  int output_filename_empty;
+  int padi;
   success              = 1;
   rxn_filename         = state->reaction_file;
   init_conc_filename   = state->init_conc_file;
@@ -62,114 +63,126 @@ int create_output_filenames(struct state_struct *state) {
 
   dictionary_filename  = state->dictionary_file;
 
-  rxn_filename_length = strlen(rxn_filename);
+
+  output_filename_empty = 0;
+  if (output_filename_length == 0) {
+    output_filename_empty = 1;
+    /*
+      No output file was specified so use input file name.
+    */
+    strcpy(output_filename,rxn_filename);
+  }
+  output_filename_length = strlen(output_filename);
   no_slash_or_dot = 1;
-  rxn_filename_base_length = -1;
-  for (i=rxn_filename_length-1;((i>=1) && no_slash_or_dot);i--) {
-    if (rxn_filename[i] == '.') {
-      rxn_filename_base_length = i;
+  output_filename_base_length = -1;
+  for (i=output_filename_length-1;((i>=1) && no_slash_or_dot);i--) {
+    if (output_filename[i] == '.') {
+      output_filename_base_length = i;
       no_slash_or_dot = 0;
     } else {
-      if (rxn_filename[i] == '/') {
+      if (output_filename[i] == '/') {
 	no_slash_or_dot = 0;
       }
     }
   }
-  if (rxn_filename_base_length < 1) {
-    rxn_filename_base_length = rxn_filename_length;
+  if (output_filename_base_length < 1) {
+    output_filename_base_length = output_filename_length;
     /*
       check for a reaction file name of "." or "./",
       if so these will flag as an error.
     */
-    if (rxn_filename_length < 3) {
-      if ((strcmp(rxn_filename,"./") == 0)  || (strcmp(rxn_filename,".") == 0) || (rxn_filename_length < 1)) {
+    if (output_filename_length < 3) {
+      if ((strcmp(output_filename,"./") == 0)  || (strcmp(output_filename,".") == 0) || (output_filename_length < 1)) {
 	success = 0;
-	fprintf(stderr,"create_output_filenames: Error invalid rxn_file was %s\n",rxn_filename);
+	fprintf(stderr,"create_output_filenames: Error invalid rxn_file was %s\n",output_filename);
 	fflush(stderr);
       }
     }
   }
   if (success) {
-    if ((rxn_filename_base_length + 11) > state->max_filename_len) {
-      rxn_filename_base_length = state->max_filename_len - 12;
+    if ((output_filename_base_length + 11) > state->max_filename_len) {
+      output_filename_base_length = state->max_filename_len - 12;
       /*
 	Probably should print a warning mewssage here.
       */
-      fprintf(stderr,"create_output_filenames: Warning, truncated rxn_filename_base to %lld characters\n",rxn_filename_base_length);
+      fprintf(stderr,"create_output_filenames: Warning, truncated output_filename_base to %ld characters\n",output_filename_base_length);
       fflush(stderr);
     }
-    state->rxn_filename_base_length = rxn_filename_base_length;
+    state->output_filename_base_length = output_filename_base_length;
     if (init_conc_filename[0] == '\0') {
-      strncpy(init_conc_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&init_conc_filename[rxn_filename_base_length],".concs");
+      strncpy(init_conc_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&init_conc_filename[output_filename_base_length],".concs");
     }
     if (log_filename[0] == '\0') {
-      strncpy(log_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&log_filename[rxn_filename_base_length],".log");
+      strncpy(log_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&log_filename[output_filename_base_length],".log");
     }
-    if (output_filename[0] == '\0') {
-      strncpy(output_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&output_filename[rxn_filename_base_length],".out");
+    if (output_filename_empty) {
+      /*
+	No output file was specified so we use the rxn_file prefix as the
+	output file base, and make its suffix .out.
+      */
+      strcpy((char*)&output_filename[output_filename_base_length],".out");
     }
     if (counts_out_filename[0] == '\0') {
-      strncpy(counts_out_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&counts_out_filename[rxn_filename_base_length],".count");
+      strncpy(counts_out_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&counts_out_filename[output_filename_base_length],".count");
     }
     if (concs_out_filename[0] == '\0') {
-      strncpy(concs_out_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&concs_out_filename[rxn_filename_base_length],".concs");
+      strncpy(concs_out_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&concs_out_filename[output_filename_base_length],".concs");
     }
     if (ode_concs_filename[0] == '\0') {
-      strncpy(ode_concs_filename,rxn_filename,rxn_filename_base_length);      
-      strcpy((char*)&ode_concs_filename[rxn_filename_base_length],".ode_concs");
+      strncpy(ode_concs_filename,output_filename,output_filename_base_length);      
+      strcpy((char*)&ode_concs_filename[output_filename_base_length],".ode_concs");
     }
-    strncpy(ode_dconcs_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&ode_dconcs_filename[rxn_filename_base_length],".ode_dconcs");
-    strncpy(ode_lklhd_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&ode_lklhd_filename[rxn_filename_base_length],".ode_lklhd");
-    strncpy(ode_bflux_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&ode_bflux_filename[rxn_filename_base_length],".ode_bflux");
-    strncpy(net_lklhd_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&net_lklhd_filename[rxn_filename_base_length],".nlklhd");
-    strncpy(nl_bndry_flx_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&nl_bndry_flx_filename[rxn_filename_base_length],".nl_flux");
-    strncpy(aux_data_filename,rxn_filename,rxn_filename_base_length);
-    strcpy((char*)&aux_data_filename[rxn_filename_base_length],".aux");
+    strncpy(ode_dconcs_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&ode_dconcs_filename[output_filename_base_length],".ode_dconcs");
+    strncpy(ode_lklhd_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&ode_lklhd_filename[output_filename_base_length],".ode_lklhd");
+    strncpy(ode_bflux_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&ode_bflux_filename[output_filename_base_length],".ode_bflux");
+    strncpy(net_lklhd_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&net_lklhd_filename[output_filename_base_length],".nlklhd");
+    strncpy(nl_bndry_flx_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&nl_bndry_flx_filename[output_filename_base_length],".nl_flux");
+    strncpy(aux_data_filename,output_filename,output_filename_base_length);
+    strcpy((char*)&aux_data_filename[output_filename_base_length],".aux");
     if (rxn_lklhd_filename[0] == '\0') {
-      strncpy(rxn_lklhd_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&rxn_lklhd_filename[rxn_filename_base_length],".lklhd");
+      strncpy(rxn_lklhd_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&rxn_lklhd_filename[output_filename_base_length],".lklhd");
     }
     if (free_energy_filename[0] == '\0') {
-      strncpy(free_energy_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&free_energy_filename[rxn_filename_base_length],".fe");
+      strncpy(free_energy_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&free_energy_filename[output_filename_base_length],".fe");
     }
     if (restart_filename[0] == '\0') {
-      strncpy(restart_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&restart_filename[rxn_filename_base_length],".rstrt");
+      strncpy(restart_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&restart_filename[output_filename_base_length],".rstrt");
     }
     if (rxn_view_filename[0] == '\0') {
-      strncpy(rxn_view_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&rxn_view_filename[rxn_filename_base_length],".view");
+      strncpy(rxn_view_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&rxn_view_filename[output_filename_base_length],".view");
     }
     if (bndry_flux_filename[0] == '\0') {
-      strncpy(bndry_flux_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&bndry_flux_filename[rxn_filename_base_length],".flux");
+      strncpy(bndry_flux_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&bndry_flux_filename[output_filename_base_length],".flux");
     }
     if (rxn_echo_filename[0] == '\0') {
-      strncpy(rxn_echo_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&rxn_echo_filename[rxn_filename_base_length],".echo");
+      strncpy(rxn_echo_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&rxn_echo_filename[output_filename_base_length],".echo");
     }
     if (rxn_mat_filename[0] == '\0') {
-      strncpy(rxn_mat_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&rxn_mat_filename[rxn_filename_base_length],".mat");
+      strncpy(rxn_mat_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&rxn_mat_filename[output_filename_base_length],".mat");
     }
     if (dg0ke_filename[0] == '\0') {
-      strncpy(dg0ke_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&dg0ke_filename[rxn_filename_base_length],".dg0ke");
+      strncpy(dg0ke_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&dg0ke_filename[output_filename_base_length],".dg0ke");
     }
     if (dictionary_filename[0] == '\0') {
-      strncpy(dictionary_filename,rxn_filename,rxn_filename_base_length);
-      strcpy((char*)&dictionary_filename[rxn_filename_base_length],".dict");
+      strncpy(dictionary_filename,output_filename,output_filename_base_length);
+      strcpy((char*)&dictionary_filename[output_filename_base_length],".dict");
     }
   }
   return(success);
