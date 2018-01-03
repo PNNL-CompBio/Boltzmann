4475d8e04f116b408b6a38fe989b0a01a0c04b5a
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:28 -0800

    r4451 | dbaxter | 2017-08-22 17:54:12 -0700 (Tue, 22 Aug 2017) | 3 lines
    Added setup parameter, see alloc2.c comment.
    M    alloc2.h

--- alloc2.h
+++ alloc2.h
@@ -20,4 +20,4 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int alloc2(struct state_struct *state);
+extern int alloc2(struct state_struct *state, int setup);
