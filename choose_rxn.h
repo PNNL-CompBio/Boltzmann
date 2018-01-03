c0e25d198b0d5a3a133dcd9c4c7901efe96eadc8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:15 -0800

    r1866 | dbaxter | 2012-10-22 17:55:00 -0700 (Mon, 22 Oct 2012) | 3 lines
    Added r_sum_likelihoodp argument.
    M    choose_rxn.h

--- choose_rxn.h
+++ choose_rxn.h
@@ -20,4 +20,5 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int choose_rxn(struct state_struct *state);
+extern int choose_rxn(struct state_struct *state,
+		      double *r_sum_likelihoodp);
