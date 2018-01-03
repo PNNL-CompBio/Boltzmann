650f8d5287c2574b043f41121f11f0f9327c0121
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:50 -0800

    r4688 | dbaxter | 2017-11-07 17:40:56 -0800 (Tue, 07 Nov 2017) | 3 lines
    Solve a sparse lower triangular linear system  (forward solve).
    A    boltzmann_cvodes_fsolve.c

---
+++ boltzmann_cvodes_fsolve.c
@@ -0,0 +1,39 @@
+#include "boltzmann_structs.h"
+#include "boltzmann_cvodes_fsolve.h"
+int boltzmann_cvodes_fsolve(struct state_struct *state, double *r, double *z) {
+  /*
+    Perform the forward solve of Lz = r
+    Called by: boltzman_cvodes_psolve
+  */
+  struct cvodes_params_struct *cvodes_params;
+  double *l;
+  double sum;
+  int    *il;
+  int    *jl;
+
+  int    ny;
+  int    i;
+
+  int    j;
+  int    k;
+  
+  int    success;
+  int    padi;
+
+  success       = 1;
+  ny            = state->nunique_molecules;
+  cvodes_params = state->cvodes_params;
+  l             = cvodes_params->prec_l;
+  il            = cvodes_params->prec_il;
+  jl            = cvodes_params->prec_jl;
+  z[0] = r[0];
+  for (i=1;i<ny;i++) {
+    sum = r[i];
+    for (j=il[i];j<il[i+1];j++) {
+      k = jl[j];
+      sum -= z[k] * l[j];
+    }
+    z[i] = sum;
+  }
+  return(success);
+}
