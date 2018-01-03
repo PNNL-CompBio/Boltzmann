9318cbfb83ee68705a94df229053bfb45f743f28
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:41 -0800

    r4592 | dbaxter | 2017-09-26 18:00:22 -0700 (Tue, 26 Sep 2017) | 3 lines
    Header file for boltzmann_build_agent_data_block.c
    A    boltzmann_build_agent_data_block.h

---
+++ boltzmann_build_agent_data_block.h
@@ -0,0 +1,5 @@
+#ifndef _BOLTZMANN_BUILD_AGENT_DATA_BLOCK_H_
+#define _BOLTZMANN_BUILD_AGENT_DATA_BLOCK_H_ 1
+extern int boltzmann_build_agent_data_block(struct state_struct *state, 
+					    void **agent_data_p);
+#endif
