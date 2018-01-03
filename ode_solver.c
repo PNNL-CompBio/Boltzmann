1d3b15d0a63673a00b44216e81743e9ca9041b0f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:31 -0800

    r3733 | dbaxter | 2016-12-12 16:29:42 -0800 (Mon, 12 Dec 2016) | 4 lines
    Routine to be the parent of ode23tb to allow incorporation of other
    ode sovlers controlled by the ODE_SOLVER_CHOICE parameter.
    A    ode_solver.c

---
+++ ode_solver.c
@@ -0,0 +1,48 @@
+#include "boltzmann_structs.h"
+#include "ode23tb.h"
+
+#include "ode_solver.h"
+int ode_solver (struct state_struct *state, double *counts,
+		double htry, int nonnegative, int normcontrol,
+		int print_concs, int choice) {
+  /*
+    Called by: deq_run
+    Calls:     ode23tb
+               
+               counts is the vector of species molecle
+	       counts, the counts_to_concs field in state can be used
+	       to convert those counts to concentrations. 
+	       We may change that to be a concentrations vector and package
+	       all of the other arguments into a piece of the state_struct
+	       for future convenience.
+
+               htry, nonnegative, normcontrol, and print_concs are
+	       all input variables to ode23tb the default ode solver.
+
+	       choice is meant to be the indicator of which solver
+	       to use, right now we only offer ode23tb.
+
+	       Other solvers could pass their arguments, if different
+	       through state. 
+
+
+	       return value is 1 for success, 0 for failure.
+  */
+  int success;
+  int local_choice;
+  FILE *lfp;
+  FILE *efp;
+  lfp     = state->lfp;
+  success = 1;
+  local_choice = choice;
+  if (choice != 0) {
+    if (lfp) {
+      fprintf(lfp,"ode_solver: invalid ode_solver_choice, using default\n");
+      fflush(lfp);
+      state->ode_solver_choice = 0;
+    }
+  }
+  success = ode23tb(state,counts,htry,nonnegative,normcontrol,
+		    print_concs,choice);
+  return(success);
+}
