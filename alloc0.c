98b74675a9cc033c290c830ff1ff3b3d6a5303d1
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:45 -0800

    r2563 | dbaxter | 2014-04-15 10:15:28 -0700 (Tue, 15 Apr 2014) | 3 lines
    concs_out_file -> counts_out_file
    M    alloc0.c

--- alloc0.c
+++ alloc0.c
@@ -93,8 +93,8 @@ int alloc0(struct state_struct **state) {
     statep->output_file        = statep->input_dir + max_file_name_len;
     statep->log_file           = statep->output_file + max_file_name_len;
     statep->output_dir         = statep->log_file + max_file_name_len;
-    statep->concs_out_file     = statep->output_dir + max_file_name_len;
-    statep->rxn_lklhd_file     = statep->concs_out_file + max_file_name_len;
+    statep->counts_out_file    = statep->output_dir + max_file_name_len;
+    statep->rxn_lklhd_file     = statep->counts_out_file + max_file_name_len;
     statep->free_energy_file   = statep->rxn_lklhd_file + max_file_name_len;
     statep->restart_file       = statep->free_energy_file + max_file_name_len;
     statep->rxn_view_file      = statep->restart_file + max_file_name_len;
