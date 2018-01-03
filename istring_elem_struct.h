7ad947bba73524c5dadcf680d61a0655d6b4f5bb
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:03 -0800

    r1426 | dbaxter | 2012-06-08 17:54:39 -0700 (Fri, 08 Jun 2012) | 3 lines
    Structure for storing indexed strings (a string pointer and a list index).
    A    istring_elem_struct.h

---
+++ istring_elem_struct.h
@@ -0,0 +1,30 @@
+/* istring_elem_struct.h 
+  
+sss
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+*/
+#ifndef __ISTRING_ELEM_STRUCT__
+#define __ISTRING_ELEM_STRUCT__
+struct istring_elem_struct {
+  char *string;
+  int64_t index;
+}
+;
+#endif
