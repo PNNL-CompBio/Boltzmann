b70915aeef97d5fcc48537d48efd51b7a1b8c613
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:51 -0800

    r2667 | dbaxter | 2014-05-02 09:55:17 -0700 (Fri, 02 May 2014) | 3 lines
    Added new compartment_struct.h
    M    boltzmann_structs.h

--- boltzmann_structs.h
+++ boltzmann_structs.h
@@ -55,6 +55,7 @@ specific language governing permissions and limitations under the License.
 #include "rxn_matrix_struct.h"
 #include "molecules_matrix_struct.h"
 #include "molecule_struct.h"
+#include "compartment_struct.h"
 #include "stack_level_elem_struct.h"
 #include "vgrng_state_struct.h"
 #include "pseudoisomer_struct.h"
