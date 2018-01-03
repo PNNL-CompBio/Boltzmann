d8a9d24bff305d4ad218fd3d7ef7b1458187f28b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:36 -0800

    r4541 | dbaxter | 2017-09-20 10:19:15 -0700 (Wed, 20 Sep 2017) | 3 lines
    Added some comments but backed out of adding arguments just yet.
    M    alloc9.h

--- alloc9.h
+++ alloc9.h
@@ -1,4 +1,10 @@
 #ifndef _ALLOC9_H_
 #define _ALLOC9_H_ 1
+/*
+extern int alloc9(struct state_struct *state,
+		  int direction,
+		  void *workspace,
+		  void **next_addr);
+*/
 extern int alloc9(struct state_struct *state);
 #endif
