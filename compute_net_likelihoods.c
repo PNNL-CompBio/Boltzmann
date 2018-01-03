2e6a2d1599cc96375f90f1f6eb6eb69a8ef6a3bd
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:40 -0800

    r3878 | dbaxter | 2017-01-21 16:21:45 -0800 (Sat, 21 Jan 2017) | 4 lines
    Routine to compute the net likelihoods = (forward - reverse)/total
    for each reaction.
    A    compute_net_likelihoods.c

---
+++ compute_net_likelihoods.c
@@ -0,0 +1,48 @@
+#include "boltzmann_structs.h"
+#include "compute_net_likelihoods.h"
+int compute_net_likelihoods(struct state_struct *state) {
+  /*
+    Compute the net likelihood for each reaction.
+    Uses the forward_rxn_likelihood and reverse_rxn_likelihood 
+    fields in state to set the net_likelilhood field for each 
+    reaction.
+    Called by: ode23b
+    Calls:
+  */
+  int64_t number_reactions;
+  double  *forward_rxn_likelihood; /* number_reactions */
+  double  *reverse_rxn_likelihood; /* number_reactions */
+  double *net_likelihood;
+  double  total_likelihood;
+  double  recip_total_likelihood;
+  int64_t i;
+  int success;
+  int padi;
+  FILE *lfp;
+  FILE *efp;
+  success                = 1;
+  number_reactions       = state->number_reactions;
+  forward_rxn_likelihood = state->forward_rxn_likelihood;
+  reverse_rxn_likelihood = state->reverse_rxn_likelihood;
+  net_likelihood         = state->net_likelihood;
+  lfp                    = state->lfp;
+  total_likelihood = 0;
+  for (i=0;i<number_reactions;i++) {
+    total_likelihood = total_likelihood + forward_rxn_likelihood[i] +
+      reverse_rxn_likelihood[i];
+    net_likelihood[i] = forward_rxn_likelihood[i] - reverse_rxn_likelihood[i];
+  }
+  if (total_likelihood <= 0) {
+    success = 0;
+    if (lfp) {
+      fprintf(lfp,"compute_net_likelihoods: Error total likelihood is <= 0.\n");
+      fflush(lfp);
+    }
+  } else {
+    recip_total_likelihood = 1.0 / total_likelihood;
+    for (i=0;i<number_reactions;i++) {
+      net_likelihood[i] *= recip_total_likelihood;
+    }
+  }
+  return(success);
+}
