ed51393011a0c72b6865c2e506ce3197488aa418
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:05 -0800

    r1491 | dbaxter | 2012-06-13 15:38:40 -0700 (Wed, 13 Jun 2012) | 3 lines
    Was print_species_dictionary.c
    A    print_molecules_dictionary.c

---
+++ print_molecules_dictionary.c
@@ -0,0 +1,69 @@
+/* print_molecules_dictionary.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "boltzmann_structs.h"
+
+#include "print_molecules_dictionary.h"
+int print_molecules_dictionary(struct state_struct *state) {
+  /*
+    Print the  unique molecules in sorted order.
+    Called by: boltzmann
+    Calls:     fopen, fprintf, fclose (intrinsic)
+  */
+  struct rxn_struct *reactions;
+  struct rxn_matrix_struct *rxns_matrix;
+  int64_t *column_indices;
+  struct istring_elem_struct *sorted_molecules;
+  struct istring_elem_struct *cur_molecules;
+  int nzr;
+  int i;
+  int success;
+  int nu_molecules;
+  FILE *dict_fp;
+  success = 1;
+  nu_molecules     = state->unique_molecules;
+  cur_molecules = state->sorted_molecules;
+  dict_fp = fopen("molecules.dict","w+");
+  if (dict_fp == NULL) {
+    fprintf(stderr,
+	    "print_molecules_dictionary: Error could not open "
+	    "molecules.dict file.\n");
+    fflush(stderr);
+    success = 0;
+  }
+  if (success) {
+    for (i=0;i<nu_molecules;i++) {
+      fprintf(dict_fp,"%d %s\n",i,cur_molecules->string);
+      cur_molecules += 1; /* Caution address arithmetic. */
+    }
+    fclose(dict_fp);
+  }
+  return(success);
+}
