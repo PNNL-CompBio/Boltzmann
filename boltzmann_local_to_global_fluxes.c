f81abc806dca3a4564ffc7fd0e67e9d68ef47498
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:47 -0800

    r2604 | dbaxter | 2014-04-21 17:19:33 -0700 (Mon, 21 Apr 2014) | 3 lines
    bndry_flux_concs -> bndry_flux_counts
    M    boltzmann_local_to_global_fluxes.c

--- boltzmann_local_to_global_fluxes.c
+++ boltzmann_local_to_global_fluxes.c
@@ -58,7 +58,7 @@ int boltzmann_local_to_global_fluxes(struct super_state_struct *super_state,
   }
   if (success) {
     state_index         = local_state->agent_type;
-    local_fluxes        = local_state->bndry_flux_concs;
+    local_fluxes        = local_state->bndry_flux_counts;
     nunique_molecules   = local_state->nunique_molecules;
     molecule_map_starts = super_state_pointers->molecule_map_starts;
     molecule_maps       = super_state_pointers->molecule_map;
