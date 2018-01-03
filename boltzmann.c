156155f72bfaaaac3055e205d19421e78b2ad360
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:32 -0800

    r2290 | dbaxter | 2013-03-20 17:59:48 -0700 (Wed, 20 Mar 2013) | 3 lines
    Removing includes also found in boltzmann_structs.h
    M    boltzmann.c

--- boltzmann.c
+++ boltzmann.c
@@ -20,24 +20,13 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <float.h>
-#include <signal.h>
-#include <unistd.h>
-/*
+#include "boltzmann_structs.h"/*
   Maybe only for linux systems.
 */
 #ifdef LIBUNWIND
 #include <libunwind.h>
 #include <unwind.h>
 #endif
-
-#include "djb_timing_b.h"
-#include "boltzmann_structs.h"
-
 
 #ifdef TIMING_ON
 struct timing_struct timing_data;
