9122d1c14202068a3631a2fa007bfa97fd11ad82
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:36 -0800

    r4543 | dbaxter | 2017-09-20 10:23:04 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_aux_data.c
    A    boltzmann_flatten_aux_data.h

---
+++ boltzmann_flatten_aux_data.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMAN_FLATTEN_AUX_DATA_H_
+#define _BOLTZMAN_FLATTEN_AUX_DATA_H_ 1
+extern int boltzmann_flatten_aux_data(struct state_struct *state, 
+				     void **aux_data_p,
+				     int direction);
+#endif
