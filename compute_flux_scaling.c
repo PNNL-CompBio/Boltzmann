75bd94cc03655c2d9ea3f1a67f209d00b62d2468
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:45 -0800

    r3938 | dbaxter | 2017-02-06 11:45:11 -0800 (Mon, 06 Feb 2017) | 6 lines
    Changed to allow flux_scaling to be set to be fixed by user,
    or if state->flux_scaling is set to 0, then ti will be the forward rate
    constant for the base reaction, kf_base_reaction, multiplied by the
    product of the base reaction reactant species concentrations.
    M    compute_flux_scaling.c

--- compute_flux_scaling.c
+++ compute_flux_scaling.c
@@ -3,8 +3,12 @@
 #include "compute_flux_scaling.h"
 double compute_flux_scaling(struct state_struct *state, double *concs) {
   /*
-    Compute the kf_base_reactioin * 
-    (product of base_reaciont reactant concentrations).
+    if flux_scaling field of state is 0, 
+      Compute the kf_base_reaction * 
+      (product of base_reaciont reactant concentrations).
+    else 
+      use the value in state.
+
     Called by: num_jac_col, ode23tb
     Uses kf_base_reaction, number_base_reaction_reactants
          and base_reactants fields of state.
@@ -18,12 +22,19 @@ double compute_flux_scaling(struct state_struct *state, double *concs) {
   int i;
   int species;
   int padi;
-  nbr = state->number_base_reaction_reactants;
-  flux_scaling = state->kf_base_reaction;
-  base_reactants = state->base_reactants;
-  for (i=0;i<nbr;i++) {
-    species = base_reactants[i];
-    flux_scaling = flux_scaling * concs[species];
+  if (state->flux_scaling == 0) {
+    nbr = state->number_base_reaction_reactants;
+    flux_scaling = state->kf_base_reaction;
+    base_reactants = state->base_reactants;
+    for (i=0;i<nbr;i++) {
+      species = base_reactants[i];
+      flux_scaling = flux_scaling * concs[species];
+    }
+  } else {
+  /* For now hardwire flux_scaling to be 1.
+    flux_scaling = 1;
+  */
+    flux_scaling = state->flux_scaling;
   }
   return (flux_scaling);
 }
