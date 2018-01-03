6f0228239f13e390bb8cb70404240c268217a0b6
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:14 -0800

    r1831 | dbaxter | 2012-10-16 11:43:24 -0700 (Tue, 16 Oct 2012) | 3 lines
    Routine to print the free energy output file header.
    A    print_free_energy_header.c

---
+++ print_free_energy_header.c
@@ -0,0 +1,66 @@
+/* print_free_energy_header.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "djb_timing_b.h"
+#include "boltzmann_structs.h"
+
+#include "print_free_energy_header.h"
+void print_free_energy_header(struct state_struct *state) {
+  /*
+    Print the header lines for the free energy output file.
+    Called by: boltzmann_init .
+    Calls    : fprintf,fflush.
+  */
+  struct rxn_struct *reactions;
+  int i;
+  int padi;
+
+  /*
+    Print header lines for the free energy file.
+  */
+  if (state->free_energy_fp) {
+    if (state->free_energy_format == 1) {
+      fprintf(state->free_energy_fp,"negative_log_likelihoods\n");
+    } else if (state->free_energy_format == 2) {
+      fprintf(state->free_energy_fp,"free energy (KJ/mol)\n");
+    } else if (state->free_energy_format == 3) {
+      fprintf(state->free_energy_fp,"free energy (Kcal/mol)\n");
+    }
+    fprintf(state->free_energy_fp,"iter");
+    reactions = state->reactions;
+    for (i=0;i<state->number_reactions;i++) {
+      fprintf(state->free_energy_fp,"\t%s",reactions->title);
+      reactions += 1; /* Caution address arithmetic */
+    }
+    fprintf(state->free_energy_fp,"\n");
+    fflush(state->free_energy_fp);
+  }
+  return;
+}
