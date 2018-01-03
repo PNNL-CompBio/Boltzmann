c243e570769ccc2001496d8943991dbdc8c52371
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:55 -0800

    r4743 | dbaxter | 2017-11-07 18:36:21 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for boltzmann_set_cvodes_linear_solver.c
    A    boltzmann_set_cvodes_linear_solver.h

---
+++ boltzmann_set_cvodes_linear_solver.h
@@ -0,0 +1,5 @@
+#ifndef _BOLTZMANN_SET_CVODES_LINEAR_SOLVER_H_
+#define _BOLTZMANN_SET_CVODES_LINEAR_SOLVER_H_ 1
+extern int boltzmann_set_cvodes_linear_solver(struct state_struct *state,
+					      void *cvode_mem);
+#endif
