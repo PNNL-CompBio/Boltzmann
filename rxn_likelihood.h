726bd37fee7e7a954386aefe799751acbccf57b8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:48 -0800

    r2621 | dbaxter | 2014-04-21 17:47:22 -0700 (Mon, 21 Apr 2014) | 3 lines
    concs -> counts.
    M    rxn_likelihood.h

--- rxn_likelihood.h
+++ rxn_likelihood.h
@@ -20,7 +20,7 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern double rxn_likelihood(double *concs, 
+extern double rxn_likelihood(double *counts, 
 			     struct state_struct *state,
 			     int rxn_direction,
 			     int rxn);
