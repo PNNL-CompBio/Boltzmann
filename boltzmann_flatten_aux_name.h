33b6aca194838217d4c707696b6351480bd52504
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:35 -0800

    r4529 | dbaxter | 2017-09-20 10:10:29 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_aux_name.c
    A    boltzmann_flatten_aux_name.h

---
+++ boltzmann_flatten_aux_name.h
@@ -0,0 +1,8 @@
+#ifndef _BOLTZMANN_FLATTEN_AUX_NAME_H_
+#define _BOLTZMANN_FLATTEN_AUX_NAME_H_ 1
+extern int boltzmann_flatten_aux_name(struct state_struct *state,
+				      void *flattened, 
+				      int direction, 
+				      int *word_pos_p, 
+				      FILE *lfp);
+#endif
