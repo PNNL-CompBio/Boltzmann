fbf25593b7e7cc24bbee9f5ba9cf1ccffef86d75
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:03 -0800

    r4169 | dbaxter | 2017-04-20 10:35:12 -0700 (Thu, 20 Apr 2017) | 3 lines
    header file for lr6_approximate_delta_concs.c
    A    lr6_approximate_delta_concs.h

---
+++ lr6_approximate_delta_concs.h
@@ -0,0 +1,7 @@
+#ifndef _LR6_APPROXIMATE_DELTA_CONCS_H_
+#define _LR6_APPROXIMATE_DELTA_CONCS_H_ 1
+extern int lr6_approximate_delta_concs(struct state_struct *state, 
+				       double *concs,
+				       double *flux, 
+				       int choice);
+#endif
