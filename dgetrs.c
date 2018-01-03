0c894736a3da1a9fad25bf3393e6f41f4e35a97b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:28 -0800

    r5024 | dbaxter | 2017-12-13 18:29:54 -0800 (Wed, 13 Dec 2017) | 4 lines
    Added len arguments for character arguments to improve fortran
    compatability.
    M    dgetrs.c

--- dgetrs.c
+++ dgetrs.c
@@ -4,14 +4,15 @@
 #include "dtrsm.h"
 #include "dgetrs.h"
 void dgetrs_(char *trans_p, 
-	    int *n_p, 
-	    int *nrhs_p, 
-	    double *a,
-	    int *lda_p,
-	    int *ipiv,
-	    double *b,
-	    int *ldb_p,
-	    int *info_p) {
+	     int *n_p, 
+	     int *nrhs_p, 
+	     double *a,
+	     int *lda_p,
+	     int *ipiv,
+	     double *b,
+	     int *ldb_p,
+	     int *info_p,
+	     int len_trans) {
   /*
     Adapted from dgetrs.f from netlib.
     Solves system of linear equations
@@ -69,8 +70,8 @@ void dgetrs_(char *trans_p,
   izero = 0;
   inc1  = 1;
   incm1 = -1;
-  notran = lsame_(&trans,&n_char);
-  if ((notran == 0) && ((lsame_(&trans,&t_char) + lsame_(&trans,&c_char)) == 0)) {
+  notran = lsame_(&trans,&n_char,1,1);
+  if ((notran == 0) && ((lsame_(&trans,&t_char,1,1) + lsame_(&trans,&c_char,1,1)) == 0)) {
     info = -1;
   } else {
     if (n < 0) {
@@ -106,12 +107,12 @@ void dgetrs_(char *trans_p,
 	  Solve L x = b overwriting B with X
 	*/
 	dtrsm_(&l_char,&l_char,&n_char,&u_char,&n,&nrhs,
-	      &one, a, &lda, b, &ldb);
+	       &one, a, &lda, b, &ldb,1,1,1,1);
 	/*
 	  Solve U x = b overwriting b with x.
 	*/
 	dtrsm_(&l_char,&u_char,&n_char,&n_char,&n,&nrhs,
-	      &one,a, &lda, b, &ldb);
+	       &one,a, &lda, b, &ldb,1,1,1,1);
       }
     } else {
       /*
@@ -121,12 +122,12 @@ void dgetrs_(char *trans_p,
 	 solve u^t *x = b overwriting b with x
       */
       dtrsm_(&l_char,&u_char,&t_char,&n_char,&n,&nrhs,
-	    &one,a,&lda,b,&ldb);
+	     &one,a,&lda,b,&ldb,1,1,1,1);
       /*
 	solve l^t * x = b overwriting b with x
       */
       dtrsm_(&l_char,&l_char,&t_char,&u_char,&n,&nrhs,
-	    &one, a,&lda,b,&ldb);
+	     &one, a,&lda,b,&ldb,1,1,1,1);
       /*
 	Apply row interchanges to solution vectors.
       */
