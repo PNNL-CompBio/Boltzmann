0efc325ee9b527adf21edc4e1e0b3e585927215f
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:51 -0800

    r2670 | dbaxter | 2014-05-02 10:16:54 -0700 (Fri, 02 May 2014) | 3 lines
    Compartment type changed from m0olecule_struct to compartment_struct.
    M    merge_compartments.h

--- merge_compartments.h
+++ merge_compartments.h
@@ -20,9 +20,9 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern int merge_compartments(struct molecule_struct *list1,
-			      struct molecule_struct *list2,
-			      struct molecule_struct *mlist,
+extern int merge_compartments(struct compartment_struct *list1,
+			      struct compartment_struct *list2,
+			      struct compartment_struct *mlist,
 			      char *compartment_text,
 			      int l1,
 			      int l2);
