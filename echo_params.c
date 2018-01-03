6f8be46d079c8832ab99294b02c339c8740486dc
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:55 -0800

    r4048 | dbaxter | 2017-02-08 10:51:14 -0800 (Wed, 08 Feb 2017) | 41 lines
    Added echoing of many new parametesrs
    compartment_file,
    sbml_file,
    ms2js_file,
    kg2js_file,
    rxn_echo_file,
    rxn_mat_file,
    dg0ke_file,
    dictionary_file,
    ode_concs_file,
    ode_lklhd_file,
    ode_flux_file,
    ode_bflux_file,
    net_lklhd_file,
    nl_bndry_flx_file,
    warmup_steps,
    record_steps,
    free_energy_format,
    rxn_view_freq,
    count_view_freq,
    lklhd_view_freq,
    fe_view_freq,
    ode_rxn_view_freq,
    adjust_steady_state,
    print_output,
    use_activities,
    use_deq,
    use_pseudoisomers,
    use_metroplis,
    use_regulation,
    max_regs_per_rxn,
    base_reaction,
    ode_sovler_choice,.
    delta_concs_choice,
    default_volume,
    flux_scaling,
    kf_base_reaction,
    ode_t_final,
    min_conc
    M    echo_params.c

--- echo_params.c
+++ echo_params.c
@@ -46,8 +46,22 @@ int echo_params (FILE *lfp, struct state_struct *state) {
     fprintf(lfp,"state->restart_file           = %s\n",state->restart_file);
     fprintf(lfp,"state->rxn_view_file          = %s\n",state->rxn_view_file);
     fprintf(lfp,"state->bndry_flux_file        = %s\n",state->bndry_flux_file);
-    fprintf(lfp,"state->pseudoisomer_file      = %s\n",
-	    state->pseudoisomer_file);
+    fprintf(lfp,"state->pseudoisomer_file      = %s\n",state->pseudoisomer_file);
+    fprintf(lfp,"state->compartment_file       = %s\n",state->compartment_file);
+    fprintf(lfp,"state->sbml_file              = %s\n",state->sbml_file);
+    fprintf(lfp,"state->ms2js_file             = %s\n",state->ms2js_file);
+    fprintf(lfp,"state->kg2js_file             = %s\n",state->kg2js_file);
+    fprintf(lfp,"state->rxn_echo_file          = %s\n",state->rxn_echo_file);
+    fprintf(lfp,"state->rxn_mat_file           = %s\n",state->rxn_mat_file);
+    fprintf(lfp,"state->dg0ke_file             = %s\n",state->dg0ke_file);
+    fprintf(lfp,"state->dictionary_file        = %s\n",state->dictionary_file);
+    fprintf(lfp,"state->ode_concs_file         = %s\n",state->ode_concs_file);
+    fprintf(lfp,"state->ode_lklhd_file         = %s\n",state->ode_lklhd_file);
+    fprintf(lfp,"state->ode_flux_file          = %s\n",state->ode_flux_file);
+    fprintf(lfp,"state->ode_bflux_file         = %s\n",state->ode_bflux_file);
+    fprintf(lfp,"state->net_lklhd_file         = %s\n",state->net_lklhd_file);
+    fprintf(lfp,"state->nl_bndry_flx_file      = %s\n",state->nl_bndry_flx_file);
+    
     fprintf(lfp,"state->solvent_string         = %s\n",
 	    state->solvent_string);
     
@@ -55,10 +69,36 @@ int echo_params (FILE *lfp, struct state_struct *state) {
     fprintf(lfp,"state->align_len              = %lld\n",state->align_len);
     fprintf(lfp,"state->max_filename_len       = %lld\n",state->max_filename_len);
     fprintf(lfp,"state->max_param_line_len     = %lld\n",state->max_param_line_len);
+    fprintf(lfp,"state->warmup_steps           = %lld\n",state->warmup_steps);
+    fprintf(lfp,"state->record_steps           = %lld\n",state->record_steps);
+    fprintf(lfp,"state->free_energy_format     = %lld\n",state->free_energy_format);
+    fprintf(lfp,"state->rxn_view_freq          = %lld\n",state->rxn_view_freq);
+    fprintf(lfp,"state->count_view_freq        = %lld\n",state->count_view_freq);
+    fprintf(lfp,"state->lklhd_view_freq        = %lld\n",state->lklhd_view_freq);
+    fprintf(lfp,"state->fe_view_freq           = %lld\n",state->fe_view_freq);
+    fprintf(lfp,"state->ode_rxn_view_freq      = %lld\n",state->ode_rxn_view_freq);
+    fprintf(lfp,"state->adjust_steady_state    = %lld\n",state->adjust_steady_state);
+    fprintf(lfp,"state->print_output           = %lld\n",state->print_output);
+    fprintf(lfp,"state->use_activities         = %lld\n",state->use_activities);
+    fprintf(lfp,"state->use_deq                = %lld\n",state->use_deq);
+    fprintf(lfp,"state->use_pseudoisomers      = %lld\n",state->use_pseudoisomers);
+    fprintf(lfp,"state->use_metropolis         = %lld\n",state->use_metropolis);
+    fprintf(lfp,"state->use_regulation         = %lld\n",state->use_regulation);
+    fprintf(lfp,"state->max_regs_per_rxn       = %lld\n",state->max_regs_per_rxn);
+    fprintf(lfp,"state->base_reaction          = %lld\n",state->base_reaction);
+    fprintf(lfp,"state->ode_solver_choice      = %lld\n",state->ode_solver_choice);
+    fprintf(lfp,"state->delta_concs_choice      = %lld\n",state->delta_concs_choice);
     fprintf(lfp,"state->ideal_gas_r            = %le\n",state->ideal_gas_r);
     fprintf(lfp,"state->temp_kelvin            = %le\n",state->temp_kelvin);
     fprintf(lfp,"state->ph                     = %le\n",state->ph);
     fprintf(lfp,"state->ionic_strength         = %le\n",state->ionic_strength);
+    fprintf(lfp,"state->default_volume         = %le\n",state->default_volume);
+    fprintf(lfp,"state->flux_scaling           = %le\n",state->flux_scaling);
+    fprintf(lfp,"state->kf_base_reaction       = %le\n",state->kf_base_reaction);
+    fprintf(lfp,"state->ode_t_final            = %le\n",state->ode_t_final);
+    fprintf(lfp,"state->min_conc               = %le\n",state->min_conc);
+    
+    
   }
   return (success);
 }
