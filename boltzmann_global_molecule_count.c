6ff3dc131d45047d89c305c1baec93e9630a25d2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:30 -0800

    r2232 | dbaxter | 2013-03-20 17:06:00 -0700 (Wed, 20 Mar 2013) | 3 lines
    Removed includes that are also in boltzmann_structs.h.
    M    boltzmann_global_molecule_count.c

--- boltzmann_global_molecule_count.c
+++ boltzmann_global_molecule_count.c
@@ -20,20 +20,6 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <float.h>
-#include <signal.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include "djb_timing_b.h"
 #include "boltzmann_structs.h"
 
 #include "boltzmann_global_molecule_count.h"
