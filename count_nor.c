d80268a362799e1a89519c700d9870fc5b37869a
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:56 -0800

    r2778 | dbaxter | 2014-06-13 14:01:46 -0700 (Fri, 13 Jun 2014) | 3 lines
    Routine to count characters that are not '|' from begining of a line.
    A    count_nor.c

---
+++ count_nor.c
@@ -0,0 +1,49 @@
+/* count_nor.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include "boltzmann_structs.h"
+
+#include "count_nor.h"
+
+int count_nor(char *line) {
+  /*
+    Return the number of leading non-or characters ('|') 
+    in the character string, line.
+    Called by: keg_from_modelseed.c 
+    Calls      strlen(intrinsic).
+  */
+  int nor_chars;
+  int i;
+  int c;
+  int or;
+  /*
+    Want to return 0 if line has zero length.
+  */
+  or = (int)'|';
+  nor_chars = 0;
+  for (i=0;i<strlen(line);i++) {
+    c  = (int)line[i];
+    if (c == or) break;
+    nor_chars += 1;
+  }
+  return (nor_chars);
+}
