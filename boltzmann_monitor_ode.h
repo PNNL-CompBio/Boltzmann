a6fdd2a2b3c7ba850e2050a96fe618308086a825
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:50 -0800

    r4678 | dbaxter | 2017-11-07 17:27:56 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header for boltzmann_monitor_ode.c
    A    boltzmann_monitor_ode.h

---
+++ boltzmann_monitor_ode.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_MONITOR_ODE_H_
+#define _BOLTZMANN_MONITOR_ODE_H_ 1
+extern void boltzmann_monitor_ode(struct state_struct *state,
+				  double time,
+				  double *concs);
+#endif
