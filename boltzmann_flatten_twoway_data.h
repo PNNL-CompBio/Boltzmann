0ac9008d554e133c9aa941ed484d4cc6eba7e8d9
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:33 -0800

    r4516 | dbaxter | 2017-09-20 09:32:57 -0700 (Wed, 20 Sep 2017) | 3 lines
    Header file for boltzmann_flatten_twoway_data.c
    A    boltzmann_flatten_twoway_data.h

---
+++ boltzmann_flatten_twoway_data.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_FLATTEN_TWOWAY_DATA_H_
+#define _BOLTZMANN_FLATTEN_TWOWAY_DATA_H_ 1
+extern int boltzmann_flatten_twoway_data(struct state_struct *state,
+					 void *fstate, int direction,
+					 int *word_pos_p, FILE *lfp);
+#endif
