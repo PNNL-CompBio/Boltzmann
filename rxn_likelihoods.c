35509363032b00245540338360572a4a29f525b4
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:47 -0800

    r2599 | dbaxter | 2014-04-21 17:08:46 -0700 (Mon, 21 Apr 2014) | 4 lines
    concs -> counts
    concentrations -> counts
    M    rxn_likelihoods.c

--- rxn_likelihoods.c
+++ rxn_likelihoods.c
@@ -25,25 +25,25 @@ specific language governing permissions and limitations under the License.
 #include "rxn_likelihood.h"
 
 #include "rxn_likelihoods.h"
-int rxn_likelihoods(double *concs, 
+int rxn_likelihoods(double *counts, 
 		    double *rxn_likelihood_values,
 		    struct state_struct *state,
 		    int rxn_direction) {
   /*
-    Compute the reaction quotients Q_p as the ratio of reactant concentrations
-    to product concentration with any concentration where the stoichiometric
+    Compute the reaction quotients Q_p as the ratio of reactant counts
+    to product counts with any count where the stoichiometric
     coefficient is greater than 1 is raised to that power, and multiply by
     the reaction equilibrium constant ke.
     This could also call rxn_likelihood? YES.
 
     We want to do this in a stable fashion and we want to avoid division
     by zero. So to accomplish this in a stable fashion, the expectation
-    is that all product concentations would have increased and thereby
+    is that all product counts would have increased and thereby
     must be nonzero as they could not be negative to start with.
     The expectation is that there will only be a few (probably fewer than 4)
-    reactants or products, and thus the product of their concentrations
+    reactants or products, and thus the product of their counts
     should not over, nor under flow. If this changes we might want
-    to sort reactant and product concentrations and take the product of 
+    to sort reactant and product counts and take the product of 
     successive quotients which we would expect to be well scaled.
 
     Called by: rxn_log_likelihoods
@@ -51,9 +51,9 @@ int rxn_likelihoods(double *concs,
     Arguments:
      Name           TMF          Descripton  
 
-     concs          D*I          double precision vector of length number-
+     counts          D*I         double precision vector of length number-
                                  unique-molecules with the molecule
-				 concentrations to be use in the reaction
+				 counts to be use in the reaction
 				 likelihood computation.
 				 
      rxn_likelihood_values       double precision vector of length 
@@ -87,7 +87,7 @@ int rxn_likelihoods(double *concs,
   success           = 1;
   nrxns             = (int)state->number_reactions;
   for (i=0;i<nrxns;i++) {
-    rxn_likelihood_values[i] = rxn_likelihood(concs,state,rxn_direction,i);
+    rxn_likelihood_values[i] = rxn_likelihood(counts,state,rxn_direction,i);
   } /* end for(i...) */
   return(success);
 }
