fab063c3ae8730c0172b1e279a6f435d830637fc
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:16 -0800

    r4326 | dbaxter | 2017-06-19 13:33:56 -0700 (Mon, 19 Jun 2017) | 3 lines
    Header file for ode23tb_limit_h.c
    A    ode23tb_limit_h.h

---
+++ ode23tb_limit_h.h
@@ -0,0 +1,5 @@
+#ifndef _ODE23TB_LIMIT_H_H_
+#define _ODE23TB_LIMIT_H_H_ 1
+extern void ode23tb_limit_h(double hmax, double hmin, double rh, double tdir,
+			    double *absh_p,double *h_p);
+#endif
