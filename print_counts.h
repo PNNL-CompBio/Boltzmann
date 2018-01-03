f400d6bdab11cd274dfc8f67d6d0c5781e667668
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:16 -0800

    r3263 | dbaxter | 2016-02-15 14:53:46 -0800 (Mon, 15 Feb 2016) | 3 lines
    Changed step to be 8 bytes to accomodate more than 2 billion steps.
    M    print_counts.h

--- print_counts.h
+++ print_counts.h
@@ -20,4 +20,4 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 specific language governing permissions and limitations under the License.
 ******************************************************************************/
-extern void print_counts(struct state_struct *state, int step);
+extern void print_counts(struct state_struct *state, int64_t step);
