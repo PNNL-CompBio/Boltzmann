f9c1331284436308d341fc16beeff39d436f7475
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:11 -0800

    r4260 | dbaxter | 2017-06-14 10:32:30 -0700 (Wed, 14 Jun 2017) | 3 lines
    rxn_matrix_struct -> reactions_matrix_struct
    M    translate_compartments.c

--- translate_compartments.c
+++ translate_compartments.c
@@ -31,7 +31,7 @@ int translate_compartments(struct state_struct *state) {
     of the rxns_matrix as set by call to unique_compartments.
     Called by: rxns_init
   */
-  struct rxn_matrix_struct *rxns_matrix;
+  struct reactions_matrix_struct *rxns_matrix;
   struct molecule_struct *molecule;
   int64_t *compartment_indices;
   int nzr;
