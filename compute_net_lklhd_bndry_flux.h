9c8951124d053916f9e30463e19a22d8da3b9ca2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:43 -0800

    r3911 | dbaxter | 2017-01-24 15:02:11 -0800 (Tue, 24 Jan 2017) | 3 lines
    Added net_likelihood and net_lklhd_bndry_flux arguments.
    M    compute_net_lklhd_bndry_flux.h

--- compute_net_lklhd_bndry_flux.h
+++ compute_net_lklhd_bndry_flux.h
@@ -1,4 +1,6 @@
 #ifndef _COMPUTE_NET_LKLHD_BNDRY_FLUX_H_
 #define _COMPUTE_NET_LKLHD_BNDRY_FLUX_H_ 1
-extern void compute_net_lklhd_bndry_flux(struct state_struct *state);
+extern void compute_net_lklhd_bndry_flux(struct state_struct *state,
+					 double *net_likelihood, 
+					 double *net_lklhd_bndry_flux);
 #endif
