77c7105e808a22337dc84262a76628524cfda4dc
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:50 -0800

    r4682 | dbaxter | 2017-11-07 17:31:30 -0800 (Tue, 07 Nov 2017) | 3 lines
    Header file for boltzmann_print_cvodeinit_errors.c
    A    boltzmann_print_cvodeinit_errors.h

---
+++ boltzmann_print_cvodeinit_errors.h
@@ -0,0 +1,6 @@
+#ifndef _BOLTZMANN_PRINT_CVODEINIT_ERRORS_H_
+#define _BOLTZMANN_PRINT_CVODEINIT_ERRORS_H_ 1
+extern void boltzmann_print_cvodeinit_errors(int flag, 
+					     void *cvode_mem, 
+					     struct state_struct *state);
+#endif
