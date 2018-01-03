931b7034b96c0e89ee4bdc7916d855b995383ef2
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:02 -0800

    r2862 | dbaxter | 2014-10-14 09:41:05 -0700 (Tue, 14 Oct 2014) | 3 lines
    Structure for translating kegg/modelseed id's to json ids.
    A    t2js_struct.h

---
+++ t2js_struct.h
@@ -0,0 +1,32 @@
+/* trnakg2js_struct.h
+*******************************************************************************
+boltzmann
+Author: Doug Baxter.
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#ifndef _T2JS_STRUCT_DEF_ 
+#define _T2JS_STRUCT_DEF_  1
+struct t2js_struct {
+  int64_t num_ids;
+  int64_t length_strings; 
+  char    *strings;
+  char    **dictionary_ids;
+  char    **json_ids;
+}
+;
+#endif
