304aec1ed633f9c976f237a27c5a663ca7c3983b
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:35 -0800

    r2327 | dbaxter | 2013-06-07 07:57:34 -0700 (Fri, 07 Jun 2013) | 3 lines
    Header for compute_molecular_partition_probability.c
    A    compute_molecular_partition_probability.h

---
+++ compute_molecular_partition_probability.h
@@ -0,0 +1,35 @@
+/* compute_molecular_partition_probability.h
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+The terms and conditions of the License may be found in
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+/*
+ * compute_molecular_partition_probability.h
+ *
+ *  Created on: May 2, 2013
+ *      Author:  Dennis G. Thomas
+        Modified by Doug Baxter Jun 2, 2013
+ */
+
+#ifndef _COMPUTE_MOLECULAR_PARTITION_PROBABILITY_H_
+#define _COMPUTE_MOLECULAR_PARTITION_PROBABILITY_H_
+
+extern int compute_molecular_partition_probability(struct formation_energy_struct *fes);
+#endif /* _COMPUTE_MOLECULAR_PARTITION_PROBABILITY_H_ */
