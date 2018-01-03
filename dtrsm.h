95a339b01db733de6400968553931e4d35ca53d8
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:25 -0800

    r4983 | dbaxter | 2017-12-13 17:53:44 -0800 (Wed, 13 Dec 2017) | 4 lines
    Added length arguments for character variables to make it a fortran
    compatible call.
    M    dtrsm.h

--- dtrsm.h
+++ dtrsm.h
@@ -1,6 +1,7 @@
 #ifndef _DTRSM_H_ 
 #define _DTRSM_H_ 1
 extern void dtrsm_(char *side_p, char *uplo_p, char *transa_p, char *diag_p,
-		  int *m_p, int *n_p, double *alpha_p, double *a,
-		  int *lda_p, double *b, int *ldb_p);
+		   int *m_p, int *n_p, double *alpha_p, double *a,
+		   int *lda_p, double *b, int *ldb_p,
+		   int side_len, int uplo_len, int trans_len, int diag_len);
 #endif
