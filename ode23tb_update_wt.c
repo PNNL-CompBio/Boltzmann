8b3180af2bb0b85857b551f64ace95272ea95e32
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:31:18 -0800

    r4340 | dbaxter | 2017-06-19 13:49:53 -0700 (Mon, 19 Jun 2017) | 4 lines
    Routine to update the scaled solution vector (scaled by its infinity or two
    norm)
    A    ode23tb_update_wt.c

---
+++ ode23tb_update_wt.c
@@ -0,0 +1,30 @@
+#include "system_includes.h"
+#include "vec_set_constant.h"
+#include "ode23tb_update_wt.h"
+void ode23tb_update_wt(int normcontrol, int ny, double normy, 
+		     double *y, double *wt) {
+  /*
+    Set the wt vector to be max of the norm of y and the threshold when
+    normconrol is nonzero and
+    the maximum of the the absolute value of y and threshold when 
+    normcontrol is 0.
+    Called by: ode23tb
+    Calls:     fabs
+  */
+  double wt2;
+  double wta;
+  int    i;
+  int    padi;
+  if (normcontrol) {
+    if (normy > wt[0]) {
+      vec_set_constant(ny,wt,normy);
+    }
+  } else {
+    for (i=0;i<ny;i++) {
+      wta = fabs(y[i]);
+      if (wta > wt[i])  {
+	wt[i] = wta;
+      }
+    }
+  }
+}
