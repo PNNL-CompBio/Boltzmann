ee2f071911f16256a3f987215ceb1519ace1ad19
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:12 -0800

    r1807 | dbaxter | 2012-10-15 17:18:22 -0700 (Mon, 15 Oct 2012) | 6 lines
    Changed documentation.
    Changed implementation to allow rxn that will drain the reactants
    to still be chosen, temporarily replace those 0 or negative concentrations
    with .5 allow the computation to proceed.
    M    rxn_likelihood.c

--- rxn_likelihood.c
+++ rxn_likelihood.c
@@ -40,7 +40,6 @@ double rxn_likelihood(double *concs,
     to product concentration with any concentration where the stoichiometric
     coefficient is greater than 1 is raised to that power, and multiply by
     the reaction equilibrium constant ke.
-    This could also be call rxn_likelihood?
 
     We want to do this in a stable fashion and we want to avoid division
     by zero. So to accomplish this in a stable fashion, the expectation
@@ -52,7 +51,7 @@ double rxn_likelihood(double *concs,
     to sort reactant and product concentrations and take the product of 
     successive quotients which we would expect to be well scaled.
 
-    Called by: choose_rxn
+    Called by: rxn_likelihoods
     Calls      fprintf, fflush, log (intrinsic)
   */
   struct rxn_matrix_struct *rxns_matrix;
@@ -93,37 +92,31 @@ double rxn_likelihood(double *concs,
   i = rxn;
   left_concs        = 1.0;
   right_concs       = 1.0;
+  eq_k = ke[i];
+  if (rxn_direction < 0) {
+    eq_k = 1/eq_k;
+  }
   for (j=rxn_ptrs[i];j<rxn_ptrs[i+1];j++) {
     coeff = rcoef[j];
     conc  = concs[molecules_indices[j]];
+    if (rxn_direction < 0) {
+      coeff = -coeff;
+    }
     if (coeff < 0) {
       for (k=0;k<(0-coeff);k++) {
 	left_concs = left_concs * (conc-k);	
       } 
     } else {
       for (k=0;k<coeff;k++) {
-	right_concs = right_concs * (conc-k);
-	;	
+	if ((conc - k) <= 0) {
+	  right_concs = right_concs * 0.5;
+	} else {
+	  right_concs = right_concs * (conc-k);
+	}
       } 
     }
   }
-  eq_k = ke[i];
-  if (rxn_direction < 0) {
-    eq_k = 1/eq_k;
-    t_concs = right_concs;
-    right_concs = left_concs;
-    left_concs  = t_concs;
-  }
-  if ((left_concs < 1.0) && (right_concs > 1.0)) {
-    /*left_concs = small_nonzero;*/
-    left_concs = 1.0;
-    rxn_likelihood = 0;
-  } else {
-    if (right_concs < 1.0) {
-      /*right_concs = small_nonzero;*/
-      right_concs = 1.0;
-    }
-    rxn_likelihood = eq_k * (left_concs/ right_concs);
-  } 
+  rxn_likelihood = eq_k * (left_concs/ right_concs);
+
   return(rxn_likelihood);
 }
