8e30484e06b8c43c52db8b2f02e2ff65189c2ce3
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:29:04 -0800

    r1436 | dbaxter | 2012-06-08 18:09:09 -0700 (Fri, 08 Jun 2012) | 3 lines
    Routine to parse the reactions file and build the reactions matrix as
    well as usorted species and compartment lists.

---
+++ parse_reactions_file.c
@@ -0,0 +1,492 @@
+/* parse_reactions_file.c
+*******************************************************************************
+boltzmann
+
+Pacific Northwest National Laboratory, Richland, WA 99352.
+
+Copyright (c) 2010 Battelle Memorial Institute.
+
+Publications based on work performed using the software should include 
+the following citation as a reference:
+
+
+Licensed under the Educational Community License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+The terms and conditions of the License may be found in 
+ECL-2.0_LICENSE_TERMS.TXT in the directory containing this file.
+        
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
+******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <float.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "boltzmann_structs.h"
+
+#include "parse_rxn_file_keyword.h"
+#include "count_ws.h"
+#include "count_nws.h"
+#include "upcase.h"
+#include "is_a_coef.h"
+
+#include "parse_reactions_file.h"
+int parse_reactions_file(struct state_struct *state) {
+  /*
+    Determine the number of reactions,
+    Total length of species names, and
+    max possiblie number of species, and
+    total length of the file, total length
+    of compartment names, total length of pathway_names,
+    total_length reaction titles.
+    Called by: boltzmann
+    Calls    : parse_rxn_file_keyword
+               count_ws
+	       count_nws
+	       upcase
+  */
+  struct rxn_struct *reactions;
+  struct rxn_struct *reaction;
+  struct rxn_matrix_struct *rxns_matrix;
+  struct istring_elem_struct *unsorted_species;
+  struct istring_elem_struct *unsorted_cmpts;
+  int64_t *keyword_lens;
+  int64_t *row_ptrs;
+  int64_t *column_indices;
+  int64_t *coefficients;
+  int64_t rxn_buff_len;
+  int64_t total_length;
+  int64_t species_len;
+  int64_t rxn_title_len;
+  int64_t pathway_len;
+  int64_t compartment_len;
+  int64_t line_len;
+  int64_t seek_offset;
+  int64_t rxn_title_pos;
+  int64_t pathway_pos;
+  int64_t compartment_pos;
+  int64_t species_pos;
+  int64_t align_len; 
+  int64_t align_mask; 
+  int64_t len;
+  int64_t sll;
+  char **matrix_text;
+  char *rxn_buffer;
+  char *fgp;
+  char **keywords;
+  char *rctnts;
+  char *prdcts;
+  char *rxn_title_text;
+  char *pathway_text;
+  char *compartment_text;
+  char *species_text;
+  char *raw_species_text;
+
+  int success;
+  int rxns;
+
+  int species;
+  int ws_chars;
+
+  int line_type;
+  int kl;
+
+  int cmpts;
+  int pos;
+
+  int skip;
+  int sl;
+
+  int ns;
+  int padding;
+
+  FILE *rxn_fp;
+  FILE *lfp;
+  success = 1;
+  seek_offset = (int64_t)0;
+  rxn_buff_len = state->rxn_buff_len;
+  lfp          = state->lfp;
+  rxn_fp       = state->rxn_fp;
+  align_len    = state->align_len;
+  align_mask   = state->align_mask;
+  if (rxn_fp == NULL) {
+    success = 0;
+    fprintf(stderr,"parse_reactions_file: reaction file not open, %s\n",
+	    state->reaction_file);
+    fflush(stderr);
+  }
+  if (success) {
+    /*
+      Seek to beginning of file.
+    */
+    fseek(rxn_fp,seek_offset,SEEK_SET);
+    rxn_buffer = state->rxn_buffer;
+    keywords   = state->rxn_file_keywords;
+    keyword_lens = state->rxn_file_keyword_lengths;
+    rxns    = 0;
+    species = 0;
+    cmpts   = 0;
+    /*
+      Should get a reaction line, a pathway line, a left line, a right line,
+      a dgzero line, a dgzero-units line and a terminating // line per
+      reaction. We build a state machine
+      This is not quite accurate,as some reactions may not
+      have a pathway line, and Bill wants to also add an additional
+      compartment line.
+      
+    */
+    rxn_title_pos          = (int64_t)0;
+    pathway_pos            = (int64_t)0;
+    compartment_pos        = (int64_t)0;
+    species_pos            = (int64_t)0;
+    unsorted_species            = state->unsorted_species;
+    unsorted_cmpts              = state->unsorted_cmpts;
+    rxn_title_text              = state->rxn_title_text;
+    pathway_text                = state->pathway_text;
+    compartment_text            = state->compartment_text;
+    species_text                = state->species_text;
+    raw_species_text            = state->raw_species_text;
+    reactions                   = state->reactions;
+    rxns_matrix                 = state->reactions_matrix;
+    row_ptrs                    = rxns_matrix->row_ptrs;
+    column_indices              = rxns_matrix->column_indices;
+    coefficients                = rxns_matrix->coefficients;
+    matrix_text                 = rxns_matrix->text;
+    reaction                    = reactions;
+    reaction->lcompartment      = NULL;
+    reaction->rcompartment      = NULL;
+    reaction->pathway           = NULL;
+    reaction->left_compartment  = -1;
+    reaction->right_compartment = -1;
+    reaction->num_reactants     = 0;
+    reaction->num_products      = 0;
+    row_ptrs[rxns]              = species;
+    fgp = fgets(rxn_buffer,rxn_buff_len,rxn_fp);
+    while ((fgp && success) && (! feof(rxn_fp))) {
+      line_len = strlen(rxn_buffer);
+      /*
+	Check that last character in line is a newline and replace with 
+	a \0.
+      */
+      if (rxn_buffer[line_len-1] == '\n') {
+	rxn_buffer[line_len-1] = '\0';
+	line_len -= 1;
+      } else {
+	fprintf(stderr,"parse_reactions_file: Error input line longer than"
+		" %d characters\n",rxn_buff_len);
+	fflush(stderr);
+	success = 0;
+	break;
+      }
+      if (success) {
+      	/*
+      	  state, every reaction starts with a REACTION line
+      	  ends with a // line.
+      	  It requires: LEFT, RIGHT, DGZERO and DGZERO-UNITS lines.
+      	  It may have PATHWAY and LEFT_COMPARTMENT, RIGHT_COMPARTMENT  lines.
+
+      	  Allow pre REACTION lines in reactions file as a header.
+      	  So before the first REACTION line the state is -1
+      		
+      	  When a // is seen then we hit transfer state 1, where 
+      	  we must get a REACTION line next.
+      	*/
+	line_type = parse_rxn_file_keyword(rxn_buffer,state);
+	if (line_type >= 0) {
+	  kl = keyword_lens[line_type];
+	  ws_chars = count_ws((char *)&rxn_buffer[kl]);      
+	} else {
+	  kl       = 0;
+	  ws_chars = 0;
+	}
+	/*
+	  Line header matched.
+	*/
+	switch (line_type) {
+	case 0: 
+	  /*
+	    Reaction title line.
+	    Get size of reaction title allow +1 for terminating \0.
+          */
+	  rxn_title_len = (int64_t)(line_len - kl - ws_chars+1);
+	  /*
+	    Copy the reaction title.
+	  */
+	  reaction->title = (char *)&rxn_title_text[rxn_title_pos];
+	  strcpy(reaction->title,(char*)&rxn_buffer[kl+ws_chars]);
+	  /*
+	    caution bit twiddle follows: 
+	    padding = (align_len - (rxn_titl_len % align_len)) % align_len
+	    Thou shalt not use %.
+	  */
+	  padding = (align_len - (rxn_title_len & align_mask)) & align_mask;
+	  rxn_title_pos += rxn_title_len + padding;
+	  break;
+	case 1:
+	  /*
+	    Pathway line.
+	  */
+	  pathway_len = line_len - kl - ws_chars + 1;
+	  reaction->pathway = (char *)&pathway_text[pathway_pos];
+	  strcpy(reaction->pathway,(char*)&rxn_buffer[kl+ws_chars]);
+	  padding = (align_len - (pathway_len & align_mask)) & align_mask;
+	  pathway_pos += pathway_len + padding;
+	  break;
+	case 2: 
+	  /*
+	    Compartment line.
+	  */
+	  compartment_len = line_len - kl - ws_chars + 1;
+	  reaction->lcompartment = (char *)&compartment_text[compartment_pos];
+	  strcpy(reaction->lcompartment,(char*)&rxn_buffer[kl+ws_chars]);
+	  upcase(compartment_len,reaction->lcompartment,
+		 reaction->lcompartment);
+	  reaction->rcompartment = NULL;
+	  padding = (align_len - (compartment_len & align_mask)) & align_mask;
+	  compartment_pos += compartment_len + padding;
+	  reaction->left_compartment = cmpts;
+	  reaction->right_compartment = cmpts;
+	  unsorted_cmpts->string = reaction->lcompartment;
+	  unsorted_cmpts->index  = cmpts;
+	  cmpts += 1;
+	  break;
+	case 3: 
+	  /*
+	    Left Compartment line.
+	  */
+	  compartment_len = line_len - kl - ws_chars + 1;
+	  reaction->lcompartment = (char *)&compartment_text[compartment_pos];
+	  strcpy(reaction->lcompartment,(char*)&rxn_buffer[kl+ws_chars]);
+	  upcase(compartment_len,reaction->lcompartment,
+		 reaction->lcompartment);
+	  padding = (align_len - (compartment_len & align_mask)) & align_mask;
+	  compartment_pos += compartment_len + padding;
+	  reaction->left_compartment = cmpts;
+	  unsorted_cmpts->string = reaction->lcompartment;
+	  unsorted_cmpts->index  = cmpts;
+	  cmpts += 1;
+	  break;
+	case 4: 
+	  /*
+	    Right Compartment line.
+	  */
+	  compartment_len = line_len - kl - ws_chars + 1;
+	  reaction->rcompartment = (char *)&compartment_text[compartment_pos];
+	  strcpy(reaction->rcompartment,(char*)&rxn_buffer[kl+ws_chars]);
+	  upcase(compartment_len,reaction->rcompartment,
+		 reaction->rcompartment);
+	  padding = (align_len - (compartment_len & align_mask)) & align_mask;
+	  compartment_pos += compartment_len + padding;
+	  reaction->right_compartment = cmpts;
+	  unsorted_cmpts->string = reaction->rcompartment;
+	  unsorted_cmpts->index  = cmpts;
+	  cmpts += 1;
+	  break;
+	case 5:
+	  /*
+	    A left line, count and record reactant species and coefficients.
+	  */
+	  rctnts = (char *)&rxn_buffer[kl+ ws_chars];
+	  pos = 0;
+	  len = strlen(rctnts);
+	  while (pos < len) {
+	    sl = (int64_t)count_nws(rctnts);
+	    if (sl > 0) {
+	      column_indices[species] = species;
+	      if (is_a_coef(sl,rctnts)) {
+		rctnts[sl] = '\0';
+		coefficients[species] = - atoi(rctnts);
+		rctnts[sl] = ' ';
+
+		pos += sl;
+		rctnts += sl; /* Caution address arithmetic. */
+
+		skip   =  count_ws(rctnts);
+
+		pos += skip;
+		rctnts += skip; /* Caution address arithmetic. */
+
+		sl = count_nws(rctnts);
+	      } else {
+		coefficients[species] = -1;
+	      }
+	      matrix_text[species] = (char*)&raw_species_text[species_pos];
+	      rctnts[sl] = '\0';
+	      sll = (int64_t)sl + (int64_t)1;
+	      padding = (align_len - (sll & align_mask)) & align_mask;
+	      strcpy((char *)&raw_species_text[species_pos],rctnts);
+	      upcase(sl,(char *)&raw_species_text[species_pos],
+		     (char *)&species_text[species_pos]);
+	      unsorted_species->string = (char *)&species_text[species_pos];
+	      unsorted_species->index  = species;
+	      unsorted_species += 1; /* Caution address arithmetic. */
+
+	      species_pos += (int64_t)(sll + padding);
+	      rctnts[sl] = ' ';
+
+	      pos += sl; /* Caution address arithmetic. */
+	      rctnts += sl;
+
+	      species += 1;
+	      reaction->num_reactants += 1;
+	      skip = count_ws(rctnts);
+
+	      pos += skip; /* Caution address arithmetic. */
+	      rctnts += skip;
+
+	      if (pos < len) {
+		if (rctnts[0] != '+') {
+		  fprintf(stderr,"parse_reactions_file: Error, missing + "
+			  "between reactants, reactions file line was \n%s\n",
+			  rxn_buffer);
+		  fflush(stderr);
+		  success = 0;
+		  break;
+		}
+
+		pos += 1;
+		rctnts += 1; /* Caution address arithmetic.*/
+
+		skip = count_ws(rctnts);
+
+		pos  += skip;
+		rctnts += skip; /* Caution address arithmetic.*/
+	      }
+	    } /* end if (sl > 0)  - a reactant was found . */
+	  } /* end while (pos > len) */
+	  break;
+	case 6:
+	  /*
+	    A right line, count product species.
+	  */
+	  prdcts = (char *)&rxn_buffer[kl + ws_chars];
+	  pos = 0;
+	  len = strlen(prdcts);
+	  while (pos < len) {
+	    sl = (int64_t)count_nws(prdcts);
+	    if (sl > 0) {
+	      column_indices[species] = species;
+	      if (is_a_coef(sl,prdcts)) {
+		prdcts[sl] = '\0';
+		coefficients[species] = atoi(prdcts);
+		prdcts[sl] = ' ';
+
+		pos += sl;
+		prdcts += sl; /* Caution address arithmetic. */
+
+		skip   =  count_ws(prdcts);
+		
+		pos += skip;
+		prdcts += skip; /* Caution address arithmetic.*/
+
+		sl = count_nws(prdcts);
+	      } else {
+		coefficients[species] = 1;
+	      }
+	      matrix_text[species] = (char*)&species_text[species_pos];
+	      prdcts[sl] = '\0';
+	      sll = (int64_t)sl + (int64_t)1;
+	      padding = (align_len - (sll & align_mask)) & align_mask;
+	      strcpy((char *)&raw_species_text[species_pos],prdcts);
+	      upcase(sl,(char *)&raw_species_text[species_pos],
+		     (char *)&species_text[species_pos]);
+	      species_pos += (int64_t)(sll + padding);
+	      prdcts[sl] = ' ';
+
+	      pos += sl;
+	      prdcts += sl; /* Caution address arithmetic. */
+
+	      species += 1;
+	      reaction->num_products += 1;
+	      skip = count_ws(prdcts);
+
+	      pos += skip;
+	      prdcts += skip; /* Caution address arithmetic. */
+
+	      if (pos < len) {
+		if (prdcts[0] != '+') {
+		  fprintf(stderr,"parse_reactions_file: Error, missing + "
+			  "between products, reactions file line was \n%s\n",
+			  rxn_buffer);
+		  fflush(stderr);
+		  success = 0;
+		  break;
+		}
+		pos    += 1;
+		prdcts += 1; /* Caution address arithmetic. */
+
+		skip = count_ws(prdcts);
+
+		pos  += skip;
+		prdcts += skip; /* Caution address arithmetic. */
+	      }
+	    } /* end if (sl > 0)  - a reactant was found . */
+	  } /* end while (pos > len) */
+	  break;
+	case 7:
+	  /* 
+	     A DGZERO line
+	  */
+	  ns = sscanf ((char*)&rxn_buffer[ws_chars+kl],"%le",
+		       &reaction->delta_g0);
+	  if (ns < 1) {
+	    fprintf(stderr,
+		    "parse_reactions_file: malformed DGZERO line was\n%s\n",
+		    rxn_buffer);
+	    fflush(stderr);
+	    success = 0;
+	    break;
+	  }
+	  break;
+	case 8:
+	  /* 
+	     A DGZERO-UNITS line
+	  */
+	  sl = count_nws((char*)&rxn_buffer[ws_chars+kl]);
+	  upcase(sl,(char*)&rxn_buffer[ws_chars+kl],(char*)&rxn_buffer[ws_chars+kl]);
+	  if (strncmp((char*)&rxn_buffer[ws_chars+kl],"KJ/MOL",6) == 0) {
+	    reaction->unit_i = 1;
+	  } else {
+	    reaction->unit_i = 0;
+	  }
+	  break;
+	case 9:
+	  reaction->self_id = rxns;
+	  rxns += 1;
+	  /*
+	    Caution address arithmetic follows
+	    reaction = (struct rxn_struct*)&reactions[rxns];
+	  */
+	  reaction += 1;
+	  reaction->lcompartment      = NULL;
+	  reaction->rcompartment      = NULL;
+	  reaction->pathway           = NULL;
+	  reaction->left_compartment  = -1;
+	  reaction->right_compartment = -1;
+	  reaction->num_reactants     = 0;
+	  reaction->num_products      = 0;
+	  row_ptrs[rxns]              = species;
+	  break;
+        default:
+	break;
+	}
+	fgp = fgets(rxn_buffer,rxn_buff_len,rxn_fp);
+      }/* end if (success) */
+    } /* end while(fgp...) */
+  } /* end if (success) */
+  state->reaction_file_length = total_length;
+  state->number_reactions = rxns;
+  state->number_species   = species;
+  state->number_compartments = cmpts;
+  state->species_len = species_len;
+  state->pathway_len = pathway_len;
+  state->compartment_len = compartment_len;
+  state->rxn_title_len = rxn_title_len;
+  return(success);
+}
