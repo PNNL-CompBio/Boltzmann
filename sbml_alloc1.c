dbf7739c8c32a6c075529c150d7a24d26fbfc3b6
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:30:04 -0800

    r2893 | dbaxter | 2014-10-15 17:05:12 -0700 (Wed, 15 Oct 2014) | 3 lines
    Added stuff for translation of specid's to json strings.
    M    sbml_alloc1.c

--- sbml_alloc1.c
+++ sbml_alloc1.c
@@ -36,13 +36,23 @@ int sbml_alloc1(struct sbml2bo_struct *state) {
   struct compartment_struct *compartments;
   int64_t ask_for;
   int64_t one_l;
+  int64_t trans_pos;
   char *log_file;
+  char *specid_2_json_strings;
+  char **species_trans;
+  char **sort_species_trans_scratch;
 
   int     max_compartment_len;
   int     success;
 
   int     num_cmpts;
-  int     padi;
+  int     num_species;
+
+  int     i;
+  int     j;
+
+  int     max_specid_len;
+  int     max_species_len;
 
   FILE    *lfp;
   FILE    *error_fp;
@@ -50,6 +60,9 @@ int sbml_alloc1(struct sbml2bo_struct *state) {
   one_l   = (int64_t)1;
   max_compartment_len = state->max_compartment_len;
   num_cmpts           = state->num_cmpts;
+  num_species         = state->num_species;
+  max_specid_len      = state->max_specid_len;
+  max_species_len     = state->max_species_len;
   lfp         = state->log_fp;
   if (lfp == NULL) {
     error_fp = stderr;
@@ -78,6 +91,54 @@ int sbml_alloc1(struct sbml2bo_struct *state) {
   if (success) {
     state->sorted_compartments = compartments;
     state->unsorted_compartments = (struct compartment_struct*)&(compartments[num_cmpts]);
+    ask_for = (num_species*(max_specid_len+max_species_len));
+    specid_2_json_strings = (char *)calloc(one_l,ask_for);
+    if (specid_2_json_strings == NULL) {
+      success = 0;
+      fprintf(error_fp,"sbml_alloc1: Error unable to allocate %ld bytes for "
+	      "specid_2_json_strings\n",ask_for);
+      fflush(error_fp);
+    }
+  }
+  if (success) {
+    state->specid_2_json_strings = specid_2_json_strings;
+    ask_for = ((int64_t)(num_species << 1)) * sizeof(char *);
+    species_trans = (char **)calloc(one_l,ask_for);
+    if (species_trans == NULL) {
+      success = 0;
+      fprintf(error_fp,"sbml_alloc1: Error unable to allocate %ld bytes for "
+	      "species_trans\n",ask_for);
+      fflush(error_fp);
+    }
+  }
+  if (success) {
+    state->spec_ids = species_trans;
+    state->translations = (char **)&species_trans[num_species];
+    if (state->num_kegg_ids < (num_species << 2)) {
+      ask_for = ask_for + ask_for;
+      sort_species_trans_scratch = (char **)calloc(one_l,ask_for);
+      if (sort_species_trans_scratch == NULL) {
+	success = 0;
+	fprintf(error_fp,"sbml_alloc1: Error unable to allocate %ld bytes for "
+		"sort_species_trans_scratch\n",ask_for);
+	fflush(error_fp);
+      } else {
+	state->sort_species_trans_scratch = sort_species_trans_scratch;
+      }
+    } else {
+      state->sort_species_trans_scratch = state->sort_json_ids_scratch;
+    }
+  }
+  if (success) {
+    trans_pos = (int64_t)0;
+    j = 0;
+    for (i=0;i<num_species;i++) {
+      species_trans[j] = (char*)&specid_2_json_strings[trans_pos];
+      trans_pos += max_specid_len;
+      species_trans[j+1] = (char*)&specid_2_json_strings[trans_pos];
+      trans_pos += max_species_len;
+      j += 2;
+    }
   }
   return(success);
 }
