3b7f14d4ce41e371d3e774977122eb524302836a
douglas.baxter <douglas.baxter@pnnl.gov>
2018-01-02 17:32:16 -0800

    r4891 | dbaxter | 2017-11-27 13:18:42 -0800 (Mon, 27 Nov 2017) | 4 lines
    Removed htry,nonnegative, normcontrol, print_concs arguments,
    those are fields in state and or state->ode23tb_params.
    M    ode_solver.h

--- ode_solver.h
+++ ode_solver.h
@@ -1,6 +1,4 @@
 #ifndef _ODE_SOLVER_H_
 #define _ODE_SOLVER_H_ 1
-extern int ode_solver (struct state_struct *state, double *counts,
-		       double htry, int nonnegative, int normcontrol,
-		       int print_concs, int choice);
+extern int ode_solver (struct state_struct *state, double *counts, int choice);
 #endif
