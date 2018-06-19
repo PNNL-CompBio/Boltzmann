c4a8a7aee07c83479bbd18fb27db8343d975c81c
douglas.baxter <douglas.baxter@pnnl.gov>
2018-06-18 18:37:37 -0700

    version change 5092

--- src/alloc0.c
+++ src/alloc0.c
@@ -136,7 +136,7 @@ int alloc0(struct state_struct **statep, int setup) {
     usage = state->usage;
   }
   if (success && setup) {
-    state->version_no = 5091; /* Checked in revision of state_struct.h */
+    state->version_no = 5092; /* Checked in revision of state_struct.h */
     /*
       Allocate_space for reading the parameter file.
     */
